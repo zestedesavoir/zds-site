9936741c8ba34d04266a52c624315e89e58de000
firm1 <firm1@users.noreply.github.com>
2013-11-07 05:28:59 +0100

    Module de news, Forums et Projets

---
+++ lbp/utils/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from lbp.utils.models import DateManager, Alert
+ 
+admin.site.register(DateManager)
+admin.site.register(Alert)
--- lbp/utils/models.py
+++ lbp/utils/models.py
@@ -9,3 +9,19 @@ class DateManager(models.Model):
     create_at = models.DateTimeField('Date de création', blank=True)
     pubdate = models.DateTimeField('Date de publication', blank=True, null=True)
     update = models.DateTimeField('Date de mise à jour', blank=True, null=True)
+    
+    def __unicode__(self):
+        return '{0}|{1}|{2}'.format(self.create_at, self.update, self.pubdate )
+    
+
+class Alert(models.Model):
+    class Meta :
+        verbose_name = 'Alerte'
+        verbose_name_plural = 'Alertes'
+    
+    text = models.TextField('Texte d\'alerte')
+    pubdate = models.DateTimeField('Date de publication', blank=True, null=True)
+    
+    def __unicode__(self):
+        return '{0}'.format(self.text)
+        
