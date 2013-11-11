7b5d202b0ff189ae517ce84971508ac80082b312
willy <https://dralliw@bitbucket.org>
2013-11-11 23:14:07 +0100

    FIx bug

--- lbp/utils/templatetags/topbar.py
+++ lbp/utils/templatetags/topbar.py
@@ -3,7 +3,7 @@
 from django import template
 
 from lbp.forum.models import Category as fCategory
-from lbp.utils.models import Category as pCategory
+from lbp.utils.models import Category as Category
 
 register = template.Library()
 
@@ -16,7 +16,7 @@ def top_categories(user):
 
 @register.filter('top_categories_news')
 def top_categories_news(user):
-    categories = pCategory.objects.all()
+    categories = Category.objects.all()
     liste=[]
     for c in categories :
         if c.get_news_count()>0:
@@ -26,7 +26,7 @@ def top_categories_news(user):
 
 @register.filter('top_categories_tuto')
 def top_categories_tuto(user):
-    categories = pCategory.objects.all()
+    categories = Category.objects.all()
     liste=[]
     for c in categories :
         if c.get_news_count()>0:
@@ -36,7 +36,7 @@ def top_categories_tuto(user):
 
 @register.filter('top_categories_project')
 def top_categories_project(user):
-    categories = pCategory.objects.all()
+    categories = Category.objects.all()
     liste=[]
     for c in categories :
         if c.get_news_count()>0:
