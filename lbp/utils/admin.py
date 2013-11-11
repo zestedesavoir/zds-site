f51045832e66a3f71383f52898c7c18fdae4aaf1
firm1 <firm1@users.noreply.github.com>
2013-11-11 23:32:24 +0100

    Admin availaible

--- lbp/utils/admin.py
+++ lbp/utils/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from lbp.utils.models import DateManager, Alert, Licence
+from lbp.utils.models import DateManager, Alert, Licence, Category, Version
  
 admin.site.register(DateManager)
 admin.site.register(Alert)
 admin.site.register(Licence)
+admin.site.register(Version)
+admin.site.register(Category)
