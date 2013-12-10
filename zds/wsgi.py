dbd5aa50b2b4c1c96f735158f54e6a7986aef688
firm1 <firm1@users.noreply.github.com>
2013-12-10 23:51:12 +0100

    Correct wsgi bug

--- zds/wsgi.py
+++ zds/wsgi.py
@@ -19,7 +19,7 @@ from django.core.wsgi import get_wsgi_application
 import os
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lbp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zds.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
