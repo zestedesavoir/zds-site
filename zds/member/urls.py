06d70153a80f4a46aae1609926177c7ef3b1fa1c
GerardPaligot <androwiiid@gmail.com>
2013-12-19 01:16:44 +0100

    Remove some useless url

--- zds/member/urls.py
+++ zds/member/urls.py
@@ -4,10 +4,9 @@ from django.conf.urls import patterns, url
 
 import views
 
-
 urlpatterns = patterns('',
+    url(r'^$', views.index),
     url(r'^voir/(?P<user_name>.+)$', views.details),
-    url(r'^profil/editer$', views.edit_profile),
     url(r'^profil/modifier/(?P<user_pk>\d+)', views.modify_profile),
 
     url(r'^publications$', views.publications),
@@ -18,7 +17,6 @@ urlpatterns = patterns('',
     url(r'^parametres/user$', views.settings_user),
 
     url(r'^connexion$', views.login_view),
-    url(r'^inscription$', views.register_view),
     url(r'^deconnexion/$', views.logout_view),
-    url(r'^$', views.index),
+    url(r'^inscription$', views.register_view),
 )
