341cec32a9e470c2a0229bc4b57d9adaa1cbd0ee
firm1 <firm1@users.noreply.github.com>
2013-12-20 06:30:54 +0100

    Fix create/import mini-tuto

--- zds/utils/templatetags/repo_reader.py
+++ zds/utils/templatetags/repo_reader.py
@@ -50,25 +50,28 @@ def repo_chapter(chapter, sha=None):
         return {'introduction': chapter.get_introduction(),
             'conclusion': chapter.get_conclusion()}
     else:
-        repo = Repo(chapter.get_path())
-        bls = repo.commit(sha).tree.blobs
-        for bl in bls:
-            if bl.path=='introduction.md':
-                intro = bl.data_stream.read()
-            if bl.path=='conclusion.md':
-                conclu = bl.data_stream.read()
-        return {'introduction': intro.decode('utf-8'), 'conclusion': conclu.decode('utf-8')}
+        repo = Repo(chapter['path'])
+        if chapter['type'] == 'MINI':
+            return {'introduction': None, 'conclusion': None }
+        else : 
+            bls = repo.commit(sha).tree.blobs
+            for bl in bls:
+                if bl.path=='introduction.md':
+                    intro = bl.data_stream.read()
+                if bl.path=='conclusion.md':
+                    conclu = bl.data_stream.read()
+            return {'introduction': intro.decode('utf-8'), 'conclusion': conclu.decode('utf-8')}
 
 @register.filter('repo_extract')
 def repo_extract(extract, sha=None):
     if sha == None:
         return {'text': extract.get_text()}
     else:
-        repo_e = Repo(extract.get_path())
+        repo_e = Repo(extract['path'])
         bls_e = repo_e.commit(sha).tree.blobs
         
         for bl in bls_e:
-            if bl.path==slugify(extract.title)+'.md':
+            if bl.path==slugify(extract['title'])+'.md':
                 text = bl.data_stream.read()
                 return {'text': text.decode('utf-8')}
     return {'text': ''}
