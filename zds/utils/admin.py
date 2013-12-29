97799c55d2202af777952af1a11fc297704ac56e
GerardPaligot <androwiiid@gmail.com>
2013-12-29 02:14:39 +0100

    Update common category and create subcategory

--- zds/utils/admin.py
+++ zds/utils/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from zds.utils.models import Alert, Licence, Category
+from zds.utils.models import Alert, Licence, Category, SubCategory
 
 
 admin.site.register(Alert)
 admin.site.register(Licence)
 admin.site.register(Category)
+admin.site.register(SubCategory)
