c0caecefe45ac362589a58fb04fd5d960ac2d781
GerardPaligot <androwiiid@gmail.com>
2013-12-29 03:53:43 +0100

    Get subcategories from Category

--- zds/utils/admin.py
+++ zds/utils/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from zds.utils.models import Alert, Licence, Category, SubCategory
+from zds.utils.models import Alert, Licence, Category, SubCategory, CategorySubCategory
 
 
 admin.site.register(Alert)
 admin.site.register(Licence)
 admin.site.register(Category)
 admin.site.register(SubCategory)
+admin.site.register(CategorySubCategory)
