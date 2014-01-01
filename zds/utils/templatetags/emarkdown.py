71e9bd89de566f5a9be1f5ea765a7493da904aba
cgabard <christophe.gabard@gmail.com>
2014-01-01 18:30:38 +0100

    Remove all old unnecessary code for markdown to html generation

--- zds/utils/templatetags/emarkdown.py
+++ zds/utils/templatetags/emarkdown.py
@@ -6,10 +6,6 @@ from django.utils.safestring import mark_safe
 import markdown
 import time
 
-import cssstyles
-
-
-md_cssstyle = cssstyles.StyleExtension()
 
 register = template.Library()
 
@@ -20,28 +16,9 @@ def humane_time(t, conf={}):
 
 @register.filter(needs_autoescape=False)
 def emarkdown(value):
-    # Allowed output tags from user raw HTML input and markdown generation
-    allowed_tags = ['div', 'span', 'p', 'pre', 'hr', 'img', 'br',
-                    'strong', 'em', 'i', 'b', 'code', 'sub', 'sup', 'del',
-                    'a', 'abbr', 'blockquote',
-                    'ul', 'ol', 'li',
-                    'table', 'thead', 'tbody', 'tr', 'td', 'th']
-
-    # Add h1…h6 titles, have a more beautiful way to do it?
-    [allowed_tags.append('h{}'.format(i + 1)) for i in range(6)]
-
-    allowed_attrs = {
-        '*': ['class', 'id'],
-        'a': ['href', 'title'],
-        'img': ['src', 'alt'],
-    }
-
     return mark_safe('<div class="markdown">{0}</div>'.format(
-        bleach.clean(
-            markdown.markdown(value, extensions=[
-                              md_cssstyle,
-                              'codehilite(force_linenos=True)',
-                              'extra']),
-            tags=allowed_tags,
-            attributes=allowed_attrs)
+            markdown.markdown(  value, 
+                                extensions=[
+                                'codehilite(force_linenos=True)',
+							  ])
         .encode('utf-8')))
