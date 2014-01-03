8342615af304f0fd2e718aa78072bf20bc792bc3
GerardPaligot <androwiiid@gmail.com>
2014-01-03 18:43:57 +0100

    Optimize decorator methods in member module

--- zds/member/decorator.py
+++ zds/member/decorator.py
@@ -1,15 +1,21 @@
 # coding: utf-8
 
 from django.http import Http404
+from django.contrib.auth.models import User
 
 from zds.member.models import Profile
 
 def can_read_now(func):
     '''Decorator to check that the user can read now'''
     def _can_read_now(request, *args, **kwargs):
-        profile = Profile.objects.filter(user__pk=request.user.pk).all()
-        if len(profile)>0:
-            if not profile[0].can_read_now():
+        try:
+            profile = Profile.objects.get(user__pk = request.user.pk)
+        except:
+            # The user is a visitor
+            profile = None
+
+        if profile is not None:
+            if not profile.can_read_now():
                 raise Http404
         return func(request, *args, **kwargs)
     return _can_read_now
@@ -17,8 +23,13 @@ def can_read_now(func):
 def can_write_and_read_now(func):
     '''Decorator to check that the user can read and write now'''
     def _can_write_and_read_now(request, *args, **kwargs):
-        profile = Profile.objects.filter(user__pk=request.user.pk).all()
-        if len(profile)>0:
+        try:
+            profile = Profile.objects.get(user__pk = request.user.pk)
+        except:
+            # The user is a visitor
+            profile = None
+
+        if profile is not None:
             if not profile[0].can_read_now() or not profile[0].can_write_now():
                 raise Http404
         return func(request, *args, **kwargs)
