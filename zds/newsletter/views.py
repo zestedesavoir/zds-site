6655d6214b8812d043e949b067a99a50b5e31c21
Ge0 <geoffrey.royer@gmail.com>
2014-01-06 22:43:12 +0100

    get_client_ip working

--- zds/newsletter/views.py
+++ zds/newsletter/views.py
@@ -29,9 +29,10 @@ def add_newsletter(request):
         })
 
 def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
+    #x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    #if x_forwarded_for:
+    #    ip = x_forwarded_for.split(',')[0]
+    #else:
+    #    ip = request.META.get('REMOTE_ADDR')
+    #return ip
+    return request.META.get('REMOTE_ADDR')
