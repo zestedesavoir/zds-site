da60797a2c088d7efd1fe2a48cb608dc9a3746a6
Christophe Gabard <christophe.gabard@gmail.com>
2014-01-09 22:58:57 +0100

    Refactoring emoticons extension

--- zds/utils/templatetags/mkd_ext/emoticons.py
+++ zds/utils/templatetags/mkd_ext/emoticons.py
@@ -1,50 +1,21 @@
-"""
-Emoticon Extension for python-markdown
-======================================
+# Emoticon extension for python-markdown
+# Original version :
+# https://gist.github.com/insin/815656/raw/a68516f1ffc03df465730b3ddef6de0a11b7e9a5/mdx_emoticons.py
+#
+# Patched by cgabard for supporting newer python-markdown version and extend for support multi-extensions
 
-Converts defined emoticon symbols to images, with the symbols as their ``alt``
-text. Requires python-markdown 1.6+.
-
-Basic usage:
-
-    >>> import markdown
-    >>> text = 'Some text with a pre-defined emoticon :p.'
-    >>> markdown.markdown(text, ['emoticons'])
-    '\\n<p>Some text with a pre-defined emoticon <img src="tongue.gif" alt=":p"/>.\\n</p>\\n\\n\\n'
-
-Simple custom settings:
-
-    >>> md = markdown.markdown(text,
-    ...     ['emoticons(BASE_URL=/emoticons/,FILE_EXTENSION=.jpg)']
-    ... )
-    >>> md
-    '\\n<p>Some text with a pre-defined emoticon <img src="/emoticons/tongue.jpg" alt=":p"/>.\\n</p>\\n\\n\\n'
-
-Complex custom settings:
-
-    >>> md = markdown.Markdown(text,
-    ...     extensions=['emoticons'],
-    ...     extension_configs={'emoticons': [
-    ...             ('EMOTICONS', {':p': 'cheeky'}),
-    ...             ('BASE_URL', 'http://supoib-emoticons.com/'),
-    ...             ('FILE_EXTENSION', '.png'),
-    ...         ]})
-    >>> md.toString()
-    '\\n<p>Some text with a pre-defined emoticon <img src="http://supoib-emoticons.com/cheeky.png" alt=":p"/>.\\n</p>\\n\\n\\n'
-
-"""
 import re
-
 import markdown
 from markdown.inlinepatterns import Pattern
 from markdown.util import etree
+
 class EmoticonExtension(markdown.Extension):
     def __init__ (self, configs):
         self.config = {
             'EMOTICONS': [{
                 ":)" : "test.png",
                 }, 'A mapping from emoticon symbols to image names.'],
-        }
+            }
 
         for key, value in configs.iteritems() :
             self.config[key][0] = value
@@ -52,8 +23,7 @@ class EmoticonExtension(markdown.Extension):
     def extendMarkdown(self, md, md_globals):
         self.md = md
         EMOTICON_RE = '(?P<emoticon>%s)' % '|'.join(
-            [re.escape(emoticon) \
-             for emoticon in self.getConfig('EMOTICONS').keys()])
+            [re.escape(emoticon) for emoticon in self.getConfig('EMOTICONS').keys()])
         md.inlinePatterns.add('emoticons', EmoticonPattern(EMOTICON_RE, self),">not_strong")
 
 class EmoticonPattern(Pattern):
@@ -71,6 +41,3 @@ class EmoticonPattern(Pattern):
 def makeExtension(configs=None) :
     return EmoticonExtension(configs=configs)
 
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
