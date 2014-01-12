a64eda8d4d6e4eb1de7fb18e56ea74c96fb08c36
Christophe Gabard <christophe.gabard@gmail.com>
2014-01-12 18:28:40 +0100

    Add range base support for HL in codehilite

--- scripts/UseUpdatedPythonMarkdownVersion.sh
+++ scripts/UseUpdatedPythonMarkdownVersion.sh
@@ -10,6 +10,9 @@ cd python-markdown
 # checkout the correct commit
 git checkout 73fdecaf2cf00d85b7c933f5b8d186d74a80ff2a
 
+# apply patch for supporting range line numbering
+git apply ../../patchs/python-markdown/0001-Add-range-support-for-HL-in-codehilite.patch
+
 # install
 python setup.py install --user
 
