8366085cf932de1772f8be701c1cdf8453ab0e47
Christophe Gabard <christophe.gabard@gmail.com>
2014-01-14 20:55:50 +0100

    Rewrite align, now work on multi lignes

--- zds/utils/templatetags/mkd_ext/align.py
+++ zds/utils/templatetags/mkd_ext/align.py
@@ -12,34 +12,68 @@ class AlignProcessor(BlockProcessor):
     def __init__(self, parser, startExpr, endExpr, contentAlign):
         BlockProcessor.__init__(self, parser)
 
-        self.RE = re.compile(r'(^|\n)' + re.escape(startExpr) + r'(?P<txt>.*?)' + re.escape(endExpr) + r'(\n|$)', re.MULTILINE | re.DOTALL)
+        self.REStart = re.compile(r'(^|\n)' + re.escape(startExpr))
+        self.REEnd   = re.compile(re.escape(endExpr) + r'(\n|$)')
         self.contentAlign = contentAlign
 
     def test(self, parent, block):
-        return bool(self.RE.search(block))
+        return bool(self.REStart.search(block))
 
     def run(self, parent, blocks):
-        block = blocks.pop(0)
-        m = self.RE.search(block)
-        if m:
-            before = block[:m.start()] # All lines before header
-            after = block[m.end():]    # All lines after header
-            if before:
-                self.parser.parseBlocks(parent, [before])
-            sibling = self.lastChild(parent)
-            if sibling and sibling.tag == "div" and "align" in sibling.attrib and sibling.attrib["align"] == self.contentAlign :
-                h= sibling
-                if h.text:
-                    h.text += '\n'
-            else:
-                h = util.etree.SubElement(parent, 'div')
-                h.set("align", self.contentAlign)
-            self.parser.parseChunk(h, m.group('txt'))
-            if after:
-                blocks.insert(0, after)
-        else:
-            blocks.insert(0,block)
+        
+        FirstBlock = blocks[0]
+        m = self.REStart.search(FirstBlock)
+        if not m:
             return False
+        StartBlock = (0, m.start(), m.end())
+        
+        EndBlock = (-1, -1, -1)
+        for i in range(len(blocks)):
+            mEnd = self.REEnd.search(blocks[i])
+            if mEnd:
+                EndBlock = (i, mEnd.start(), mEnd.end())
+                break
+
+        if EndBlock[0] < 0 :
+            return False
+        
+        Before = FirstBlock[:StartBlock[1]]
+        Content = []
+        After = blocks[EndBlock[0]][EndBlock[2]:]
+        for i in range(0,EndBlock[0]+1):
+            blck = blocks.pop(0)
+            
+            if i == StartBlock[0]:
+                startIndex = StartBlock[2]
+            else:
+                startIndex = 0
+            
+            if i == EndBlock[0]:
+                endIndex = EndBlock[1] 
+            else:
+                endIndex = len(blck)
+            
+            Content.append(blck[startIndex: endIndex])
+
+        Content = "\n\n".join(Content)
+
+        if Before:
+            self.parser.parseBlocks(parent, [Before])
+        
+        sibling = self.lastChild(parent)
+        if sibling and sibling.tag == "div" and "align" in sibling.attrib and sibling.attrib["align"] == self.contentAlign :
+            h= sibling
+
+            if h.text:
+                h.text += '\n'
+        else:
+            h = util.etree.SubElement(parent, 'div')
+            h.set("align", self.contentAlign)
+        
+        self.parser.parseChunk(h, Content)
+        
+        if After:
+            blocks.insert(0, After)
 
 class AlignExtension(markdown.extensions.Extension):
     """Adds align extension to Markdown class."""
