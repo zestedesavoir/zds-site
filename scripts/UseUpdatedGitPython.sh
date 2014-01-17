6badc10b36a5bc4fd6259849b482d61e10137451
firm1 <https://firm1@github.com>
2014-01-17 03:39:52 +0100

    Update script

--- scripts/UseUpdatedGitPython.sh
+++ scripts/UseUpdatedGitPython.sh
@@ -15,7 +15,7 @@ git checkout 2fc864356ef1c4a9112dcefbae02a606df59840c
 git apply ../../patchs/GitPython/add-commit-by-username.patch
 
 # install
-python setup.py install --user
+python setup.py install
 
 # cleanning
 cd ..
