7b3e7f575723a2cf98916307045a5319dace893f
Christophe Gabard <christophe.gabard@gmail.com>
2014-01-18 01:11:24 +0100

    Add legend support

--- zds/utils/templatetags/mkd_ext/smartImg.py
+++ zds/utils/templatetags/mkd_ext/smartImg.py
@@ -2,6 +2,8 @@
 
 import markdown
 from markdown.treeprocessors import Treeprocessor
+from markdown.blockprocessors import BlockProcessor
+import re
 from markdown import util
 class SmartImgProcessor(Treeprocessor):
     def __init__(self, parser, configs):
@@ -37,6 +39,41 @@ class SmartImgProcessor(Treeprocessor):
         return root
 
 
+class FigureProcessor(BlockProcessor):
+    def __init__(self, parser, md):
+        BlockProcessor.__init__(self, parser)
+        self.md = md
+        self.imPro = md.inlinePatterns["image_link"]
+        self.RE = re.compile(r'\n(Figure[ ]{0,1})*\:(?P<txtlegend>.*?)(\n|$)')
+
+    def test(self, parent, block):
+        mImg = self.imPro.getCompiledRegExp().match(block)
+        if mImg is None:
+            return False
+        mLeg = self.RE.search(block)
+        if mLeg is None:
+            return False
+        return mImg.start(11) == mLeg.start()
+
+    def run(self, parent, blocks):
+        block = blocks.pop(0)
+        mImg = self.imPro.getCompiledRegExp().match(block)
+        mLeg = self.RE.search(block)
+        before = block[:mImg.end(1)]
+        after = block[mLeg.end():]
+        content = block[mImg.start():mLeg.end()]
+        contentImg = block[mImg.end(1):mLeg.start()]
+        if before:
+            self.parser.parseBlocks(parent, [before])
+        nFig = util.etree.Element("figure")
+        nFigCaption = util.etree.Element("figurecaption")
+        self.parser.parseChunk(nFigCaption, mLeg.group('txtlegend'))
+        self.parser.parseChunk(nFig, contentImg)
+        nFig.append(nFigCaption)
+        parent.append(nFig)
+        if after:
+            blocks.insert(0,after)
+
 class SmartImgExtension(markdown.extensions.Extension):
     def __init__(self, configs={}):
         self.configs = {
@@ -50,6 +87,7 @@ class SmartImgExtension(markdown.extensions.Extension):
     def extendMarkdown(self, md, md_globals):
         md.registerExtension(self)
         md.treeprocessors.add('smart-img', SmartImgProcessor(md.parser,self.configs),"_end")
+        md.parser.blockprocessors.add('figure-processor', FigureProcessor(md.parser,md),"_begin")
 
 def makeExtension(configs={}):
     return SmartImgExtension(configs=configs)
