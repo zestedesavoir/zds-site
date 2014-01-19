2938580f1e1aaf1faafbecc2fb97f79faf3de66c
Alex-D <demodealex@gmail.com>
2014-01-20 00:28:07 +0100

    Revert "Merge master (Fix old merge fail)"
    This reverts commit 8bba4f5a57b84c85f7a5c8530066037675d6de19.

--- zds/member/models.py
+++ zds/member/models.py
@@ -13,7 +13,6 @@ from zds.utils.models import Alert
 from zds.tutorial.models import Tutorial
 from zds.article.models import Article
 
-
 import uuid
 
 
@@ -68,13 +67,8 @@ class Profile(models.Model):
     
     def get_city(self):
         ''' return physical adress by geolocalisation '''
-        try:
-            from django.contrib.gis.geoip import GeoIP
-            g = GeoIP()
-            geo = g.city(self.last_ip_address)
-            return u'{0}, {1}'.format(str(geo['city']), str(geo['country_name']))
-        except:
-            return u'Ankh-Morpork, Discworld'
+        ''' TODO : Windows support '''
+        return "TODO : Windows support"
     
     def get_avatar_url(self):
         '''Avatar URL (using custom URL or Gravatar)'''
