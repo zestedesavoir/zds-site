18d43e6305eab9ccf779e53413e0de5a702d7d0b
GerardPaligot <androwiiid@gmail.com>
2014-02-24 20:14:05 +0100

    Crispy form to add new image in a gallery

--- zds/gallery/forms.py
+++ zds/gallery/forms.py
@@ -16,13 +16,13 @@ class UserGalleryForm(forms.Form):
 class GalleryForm(forms.Form):
     title = forms.CharField(
         label = 'Titre',
-        max_length=80
+        max_length = 80
     )
 
     subtitle = forms.CharField(
         label = 'Sous-titre',
-        max_length=200,
-        required=False
+        max_length = 200,
+        required = False
     )
 
     def __init__(self, *args, **kwargs):
@@ -43,10 +43,37 @@ class GalleryForm(forms.Form):
         )
 
 class ImageForm(forms.Form):
-    gallery = forms.CharField('Gallery', required=False)
+    title = forms.CharField(
+        label = 'Titre',
+        max_length = 80,
+        required = True,
+    )
+
+    legend = forms.CharField(
+        label = u'Légende', 
+        max_length = 150,
+        required = False,
+    )
+
     physical = forms.ImageField(
-        label='Select an image',
-        help_text='max. '+str(settings.IMAGE_MAX_SIZE)+' megabytes'
-    , required=False)
-    title = forms.CharField('Titre')
-    legend = forms.CharField('Legende', required=False)
+        label = u'Sélectionnez votre image',
+        required = True,
+        help_text = 'Taille maximum : ' + str(settings.IMAGE_MAX_SIZE) + ' megabytes'
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(ImageForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-alone'
+        self.helper.form_method = 'post'
+
+        self.helper.layout = Layout(
+            Field('title'),
+            Field('legend'),
+            Field('physical'),
+            ButtonHolder(
+                Submit('submit', u'Créer'),
+                Reset('reset', u'Réinitialiser'),
+                HTML('<a class="btn btn-submit" href="{{ gallery.get_absolute_url }}">Annuler</a>'),
+            ),
+        )
