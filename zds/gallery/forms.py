a8a0c4fedc0aea8d12703252a7ddd15c210e8dad
GerardPaligot <androwiiid@gmail.com>
2014-02-24 02:11:24 +0100

    Crispy form for new gallery view

--- zds/gallery/forms.py
+++ zds/gallery/forms.py
@@ -1,6 +1,12 @@
+# coding: utf-8
+
 from django import forms
 from django.conf import settings
+from django.core.urlresolvers import reverse
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, ButtonHolder, Submit,\
+    Reset, HTML
 
 class UserGalleryForm(forms.Form):
     user= forms.CharField('Membre', required=False)
@@ -8,8 +14,33 @@ class UserGalleryForm(forms.Form):
     mode = forms.CharField('Mode', required=False)
     
 class GalleryForm(forms.Form):
-    title= forms.CharField(max_length=80)
-    subtitle = forms.CharField(max_length=200, required=False)
+    title = forms.CharField(
+        label = 'Titre',
+        max_length=80
+    )
+
+    subtitle = forms.CharField(
+        label = 'Sous-titre',
+        max_length=200,
+        required=False
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(GalleryForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-alone'
+        self.helper.form_action = reverse('zds.gallery.views.new_gallery')
+        self.helper.form_method = 'post'
+
+        self.helper.layout = Layout(
+            Field('title'),
+            Field('subtitle'),
+            ButtonHolder(
+                Submit('submit', u'Créer'),
+                Reset('reset', u'Réinitialiser'),
+                HTML('<a class="btn btn-submit" href="{% url "zds.gallery.views.gallery_list" %}">Annuler</a>'),
+            ),
+        )
 
 class ImageForm(forms.Form):
     gallery = forms.CharField('Gallery', required=False)
