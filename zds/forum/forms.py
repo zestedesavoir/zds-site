6e1db4e30c6f90d085883f6e696ad5cf400a2f18
GerardPaligot <androwiiid@gmail.com>
2014-02-26 17:20:42 +0100

    Crispy form to create a new topic

--- zds/forum/forms.py
+++ zds/forum/forms.py
@@ -2,11 +2,44 @@
 
 from django import forms
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field
+
+from zds.utils.forms import CommonLayoutEditor
+
 
 class TopicForm(forms.Form):
-    title = forms.CharField(max_length=80)
-    subtitle = forms.CharField(max_length=255, required=False)
-    text = forms.CharField(widget=forms.Textarea)
+    title = forms.CharField(
+    	label = 'Titre',
+    	max_length = 80
+    )
+
+    subtitle = forms.CharField(
+    	label = 'Sous-titre',
+    	max_length=255, 
+    	required=False,
+    )
+
+    text = forms.CharField(
+    	label = 'Texte',
+    	widget = forms.Textarea(
+            attrs = {
+                'placeholder': 'Votre message au format Markdown.'
+            }
+        )
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(TopicForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-alone'
+        self.helper.form_method = 'post'
+
+        self.helper.layout = Layout(
+            Field('title', autocomplete='off'),
+            Field('subtitle', autocomplete='off'),
+            CommonLayoutEditor(),
+        )
 
 
 class PostForm(forms.Form):
