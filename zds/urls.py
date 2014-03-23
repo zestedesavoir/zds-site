d9c2319d2937efb0c071617925029b85e1f31083
SpaceFox <ujueseo.yeou@gmail.com>
2014-03-23 18:19:50 +0100

    Catégories + forums OK

--- zds/urls.py
+++ zds/urls.py
@@ -4,9 +4,19 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
+from django.contrib.sitemaps import GenericSitemap
+
+from zds.forum.models import Category, Forum
 
 import pages.views
 import settings
+
+# SiteMap data
+sitemaps = {
+#    'blog': GenericSitemap(info_dict, priority=0.6),
+    'categories':   GenericSitemap({'queryset': Category.objects.all()}, priority=0.7),
+    'forums':       GenericSitemap({'queryset': Forum.objects.filter(group__isnull=True)}, priority=0.7),
+}
 
 
 admin.autodiscover()
@@ -28,6 +38,12 @@ urlpatterns = patterns('',
     url(r'^$', pages.views.home),
 
 )+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# SiteMap URLs
+urlpatterns += patterns('django.contrib.sitemaps.views',
+    (r'^sitemap\.xml$', 'index', {'sitemaps': sitemaps}),
+    (r'^sitemap-(?P<section>.+)\.xml$', 'sitemap', {'sitemaps': sitemaps}),
+)
 
 if settings.SERVE:
     urlpatterns += patterns('',
