c2c1774024274bf5aa875bf6ad55e0ec4e06b1bb
SpaceFox <ujueseo.yeou@gmail.com>
2014-03-25 00:35:58 +0100

    Sanctions transactionnelles

--- zds/member/views.py
+++ zds/member/views.py
@@ -1,22 +1,24 @@
 # coding: utf-8
 from datetime import datetime, timedelta
-from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, SiteProfileNotAvailable
-from django.core.context_processors import csrf
+from django.core.mail import EmailMultiAlternatives
 from django.core.urlresolvers import reverse
 from django.http import Http404
 from django.shortcuts import redirect, get_object_or_404, render_to_response
-from django.template import RequestContext
-from django.template.loader import get_template
-from django.core.mail import EmailMultiAlternatives
 from django.template import Context
+from django.template import RequestContext
 import os
 import uuid
 
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.core.context_processors import csrf
+from django.db import transaction
+from django.template.loader import get_template
 import pygal
+
 from zds.member.decorator import can_read_now, can_write_and_read_now
 from zds.tutorial.models import Tutorial
 from zds.utils import render_template
@@ -25,6 +27,7 @@ from zds.utils.tokens import generate_token
 from .forms import LoginForm, ProfileForm, RegisterForm, ChangePasswordForm, \
     ChangeUserForm, ForgotPasswordForm, NewPasswordForm
 from .models import Profile, TokenForgotPassword, Ban, TokenRegister
+
 
 @can_read_now
 def index(request):
@@ -69,6 +72,7 @@ def details(request, user_name):
 
 @can_write_and_read_now
 @login_required
+@transaction.commit_on_success
 def modify_profile(request, user_pk):
     '''Modifies sanction of a user if there is a POST request'''
     profile = get_object_or_404(Profile, user__pk=user_pk)
