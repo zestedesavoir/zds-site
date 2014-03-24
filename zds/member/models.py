2dcef659798a948ac8593d5cf022b6fdc8fd02ca
SpaceFox <ujueseo.yeou@gmail.com>
2014-03-25 00:35:42 +0100

    Models : ban.type --> 80 chars

--- zds/member/models.py
+++ zds/member/models.py
@@ -186,6 +186,6 @@ class Ban(models.Model):
     user = models.ForeignKey(User, verbose_name='Sanctionné')
     moderator = models.ForeignKey(User, verbose_name='Moderateur',
                                      related_name='bans')
-    type = models.CharField('Type',  max_length=15)
+    type = models.CharField('Type',  max_length=80)
     text = models.TextField('Explication de la sanction')
     pubdate = models.DateTimeField('Date de publication', blank=True, null=True)
