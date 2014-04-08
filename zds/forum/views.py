84a13d1543d51706114fa155241e2d286fa49a6f
SpaceFox <ujueseo.yeou@gmail.com>
2014-04-08 23:13:23 +0200

    Use new transaction framework

--- zds/forum/views.py
+++ zds/forum/views.py
@@ -11,7 +11,7 @@ import json
 
 from django.conf import settings
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from django.db.transaction import commit_on_success
+from django.db import transaction
 from django.http import Http404, HttpResponse
 from django.views.decorators.http import require_POST
 
@@ -157,7 +157,7 @@ def topic(request, topic_pk, topic_slug):
 
 @can_write_and_read_now
 @login_required
-@commit_on_success
+@transaction.atomic
 def new(request):
     '''
     Creates a new topic in a forum
@@ -296,7 +296,7 @@ def edit(request):
 
 @can_write_and_read_now
 @login_required
-@commit_on_success
+@transaction.atomic
 def answer(request):
     '''Adds an answer from a user to a topic'''
     try:
@@ -402,7 +402,7 @@ def answer(request):
 
 @can_write_and_read_now
 @login_required
-@commit_on_success
+@transaction.atomic
 def edit_post(request):
     '''
     Edit the given user's post
