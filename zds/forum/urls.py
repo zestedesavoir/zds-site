87b8ae3898a52c6b896b1e250fff2b38355e8361
firm1 <dralliw@users.noreply.github.com>
2014-04-25 14:38:05 +0200

    absolute urls

--- zds/forum/urls.py
+++ zds/forum/urls.py
@@ -23,34 +23,34 @@ urlpatterns = patterns('',
                            name='topic-feed-atom'),
 
                        # Viewing a thread
-                       url(r'^sujet/nouveau$', views.new),
-                       url(r'^sujet/editer$', views.edit),
-                       url(r'^sujet/deplacer$', views.move_topic),
+                       url(r'^sujet/nouveau$', 'zds.forum.views.new'),
+                       url(r'^sujet/editer$', 'zds.forum.views.edit'),
+                       url(r'^sujet/deplacer$', 'zds.forum.views.move_topic'),
                        url(r'^sujet/(?P<topic_pk>\d+)/(?P<topic_slug>.+)$',
-                           views.topic),
-                       url(r'^sujets/(?P<user_pk>.+)', views.find_topic),
+                           'zds.forum.views.topic'),
+                       url(r'^sujets/(?P<user_pk>.+)', 'zds.forum.views.find_topic'),
 
                        # Message-related
-                       url(r'^message/editer$', views.edit_post),
-                       url(r'^message/nouveau$', views.answer),
-                       url(r'^message/utile$', views.useful_post),
-                       url(r'^message/like$', views.like_post),
-                       url(r'^message/dislike$', views.dislike_post),
-                       url(r'^messages/(?P<user_pk>.+)$', views.find_post),
+                       url(r'^message/editer$', 'zds.forum.views.edit_post'),
+                       url(r'^message/nouveau$', 'zds.forum.views.answer'),
+                       url(r'^message/utile$', 'zds.forum.views.useful_post'),
+                       url(r'^message/like$', 'zds.forum.views.like_post'),
+                       url(r'^message/dislike$', 'zds.forum.views.dislike_post'),
+                       url(r'^messages/(?P<user_pk>.+)$', 'zds.forum.views.find_post),
 
                        # Forum details
                        url(r'^(?P<cat_slug>.+)/(?P<forum_slug>.+)/$',
-                           views.details),
+                           'zds.forum.views.details'),
 
                        # Forums belonging to one category
-                       url(r'^(?P<cat_slug>.+)/$', views.cat_details),
+                       url(r'^(?P<cat_slug>.+)/$', 'zds.forum.views.cat_details'),
 
                        # Home
-                       url(r'^$', views.index),
+                       url(r'^$', 'zds.forum.views.index'),
 
                        # Followed topics
-                       url(r'^notifications$', views.followed_topics),
+                       url(r'^notifications$', 'zds.forum.views.followed_topics'),
 
                        # Moderation
-                       url(r'^resolution_alerte$', views.solve_alert),
+                       url(r'^resolution_alerte$', 'zds.forum.views.solve_alert'),
                        )
