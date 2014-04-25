12f3a6a90f8fde7242830efc72080c90dddf07ae
firm1 <dralliw@users.noreply.github.com>
2014-04-25 14:39:06 +0200

    absolute urls

--- zds/gallery/urls.py
+++ zds/gallery/urls.py
@@ -7,14 +7,14 @@ from . import views
 
 urlpatterns = patterns('',
                        # Viewing a gallery
-                       url(r'^nouveau$', views.new_gallery),
-                       url(r'^modifier$', views.modify_gallery),
+                       url(r'^nouveau$', 'zds.gallery.views.new_gallery'),
+                       url(r'^modifier$', 'zds.gallery.views.modify_gallery'),
                        url(r'^(?P<gal_pk>\d+)/(?P<gal_slug>.+)',
-                           views.gallery_details),
-                       url(r'^$', views.gallery_list),
+                           'zds.gallery.views.gallery_details'),
+                       url(r'^$', 'zds.gallery.views.gallery_list'),
                        url(r'^image/ajouter/(?P<gal_pk>\d+)$',
-                           views.new_image),
-                       url(r'^image/modifier$', views.modify_image),
+                           'zds.gallery.views.new_image'),
+                       url(r'^image/modifier$', 'zds.gallery.views.modify_image'),
                        url(r'^image/editer/(?P<gal_pk>\d+)/(?P<img_pk>\d+)$',
-                           views.edit_image),
+                           'zds.gallery.views.edit_image'),
                        )
