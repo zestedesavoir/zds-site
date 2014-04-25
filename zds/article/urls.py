bf78c07639ffb79810802a64d94f2919fda27093
firm1 <dralliw@users.noreply.github.com>
2014-04-25 14:21:59 +0200

    Update urls.py

--- zds/article/urls.py
+++ zds/article/urls.py
@@ -15,27 +15,27 @@ urlpatterns = patterns('',
     # TODO: Handle redirect
 
     url(r'^voir/(?P<article_pk>\d+)/(?P<article_slug>.+)$',
-        'article.views.deprecated_view_redirect'),
-    url(r'^off/(?P<article_pk>\d+)/(?P<article_slug>.+)$', 'article.views.view'),
-    url(r'^(?P<article_pk>\d+)/(?P<article_slug>.+)$', 'article.views.view_online'),
-    url(r'^nouveau$', 'article.views.new'),
-    url(r'^editer$', 'article.views.edit'),
-    url(r'^modifier$', 'article.views.modify'),
-    url(r'^recherche/(?P<name>.+)$', 'article.views.find_article'),
+        'zds.article.views.deprecated_view_redirect'),
+    url(r'^off/(?P<article_pk>\d+)/(?P<article_slug>.+)$', 'zds.article.views.view'),
+    url(r'^(?P<article_pk>\d+)/(?P<article_slug>.+)$', 'zds.article.views.view_online'),
+    url(r'^nouveau$', 'zds.article.views.new'),
+    url(r'^editer$', 'zds.article.views.edit'),
+    url(r'^modifier$', 'zds.article.views.modify'),
+    url(r'^recherche/(?P<name>.+)$', 'zds.article.views.find_article'),
 
 
-    url(r'^$', 'article.views.index'),
-    url(r'^telecharger/$', 'article.views.download'),
-    url(r'^historique/(?P<article_pk>\d+)/(?P<article_slug>.+)/$', 'article.views.history'),
+    url(r'^$', 'zds.article.views.index'),
+    url(r'^telecharger/$', 'zds.article.views.download'),
+    url(r'^historique/(?P<article_pk>\d+)/(?P<article_slug>.+)/$', 'zds.article.views.history'),
     
     #Validation
-    url(r'^validation/$', 'article.views.list_validation'),
-    url(r'^validation/reserver/(?P<validation_pk>\d+)/$', 'article.views.reservation'),
-    url(r'^validation/historique/(?P<article_pk>\d+)/$', 'article.views.history_validation'),
+    url(r'^validation/$', 'zds.article.views.list_validation'),
+    url(r'^validation/reserver/(?P<validation_pk>\d+)/$', 'zds.article.views.reservation'),
+    url(r'^validation/historique/(?P<article_pk>\d+)/$', 'zds.article.views.history_validation'),
     
     #Reactions
-    url(r'^message/editer$', 'article.views.edit_reaction'),
-    url(r'^message/nouveau$', 'article.views.answer'),
-    url(r'^message/like$', 'article.views.like_reaction'),
-    url(r'^message/dislike$', 'article.views.dislike_reaction'),
+    url(r'^message/editer$', 'zds.article.views.edit_reaction'),
+    url(r'^message/nouveau$', 'zds.article.views.answer'),
+    url(r'^message/like$', 'zds.article.views.like_reaction'),
+    url(r'^message/dislike$', 'zds.article.views.dislike_reaction'),
 )
