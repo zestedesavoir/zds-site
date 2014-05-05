8cdb805732cdfcab0320c2e653e771f1a05147e0
firm1 <firm1@github.com>
2014-05-05 22:50:02 +0200

    add mail/mp for abn/register

--- zds/utils/mps.py
+++ zds/utils/mps.py
@@ -1,11 +1,14 @@
+# coding: utf-8
+
 from datetime import datetime
 from zds.mp.models import PrivateTopic, PrivatePost
 from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
 from django.template import Context
 from django.template.loader import get_template
+from zds.utils.templatetags.emarkdown import emarkdown
 
-def send_mp(author, users, title, subtitle, text, send_by_mail=True, leave=True):
+def send_mp(author, users, title, subtitle, text, send_by_mail=True, leave=True, direct=False):
     """
     Send MP at members
     """
@@ -36,32 +39,56 @@ def send_mp(author, users, title, subtitle, text, send_by_mail=True, leave=True)
     
         # send email
     if send_by_mail:
-        subject = "ZDS - MP: " + n_topic.title
-        from_email = 'ZesteDeSavoir <noreply@zestedesavoir.com>'
-        for part in users:
-            message_html = get_template('email/mp.html').render(
-                Context({
-                    'username': part.username,
-                    'url': settings.SITE_URL + n_topic.get_absolute_url(),
-                    'author': author.username
-                })
-            )
-            message_txt = get_template('email/mp.txt').render(
-                Context({
-                    'username': part.username,
-                    'url': settings.SITE_URL + n_topic.get_absolute_url(),
-                    'author': author.username
-                })
-            )
-    
-            msg = EmailMultiAlternatives(
-                subject, message_txt, from_email, [
-                    part.email])
-            msg.attach_alternative(message_html, "text/html")
-            try :
-                msg.send()
-            except:
-                msg = None
+        if direct :
+            subject = "ZDS : " + n_topic.title
+            from_email = 'ZesteDeSavoir <noreply@zestedesavoir.com>'
+            for part in users:
+                message_html = get_template('email/mp_direct.html').render(
+                    Context({
+                        'msg': emarkdown(text)
+                    })
+                )
+                message_txt = get_template('email/mp_direct.txt').render(
+                    Context({
+                        'msg': text
+                    })
+                )
+        
+                msg = EmailMultiAlternatives(
+                    subject, message_txt, from_email, [
+                        part.email])
+                msg.attach_alternative(message_html, "text/html")
+                try :
+                    msg.send()
+                except:
+                    msg = None
+        else:
+            subject = "ZDS - MP: " + n_topic.title
+            from_email = 'ZesteDeSavoir <noreply@zestedesavoir.com>'
+            for part in users:
+                message_html = get_template('email/mp.html').render(
+                    Context({
+                        'username': part.username,
+                        'url': settings.SITE_URL + n_topic.get_absolute_url(),
+                        'author': author.username
+                    })
+                )
+                message_txt = get_template('email/mp.txt').render(
+                    Context({
+                        'username': part.username,
+                        'url': settings.SITE_URL + n_topic.get_absolute_url(),
+                        'author': author.username
+                    })
+                )
+        
+                msg = EmailMultiAlternatives(
+                    subject, message_txt, from_email, [
+                        part.email])
+                msg.attach_alternative(message_html, "text/html")
+                try :
+                    msg.send()
+                except:
+                    msg = None
     if leave:
         move = n_topic.participants.first()
         n_topic.author = move
