fc51215bb220ff78ab2396d1eec7b96395aa3638
firm1 <firm1@github.com>
2014-05-09 12:03:20 +0200

    Staff can update profile member

--- zds/member/urls.py
+++ zds/member/urls.py
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
                        url(r'^actions$', 'zds.member.views.actions'),
 
                        url(r'^parametres/profil$', 'zds.member.views.settings_profile'),
+                       url(r'^parametres/mini_profil/(?P<user_name>.+)$', 'zds.member.views.settings_mini_profile'),
                        url(r'^parametres/compte$', 'zds.member.views.settings_account'),
                        url(r'^parametres/user$', 'zds.member.views.settings_user'),
 
