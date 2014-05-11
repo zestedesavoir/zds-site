010ff9eb0934fe6e3b952ca6b9df74fc543ed28f
ShigeruM <matthieu.bonan@gmail.com>
2014-05-11 17:21:35 +0200

    Add last visit field

--- zds/settings.py
+++ zds/settings.py
@@ -141,6 +141,7 @@ MIDDLEWARE_CLASSES = (
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'zds.utils.ThreadLocals',
+    'zds.middlewares.SetLastVisitMiddleware.SetLastVisitMiddleware',
 )
 
 ROOT_URLCONF = 'zds.urls'
