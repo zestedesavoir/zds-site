d5f3dcb2c9c743b890a4f118cd39b1f9ac7282c5
ShigeruM <matthieu.bonan@gmail.com>
2014-05-11 19:32:21 +0200

    Delete deprecated get_profile() (details: http://stackoverflow.com/questions/20613315/get-rid-of-get-profile-in-a-migration-to-django-1-6)

--- zds/member/decorator.py
+++ zds/member/decorator.py
@@ -10,7 +10,7 @@ def can_read_now(func):
     """Decorator to check that the user can read now."""
     def _can_read_now(request, *args, **kwargs):
         try:
-            profile = Profile.objects.get(user__pk=request.user.pk)
+            profile = request.user.profile
         except:
             # The user is a visitor
             profile = None
@@ -28,7 +28,7 @@ def can_write_and_read_now(func):
     """Decorator to check that the user can read and write now."""
     def _can_write_and_read_now(request, *args, **kwargs):
         try:
-            profile = Profile.objects.get(user__pk=request.user.pk)
+            profile = request.user.profile
 
         except:
             # The user is a visitor
