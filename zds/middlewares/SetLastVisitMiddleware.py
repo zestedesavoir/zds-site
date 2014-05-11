8e4324d7d5f0d1eeb0198761d5c1ba6002f5a05f
ShigeruM <matthieu.bonan@gmail.com>
2014-05-11 17:32:44 +0200

    Clean SetLastVisitMiddleware

--- zds/middlewares/SetLastVisitMiddleware.py
+++ zds/middlewares/SetLastVisitMiddleware.py
@@ -3,9 +3,8 @@ import datetime
 
 class SetLastVisitMiddleware(object):
     def process_response(self, request, response):
+        # Update last visit time after request finished processing.
         if request.user.is_authenticated():
-            # Update last visit time after request finished processing.
-            #User.objects.filter(pk=request.user.pk).update(last_visit=now())
             profile = User.objects.get(pk=request.user.pk).get_profile()
             if profile.last_visit is None:
                 profile.last_visit = datetime.datetime.now()
