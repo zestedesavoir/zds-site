d381baf06da63e5b0d3cb1787f36ec6c32f855f6
Alex-D <demodealex@gmail.com>
2014-06-01 01:12:14 +0200

    Amélioration de la gestion des polices

--- zds/settings.py
+++ zds/settings.py
@@ -129,6 +129,7 @@ PIPELINE_JS = {
             'js/custom/keyboard-navigation.js',
             'js/custom/message-hidden.js',
             'js/custom/spoiler.js',
+            'js/custom/webfont.js',
         ),
         'output_filename': 'js/main.js'
     }
