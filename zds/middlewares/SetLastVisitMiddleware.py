6c93095b4db60fe45a470ef3053ada45b01dcf63
firm1 <dralliw@users.noreply.github.com>
2014-05-31 11:41:16 +0200

    corrige les problèmes en cas de non connexion

--- zds/middlewares/SetLastVisitMiddleware.py
+++ zds/middlewares/SetLastVisitMiddleware.py
@@ -1,11 +1,19 @@
 import datetime
-
+from zds.utils import get_current_user
 
 class SetLastVisitMiddleware(object):
 
     def process_response(self, request, response):
         # Update last visit time after request finished processing.
-        if request.user.is_authenticated():
+        try :
+            if request.user.is_authenticated():
+                user = request.user
+            else:
+                user = None
+        except:
+            user = None
+
+        if user is not None:
             profile = request.user.profile
             if profile.last_visit is None:
                 profile.last_visit = datetime.datetime.now()
