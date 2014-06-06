f6b9dbb6d779c5df5046b934b5196c63d476931a
Quentin Gliech <quentingliech@gmail.com>
2014-06-06 09:46:02 +0200

    Ajout de `gulp watch` + déplacement des fichiers JS

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,23 +1,25 @@
 var gulp = require("gulp"),
     $ = require("gulp-load-plugins")(),
     path = require("path");
+    lr = require("tiny-lr"),
+    server = lr();
 
 var paths = {
-  scripts: "assets/js/main/**/*.js",
+  scripts: "assets/js/**/*.js",
   images: "assets/images/**/*.{png,ico}",
   smileys: "assets/smileys/*",
-  copy: ["assets/{css,js}/newsletter.{css,js}"],
+  copy: ["assets/misc/{css,js}/newsletter.{css,js}"],
+  stylesheet: "assets/scss/main.scss",
   compass: {
     sass: "scss",
     images: "images",
-    css: "css",
-    project: path.join(__dirname, "assets/"),
-    relative: false
+    css: ".sass-css",
+    project: path.join(__dirname, "assets/")
   }
 };
 
 gulp.task("clean-compass", function() {
-  return gulp.src(["assets/css/main.css", "assets/images/sprite{,@2x}-*.png"])
+  return gulp.src(["assets/.sass-css", "assets/images/sprite{,@2x}-*.png"])
     .pipe($.clean());
 });
 
@@ -38,7 +40,7 @@ gulp.task("script", ["test"], function() {
 });
 
 gulp.task("stylesheet", function() {
-  return gulp.src("assets/scss/main.scss")
+  return gulp.src(paths.stylesheet)
     .pipe($.compass({
       project: paths.compass.project,
       css: paths.compass.css,
@@ -53,14 +55,14 @@ gulp.task("stylesheet", function() {
 
 gulp.task("images", ["stylesheet"], function() {
   return gulp.src(paths.images)
-    .pipe($.imagemin())
+    .pipe($.cache($.imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
     .pipe($.size())
     .pipe(gulp.dest("dist/images"));
 });
 
 gulp.task("smileys", function() {
   return gulp.src(paths.smileys)
-    .pipe($.imagemin())
+    .pipe($.cache($.imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
     .pipe($.size())
     .pipe(gulp.dest("dist/smileys"));
 });
@@ -79,11 +81,26 @@ gulp.task("vendors", function() {
     .pipe(gulp.dest("dist/js"));
 });
 
+gulp.task("merge-scripts", ["script", "vendors"], function() {
+  return gulp.src("dist/js/{vendors,main}.min.js")
+    .pipe($.concat("all.min.js"))
+    .pipe($.size())
+    .pipe(gulp.dest("dist/js/"));
+});
+
+gulp.task("watch", function() {
+  gulp.watch(paths.script, ["script"]);
+  gulp.watch(paths.copy, ["copy"]);
+  gulp.watch(paths.smiley, ["smileys"]);
+  gulp.watch(paths.imafes, ["images"]);
+  gulp.watch(paths.stylesheet, ["stylesheet"]);
+});
+
 gulp.task("test", function() {
   return gulp.src(paths.scripts)
     .pipe($.jshint());
     //.pipe($.jshint.reporter("jshint-stylish")); <- quiet, since there is many errors
-})
+});
 
 gulp.task("copy", function() {
   return gulp.src(paths.copy)
@@ -91,6 +108,6 @@ gulp.task("copy", function() {
 });
 
 
-gulp.task("build", ["smileys", "images", "stylesheet", "vendors", "script", "copy"]);
+gulp.task("build", ["smileys", "images", "stylesheet", "vendors", "script", "merge-scripts", "copy"]);
 
 gulp.task("default", ["build"]);
