6ea0fad5f29dbe8e07d4e34c40203f21612c2a7f
alex <mathieu.nerv@gmail.com>
2014-06-06 16:37:50 +0200

    ajout de tests pour les views mp

---
+++ zds/mp/tests/tests_views.py
@@ -0,0 +1,205 @@
+# coding: utf-8
+
+import urllib
+
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from zds.member.factories import ProfileFactory
+from zds.mp.factories import PrivateTopicFactory, PrivatePostFactory
+from zds.mp.models import PrivateTopic
+from zds.utils import slugify
+
+
+class IndexViewTest(TestCase):
+
+    def setUp(self):
+        self.profile1 = ProfileFactory()
+        self.profile2 = ProfileFactory()
+        self.topic1 = PrivateTopicFactory(author=self.profile1.user)
+        self.topic1.participants.add(self.profile2.user)
+        self.post1 = PrivatePostFactory(
+            privatetopic=self.topic1,
+            author=self.profile1.user,
+            position_in_topic=1)
+
+        self.post2 = PrivatePostFactory(
+            privatetopic=self.topic1,
+            author=self.profile2.user,
+            position_in_topic=2)
+
+    def test_denies_anonymous(self):
+        response = self.client.get(reverse('zds.mp.views.index'), follow=True)
+        self.assertRedirects(
+            response,
+            reverse('zds.member.views.login_view')
+            + '?next=' + urllib.quote(reverse('zds.mp.views.index'), ''))
+
+    def test_success_delete_topic_no_participants(self):
+        topic = PrivateTopicFactory(author=self.profile1.user)
+        login_check = self.client.login(
+            username=self.profile1.user.username,
+            password='hostel77'
+        )
+        self.assertTrue(login_check)
+        self.assertEqual(1, PrivateTopic.objects.filter(pk=topic.pk).count())
+
+        response = self.client.post(
+            reverse('zds.mp.views.index'),
+            {
+                'delete': '',
+                'items': [topic.pk]
+            }
+        )
+
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(0, PrivateTopic.objects.filter(pk=topic.pk).count())
+
+    def test_success_delete_topic_as_author(self):
+
+        login_check = self.client.login(
+            username=self.profile1.user.username,
+            password='hostel77'
+        )
+        self.assertTrue(login_check)
+
+        response = self.client.post(
+            reverse('zds.mp.views.index'),
+            {
+                'delete': '',
+                'items': [self.topic1.pk]
+            }
+        )
+
+        self.assertEqual(200, response.status_code)
+        topic = PrivateTopic.objects.get(pk=self.topic1.pk)
+        self.assertEqual(self.profile2.user, topic.author)
+        self.assertNotIn(self.profile1.user, topic.participants.all())
+        self.assertNotIn(self.profile2.user, topic.participants.all())
+
+    def test_success_delete_topic_as_participant(self):
+
+        login_check = self.client.login(
+            username=self.profile2.user.username,
+            password='hostel77'
+        )
+        self.assertTrue(login_check)
+
+        response = self.client.post(
+            reverse('zds.mp.views.index'),
+            {
+                'delete': '',
+                'items': [self.topic1.pk]
+            }
+        )
+
+        self.assertEqual(200, response.status_code)
+
+        topic = PrivateTopic.objects.get(pk=self.topic1.pk)
+        self.assertNotEqual(self.profile2.user, topic.author)
+        self.assertNotIn(self.profile1.user, topic.participants.all())
+        self.assertNotIn(self.profile2.user, topic.participants.all())
+
+    def test_fail_delete_topic_not_belong_to_user(self):
+        topic = PrivateTopicFactory(author=self.profile1.user)
+
+        self.assertEqual(1, PrivateTopic.objects.filter(pk=topic.pk).count())
+
+        login_check = self.client.login(
+            username=self.profile2.user.username,
+            password='hostel77'
+        )
+        self.assertTrue(login_check)
+
+        self.client.post(
+            reverse('zds.mp.views.index'),
+            {
+                'delete': '',
+                'items': [topic.pk]
+            }
+        )
+
+        self.assertEqual(1, PrivateTopic.objects.filter(pk=topic.pk).count())
+
+
+class TopicViewTest(TestCase):
+
+    def setUp(self):
+        self.profile1 = ProfileFactory()
+        self.profile2 = ProfileFactory()
+        self.topic1 = PrivateTopicFactory(author=self.profile1.user)
+        self.topic1.participants.add(self.profile2.user)
+        self.post1 = PrivatePostFactory(
+            privatetopic=self.topic1,
+            author=self.profile1.user,
+            position_in_topic=1)
+
+        self.post2 = PrivatePostFactory(
+            privatetopic=self.topic1,
+            author=self.profile2.user,
+            position_in_topic=2)
+
+    def test_denies_anonymous(self):
+        response = self.client.get(
+            reverse(
+                'zds.mp.views.topic',
+                args=[self.topic1.pk, slugify(self.topic1.title)]),
+            follow=True)
+        self.assertRedirects(
+            response,
+            reverse('zds.member.views.login_view')
+            + '?next=' + urllib.quote(reverse(
+                'zds.mp.views.topic',
+                args=[self.topic1.pk, slugify(self.topic1.title)]), ''))
+
+    def test_fail_topic_no_exist(self):
+
+        login_check = self.client.login(
+            username=self.profile1.user.username,
+            password='hostel77'
+        )
+        self.assertTrue(login_check)
+
+        response = self.client.get(reverse(
+            'zds.mp.views.topic',
+            args=[12, 'test']))
+        self.assertEqual(404, response.status_code)
+
+    def test_fail_topic_no_permission(self):
+        topic = PrivateTopicFactory(author=self.profile1.user)
+
+        login_check = self.client.login(
+            username=self.profile2.user.username,
+            password='hostel77'
+        )
+        self.assertTrue(login_check)
+
+        response = self.client.get(reverse(
+            'zds.mp.views.topic',
+            args=[topic.pk, 'test']),
+            follow=True
+        )
+
+        self.assertEqual(403, response.status_code)
+
+    def test_fail_topic_slug(self):
+        login_check = self.client.login(
+            username=self.profile1.user.username,
+            password='hostel77'
+        )
+        self.assertTrue(login_check)
+
+        self.client.get(reverse(
+            'zds.mp.views.topic',
+            args=[self.topic1.pk, 'test']),
+            follow=True
+        )
+
+        self.assertRedirects(
+            reverse(
+                'zds.mp.views.topic',
+                args=[self.topic1.pk, 'test']),
+            reverse(
+                'zds.mp.views.topic',
+                args=[self.topic1.pk, slugify(self.topic1.title)]),
+        )
