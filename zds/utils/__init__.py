476316f9c1080199c92dce211be1ce6b70b092d1
firm1 <firm1@github.com>
2014-06-07 14:32:39 +0200

    ajoute sur la home la version du dépot

--- zds/utils/__init__.py
+++ zds/utils/__init__.py
@@ -3,6 +3,8 @@
 from django.template import RequestContext, defaultfilters
 
 from django.shortcuts import render_to_response
+from git import *
+from django.conf import settings
 
 
 try:
@@ -20,6 +22,13 @@ def get_current_user():
 def get_current_request():
     return getattr(_thread_locals, 'request', None)
 
+def get_git_version():
+    repo = Repo(settings.SITE_ROOT)
+    branch = repo.active_branch
+    commit = repo.head.commit.tree.hexsha
+    v = u"{0}/{1}".format(branch, commit[:5])
+        
+    return {'name':v, 'url':u'https://github.com/zestedesavoir/zds-site/tree/{0}'.format(commit)}
 
 class ThreadLocals(object):
 
@@ -29,6 +38,7 @@ class ThreadLocals(object):
 
 
 def render_template(tmpl, dct=None):
+    dct['git_version']=get_git_version()
     return render_to_response(
         tmpl, dct, context_instance=RequestContext(get_current_request()))
 
