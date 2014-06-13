cae075383f680b7d4b2d028373cf7c52b3c3a19d
artragis <artragis@gmail.com>
2014-06-13 22:11:55 +0200

    ajoute un test unitaire sur l'ajout d'une partie qui s'appelle conclusion

--- zds/tutorial/tests.py
+++ zds/tutorial/tests.py
@@ -782,7 +782,7 @@ class MiniTutorialTests(TestCase):
 
         mail.outbox = []
 
-    def test_fix_878_extract_named_intro(self):
+    def test_fix_878_extract_named_introduction(self):
         """test the use of an extract named introduction"""
         
         self.client.login(username=self.user_author,
@@ -806,6 +806,30 @@ class MiniTutorialTests(TestCase):
         self.assertTrue(os.path.isfile(intro_path))
         self.assertTrue(os.path.isfile(extract_path))
 
+    def test_fix_878_extract_named_conclusion(self):
+        """test the use of an extract named introduction"""
+        
+        self.client.login(username=self.user_author,
+            password='hostel77')
+        
+        result = self.client.post(
+            reverse( 'zds.tutorial.views.add_extract') +
+            '?chapitre={0}'.format(
+                self.chapter.pk),
+            
+            {
+            'title' : "Conclusion",
+            'text' : u"Le contenu de l'extrait"
+            })
+        self.assertEqual(result.status_code, 302)
+        tuto = Tutorial.objects.get(pk=self.minituto.pk)
+        self.assertEqual(Extract.objects.all().count(),1)
+        ccl_path = os.path.join(tuto.get_path(),"conclusion.md")
+        extract_path =  Extract.objects.get(pk=1).get_path()
+        self.assertNotEqual(ccl_path,extract_path)
+        self.assertTrue(os.path.isfile(ccl_path))
+        self.assertTrue(os.path.isfile(extract_path))
+        
     def test_add_note(self):
         """To test add note for tutorial."""
         user1 = ProfileFactory().user
