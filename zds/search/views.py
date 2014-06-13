ea451c4df7250c199b1ac20261ec700d09268a63
Quentin Gliech <quentingliech@gmail.com>
2014-06-14 01:11:50 +0200

    Fix pagination + indent

--- zds/search/views.py
+++ zds/search/views.py
@@ -1,10 +1,27 @@
 # coding: utf-8
 
+from django.shortcuts import render_to_response
 from haystack.views import SearchView
+from zds.utils.paginator import paginator_range
 
 class CustomSearchView(SearchView):
-    def extra_context(self):
+    def create_response(self):
+        (paginator, page) = self.build_page()
+
         page_nbr = int(self.request.GET.get('page', 1))
-        return {
-            'nb': page_nbr
+
+        context = {
+            'query': self.query,
+            'form': self.form,
+            'page': page,
+            'pages': paginator_range(page_nbr, paginator.num_pages),
+            'nb': page_nbr,
+            'paginator': paginator,
+            'suggestion': None,
         }
+
+        if self.results and hasattr(self.results, 'query') and self.results.query.backend.include_spelling:
+            context['suggestion'] = self.form.get_suggestion()
+
+        context.update(self.extra_context())
+        return render_to_response(self.template, context, context_instance=self.context_class(self.request))
