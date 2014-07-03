680a13f6d7be43f611a134505f7faea08ab62257
Alex-D <demodealex@gmail.com>
2014-07-03 23:32:15 +0200

    Redesign du messages des cookies

--- assets/js/accessibility-links.js
+++ assets/js/accessibility-links.js
@@ -7,11 +7,11 @@
 (function($){
     "use strict";
     
-    $(".accessibility a").on("focus", function(){
+    $("#accessibility a").on("focus", function(){
         $(".dropdown:visible").parent().find(".active").removeClass("active");
-        $(".accessibility").addClass("focused");
+        $("#accessibility").addClass("focused");
     });
-    $(".accessibility a").on("blur", function(){
-        $(".accessibility").removeClass("focused");
+    $("#accessibility a").on("blur", function(){
+        $("#accessibility").removeClass("focused");
     });
 })(jQuery);
