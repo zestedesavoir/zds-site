c5c92bfd800c2abe39a387921084df3ed4de4e3c
Alex-D <demodealex@gmail.com>
2014-07-04 02:00:41 +0200

    Ajout fichier cookies.js

---
+++ assets/js/cookies-banner.js
@@ -0,0 +1,30 @@
+(function(window, document, undefined) {
+    // Google Analytics ID
+    var gaProperty = "UA-27730868-1";
+
+    var $banner = $("#cookies-banner");
+
+    // Disable tracking if the opt-out cookie exists.
+    var disableStr = "ga-disable-" + gaProperty;
+    if(document.cookie.indexOf("hasconsent=true") > -1){
+        window[disableStr] = false;
+    } else if(document.cookie.indexOf("hasconsent=false") > -1){
+        window[disableStr] = true;
+    } else {
+        $banner.show();
+    }
+
+    $("#reject-cookies").on("click", function(){
+        hasconsent(false);
+    });
+
+    $("#accept-cookies").on("click", function(){
+        hasconsent(true);
+    });
+
+    function hasconsent(hasconsent){
+        document.cookie = "hasconsent="+hasconsent+"; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/";
+        window[disableStr] = !hasconsent;
+        $banner.slideUp(200);
+    }
+})(window, document);
