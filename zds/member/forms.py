904adf6c29734a98f3cb324ce600292d627050c6
Eskimon <eskimon@outlook.com>
2014-07-05 11:41:53 +0200

    Autopopuplate du field username pour le form de connexion

--- zds/member/forms.py
+++ zds/member/forms.py
@@ -73,11 +73,13 @@ class LoginForm(forms.Form):
         widget=forms.CheckboxSelectMultiple,
     )
 
-    def __init__(self, next=None, *args, **kwargs):
+    def __init__(self, next=None, user=None, *args, **kwargs):
         super(LoginForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_action = reverse('zds.member.views.login_view')
         self.helper.form_method = 'post'
+        if user is not None:
+            self.fields['username'].initial = user.username
 
         self.helper.layout = Layout(
             Field('username'),
