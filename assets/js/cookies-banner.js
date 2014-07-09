98755d2f92334740a7fea2a1cf0ff885af48158a
Alex-D <demodealex@gmail.com>
2014-07-09 18:08:08 +0200

    Nettoye les différents JS et les templates

--- assets/js/cookies-banner.js
+++ assets/js/cookies-banner.js
@@ -5,18 +5,22 @@
     var $banner = $("#cookies-banner");
 
     // Disable tracking if the opt-out cookie exists.
-    var disableStr = "ga-disable-" + gaProperty;
     if(document.cookie.indexOf("hasconsent=true") > -1){
-        window[disableStr] = false;
-    } else if(document.cookie.indexOf("hasconsent=false") > -1){
-        window[disableStr] = true;
-    } else {
+        $("#gtm").after(
+            "<script>
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-WH7642');
+            </script>"
+        );
+    } else if(document.cookie.indexOf("hasconsent=false") === -1){
         $banner.show();
     }
 
     function sethasconsent(hasconsent){
         document.cookie = "hasconsent="+hasconsent+"; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/";
-        window[disableStr] = !hasconsent;
         $banner.slideUp(200);
     }
 
