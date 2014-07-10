e9a27404ef03321e385deca7aecae980522dd765
Alex-D <demodealex@gmail.com>
2014-07-10 02:48:33 +0200

    Nettoyage et corrections des JS et templates

--- assets/js/cookies-banner.js
+++ assets/js/cookies-banner.js
@@ -1,34 +1,44 @@
-(function(window, document, undefined) {
-    // Google Analytics ID
-    var gaProperty = "UA-27730868-1";
+/* ===== Zeste de Savoir ====================================================
+   Authors: Sandhose
+            Alex-D / Alexandre Demode
+   ---------------------------------
+   Manage tracking cookies message
+   ========================================================================== */
 
+(function(window, document, undefined) {
     var $banner = $("#cookies-banner");
 
     // Disable tracking if the opt-out cookie exists.
-    if(document.cookie.indexOf("hasconsent=true") > -1){
-        $("#gtm").after(
-            "<script>
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-WH7642');
-            </script>"
-        );
-    } else if(document.cookie.indexOf("hasconsent=false") === -1){
-        $banner.show();
+    function checkHasConsent(){
+        if(document.cookie.indexOf("hasconsent=true") > -1){
+            $("#gtm").after(
+                "<script>" +
+                    "dataLayer = [{'gaTrackingId': 'UA-27730868-1'}];" +
+                    "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':" +
+                    "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0]," +
+                    "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=" +
+                    "'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);" +
+                    "})(window,document,'script','dataLayer','GTM-WH7642');" +
+                "</script>"
+            );
+        } else if(document.cookie.indexOf("hasconsent=false") === -1){
+            $banner.show();
+        }
     }
+    checkHasConsent();
 
-    function sethasconsent(hasconsent){
+
+    function setHasConsent(hasconsent){
         document.cookie = "hasconsent="+hasconsent+"; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/";
         $banner.slideUp(200);
     }
 
     $("#reject-cookies").on("click", function(){
-        sethasconsent(false);
+        setHasConsent(false);
     });
 
     $("#accept-cookies").on("click", function(){
-        sethasconsent(true);
+        setHasConsent(true);
+        checkHasConsent();
     });
 })(window, document);
--- assets/js/mathjax-config.js
+++
@@ -1,9 +0,0 @@
-MathJax.Hub.Config({
-    tex2jax: {
-        inlineMath: [['$', '$']],
-        displayMath: [['$$','$$']],
-        processEscapes: true,
-    },
-    TeX: { extensions: ["color.js", "cancel.js", "enclose.js", "bbox.js", "mathchoice.js", "newcommand.js", "verb.js", "unicode.js", "autobold.js"] },
-    messageStyle: "none",
-});
