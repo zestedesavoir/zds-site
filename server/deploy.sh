d8f2067a654a850c39d75f643931f672c47c31d6
SpaceFox <spacefox@neuf.fr>
2014-07-11 22:48:12 +0200

    Issue #5: Script de base pour déployer en prod - sans doute très améliorable

---
+++ server/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Zeste de Savoir deployment script
+#
+# Deploys specified version of Zeste de Savoir
+
+if [ "$(whoami)" != "zds" ]; then
+	echo "This script must be run by zds user" >&2
+	exit 1
+fi
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 <tag name>" >&2
+	exit 1
+fi
+cd /opt/zdsenv/ZesteDeSavoir/
+
+# Switch to new tag
+git fetch
+# -b is required to have version data in footer
+git checkout -b $1
+
+# Compute front stuff
+source /usr/local/nvm/nvm.sh
+gulp pack
+gulp build
+
+# Update application data
+source ../bin/activate
+pip install --upgrade -r requirements.txt
+python manage.py migrate
+deactivate
+
+# Restart zds
+sudo supervisorctl restart zds
