8e1df2cb62b4246f08bcb3ede2f33d4f5c4118cd
coma94 <e.web@moutot.eu>
2014-07-11 11:46:35 +0200

    MP global incluant tous les auteur d'un tuto/article

--- zds/tutorial/views.py
+++ zds/tutorial/views.py
@@ -297,33 +297,30 @@ def reject_tutorial(request):
         messages.info(request, _(u"Le tutoriel a bien été refusé."))
 
         # send feedback
-
-        for author in tutorial.authors.all():
-            comment_reject = '\n'.join(['> '+line for line in validation.comment_validator.split('\n')])
-            msg = (
-                _(u'Désolé **{0}**, ton zeste **{1}** n\'a malheureusement '
-                  u'pas passé l’étape de validation. Mais ne désespère pas, '
-                  u'certaines corrections peuvent sûrement être faites pour '
-                  u'l’améliorer et repasser la validation plus tard. '
-                  u'Voici le message que [{2}]({3}), ton validateur t\'a laissé\n\n> {4}\n\n'
-                  u'N\'hésite pas à lui envoyer un petit message pour discuter '
-                  u'de la décision ou demander plus de détails si tout cela te '
-                  u'semble injuste ou manque de clarté.').format(
-                      author.username,
-                      tutorial.title,
-                      validation.validator.username,
-                      settings.ZDS_APP['site']['url'] + validation.validator.profile.get_absolute_url(),
-                      comment_reject))
-            bot = get_object_or_404(User, username=settings.ZDS_APP['member']['bot_account'])
-            send_mp(
-                bot,
-                [author],
-                _(u"Refus de Validation : {0}").format(tutorial.title),
-                "",
-                msg,
-                True,
-                direct=False,
-            )
+        msg = (
+            _(u'Désolé, le zeste **{0}** n\'a malheureusement '
+              u'pas passé l’étape de validation. Mais ne désespère pas, '
+              u'certaines corrections peuvent surement être faite pour '
+              u'l’améliorer et repasser la validation plus tard. '
+              u'Voici le message que [{1}]({4}{2}), ton validateur t\'a laissé:\n\n`{3}`\n\n'
+              u'N\'hésite pas a lui envoyer un petit message pour discuter '
+              u'de la décision ou demander plus de détail si tout cela te '
+              u'semble injuste ou manque de clarté.')
+            .format(tutorial.title,
+                    validation.validator.username,
+                    validation.validator.profile.get_absolute_url(),
+                    validation.comment_validator,
+                    settings.ZDS_APP['site']['url']))
+        bot = get_object_or_404(User, username=settings.BOT_ACCOUNT)
+        send_mp(
+            bot,
+            tutorial.authors.all(),
+            _(u"Refus de Validation : {0}").format(tutorial.title),
+            "",
+            msg,
+            True,
+            direct=False,
+        )
         return redirect(tutorial.get_absolute_url() + "?version="
                         + validation.version)
     else:
@@ -374,30 +371,29 @@ def valid_tutorial(request):
 
         # send feedback
 
-        for author in tutorial.authors.all():
-            msg = (
-                _(u'Félicitations **{0}** ! Ton zeste [{1}]({2}) '
-                  u'a été publié par [{3}]({4}) ! Les lecteurs du monde entier '
-                  u'peuvent venir l\'éplucher et réagir à son sujet. '
-                  u'Je te conseille de rester à leur écoute afin '
-                  u'd\'apporter des corrections/compléments.'
-                  u'Un Tutoriel vivant et à jour est bien plus lu '
-                  u'qu\'un sujet abandonné !').format(
-                      author.username,
-                      tutorial.title,
-                      settings.ZDS_APP['site']['url'] + tutorial.get_absolute_url_online(),
-                      validation.validator.username,
-                      settings.ZDS_APP['site']['url'] + validation.validator.profile.get_absolute_url()))
-            bot = get_object_or_404(User, username=settings.ZDS_APP['member']['bot_account'])
-            send_mp(
-                bot,
-                [author],
-                u"Publication : {0}".format(tutorial.title),
-                "",
-                msg,
-                True,
-                direct=False,
-            )
+        msg = (
+            _(u'Félicitations ! Le zeste [{0}]({4}{1}) '
+              u'a été publié par [{2}]({4}{3}) ! Les lecteurs du monde entier '
+              u'peuvent venir l\'éplucher et réagir a son sujet. '-
+              u'Je te conseille de rester a leur écoute afin '
+              u'd\'apporter des corrections/compléments.'
+              u'Un Tutoriel vivant et a jour est bien plus lu '
+              u'qu\'un sujet abandonné !')
+            .format(tutorial.title,
+                    tutorial.get_absolute_url_online(),
+                    validation.validator.username,
+                    validation.validator.profile.get_absolute_url(),
+                    settings.ZDS_APP['site']['url']))
+        bot = get_object_or_404(User, username=settings.BOT_ACCOUNT)
+        send_mp(
+            bot,
+            tutorial.authors.all(),
+            _(u"Publication : {0}").format(tutorial.title),
+            "",
+            msg,
+            True,
+            direct=False,
+        )
         return redirect(tutorial.get_absolute_url() + "?version="
                         + validation.version)
     else:
