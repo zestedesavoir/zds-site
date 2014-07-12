20ab87b0a78c4dc1a78efb938c72d71dd94d0650
SpaceFox <spacefox@neuf.fr>
2014-07-12 20:13:27 +0200

    Issue #1123: Plus besoin de ré-uploader l'image à la moindre modif

--- zds/gallery/forms.py
+++ zds/gallery/forms.py
@@ -121,9 +121,7 @@ class ImageForm(forms.Form):
     physical = forms.ImageField(
         label=u'Sélectionnez votre image',
         required=True,
-        help_text='Taille maximum : '
-        + str(settings.IMAGE_MAX_SIZE)
-        + ' megabytes'
+        help_text='Taille maximum : ' + str(settings.IMAGE_MAX_SIZE / 1024) + ' <abbr title="kibioctet">Kio</abbr>'
     )
 
     def __init__(self, *args, **kwargs):
@@ -143,6 +141,27 @@ class ImageForm(forms.Form):
             ),
         )
 
+
+class UpdateImageForm(ImageForm):
+    def __init__(self, *args, **kwargs):
+        super(ImageForm, self).__init__(*args, **kwargs)
+
+        self.fields['physical'].required = False
+
+        self.helper = FormHelper()
+        self.helper.form_class = 'clearfix'
+        self.helper.form_method = 'post'
+
+        self.helper.layout = Layout(
+            Field('title'),
+            Field('legend'),
+            Field('physical'),
+            ButtonHolder(
+                StrictButton(u'Mettre à jour', type='submit'),
+                HTML('<a class="btn btn-cancel" '
+                u'href="{{ gallery.get_absolute_url }}">Annuler</a>'),
+            ),
+        )
 
 class ImageAsAvatarForm(forms.Form):
     """"Form to add current image as avatar"""
