1bfb795428f7ea07910f3e4e80a2a5d15f8eb5a9
Alex-D <demodealex@gmail.com>
2014-07-16 23:56:34 +0200

    Corrige une erreur d'algo dropdown Firefox

--- assets/js/data-click.js
+++ assets/js/data-click.js
@@ -17,7 +17,7 @@
         dropdownMouseDown = false;
     })
     .on("click focus", function(e){
-        if(e.type === "focus" && !dropdownMouseDown)
+        if(e.type === "focus" && dropdownMouseDown)
             return false;
 
         if(!($(this).hasClass("dont-click-if-sidebar") && $(".header-container .mobile-menu-btn").is(":visible"))){
