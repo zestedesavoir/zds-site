7f5e13ffdfe10c9dc742aad31552f21d7245c024
Eskimon <eskimon@outlook.com>
2014-07-27 10:02:58 +0200

    Correction de fautes & format de MP auto

--- zds/article/views.py
+++ zds/article/views.py
@@ -470,9 +470,9 @@ def modify(request):
                     msg = (u'Désolé **{0}**, ton zeste **{1}** '
                     u'n\'a malheureusement pas passé l’étape de validation. '
                     u'Mais ne désespère pas, certaines corrections peuvent '
-                    u'surement être faite pour l’améliorer et repasser la '
+                    u'sûrement être faites pour l’améliorer et repasser la '
                     u'validation plus tard. Voici le message que [{2}]({3}), '
-                    u'ton validateur t\'a laissé\n\n`{4}`\n\nN\'hésite pas a '
+                    u'ton validateur t\'a laissé\n\n> {4}\n\nN\'hésite pas a '
                     u'lui envoyer un petit message pour discuter de la décision '
                     u'ou demander plus de détail si tout cela te semble '
                     u'injuste ou manque de clarté.'.format(
@@ -952,8 +952,8 @@ def solve_alert(request):
     msg = (u'Bonjour {0},\n\nVous recevez ce message car vous avez '
     u'signalé le message de *{1}*, dans l\'article [{2}]({3}). '
     u'Votre alerte a été traitée par **{4}** et il vous a laissé '
-    u'le message suivant :\n\n`{5}`\n\n\nToute l\'équipe de '
-    u'la modération vous remercie'.format(
+    u'le message suivant :\n\n> {5}\n\nToute l\'équipe de '
+    u'la modération vous remercie !'.format(
         alert.author.username,
         reaction.author.username,
         reaction.article.title,
