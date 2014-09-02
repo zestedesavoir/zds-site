b7402469f700a14e3a7719ec495172b23cacea45
firm1 <firm1@github.com>
2014-09-03 01:30:03 +0200

    police Merriweather dans le contenu des pdfs

--- zds/tutorial/views.py
+++ zds/tutorial/views.py
@@ -2942,7 +2942,7 @@ def MEP(tutorial, sha):
     os.system(settings.PANDOC_LOC + "pandoc " + "--latex-engine=xelatex "
               + "--template=../../assets/tex/template.tex " + "-s " + "-S "
               + "-N " + "--toc " + "-V documentclass=scrbook "
-              + "-V lang=francais " + "-V mainfont=Verdana "
+              + "-V lang=francais " + "-V mainfont=Merriweather "
               + "-V monofont=\"Andale Mono\" " + "-V fontsize=12pt "
               + "-V geometry:margin=1in "
               + os.path.join(tutorial.get_prod_path(), tutorial.slug) + ".md "
