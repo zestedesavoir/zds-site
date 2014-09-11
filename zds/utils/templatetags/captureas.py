658cbb54163d5e6f77b4d39c09c71aef6fa86169
Christophe Gabard <cgabard@planorama.com>
2014-09-11 13:37:23 +0200

    Add doc string

--- zds/utils/templatetags/captureas.py
+++ zds/utils/templatetags/captureas.py
@@ -2,9 +2,21 @@ from django import template
 
 register = template.Library()
 
+"""
+Define a tag allowing to capture template content as a variable.
+"""
+
 
 @register.tag(name='captureas')
 def do_captureas(parser, token):
+    """
+    Define a tag allowing to capture template content as a variable.
+
+    :param parser: The django template parser
+    :param token: tag token (tag_name + variable_name)
+    :return: Template node.
+    """
+
     try:
         tag_name, variable_name = token.split_contents()
     except ValueError:
@@ -17,12 +29,28 @@ def do_captureas(parser, token):
 
 
 class CaptureasNode(template.Node):
+    """
+    Capture end render node content to a variable name.
+    """
 
     def __init__(self, nodelist, variable_name):
+        """
+        Create a template node which render `nodelist` to `variable_name`.
+
+        :param nodelist: The node list to capture.
+        :param variable_name: The variable name which will gain the rendered content.
+        """
         self.__node_list = nodelist
         self.__variable_name = variable_name
 
     def render(self, context):
+        """
+        Render the node list to the variable name.
+
+        :param context: Current context.
+        :return: Empty string
+        :rtype: str
+        """
         output = self.__node_list.render(context)
         context[self.__variable_name] = output.strip()
         return ''
