713dff2a9d308375596f9e85560fff9e4c45e660
Christophe Gabard <cgabard@planorama.com>
2014-09-11 11:53:14 +0200

    Adding doc string

--- zds/utils/templatetags/append_to_get.py
+++ zds/utils/templatetags/append_to_get.py
@@ -4,12 +4,21 @@ from functools import wraps
 register = template.Library()
 
 """
-Decorator to facilitate template tag creation
+Decorator to facilitate template tag creation.
 """
 
 
 def easy_tag(func):
-    """deal with the repetitive parts of parsing template tags"""
+    """
+    Deal with the repetitive parts of parsing template tags :
+
+     - Wraps functions attributes;
+     - Raise `TemplateSyntaxError` if arguments are not well formated.
+
+    :rtype: function
+    :param func: Function to wraps.
+    :type func: function
+    """
 
     @wraps(func)
     def inner(parser, token):
@@ -21,15 +30,34 @@ def easy_tag(func):
 
 
 class AppendGetNode(template.Node):
+    """
+    Template node allowing to render an URL appending argument to current GET address.
 
-    def __init__(self, dictionary):
+    Parse a string like `key1=var1,key2=var2` and generate a new URL with the provided parameters appended to current
+    parameters.
+    """
+
+    def __init__(self, arg_list):
+        """
+        Create a template node which append `arg_list` to GET URL.
+
+        :param str arg_list: the argument list to append.
+        """
+
         self.dict_pairs = {}
-        for pair in dictionary.split(','):
+        for pair in arg_list.split(','):
             pair = pair.split('=')
             if len(pair) > 1:
                 self.dict_pairs[pair[0]] = template.Variable(pair[1])
 
     def render(self, context):
+        """
+        Render the new URL according to the current context.
+
+        :param context: Current context.
+        :return: New URL with arguments appended.
+        :rtype: str
+        """
         get = context['request'].GET.copy()
         path = context['request'].META['PATH_INFO']
 
@@ -37,13 +65,19 @@ class AppendGetNode(template.Node):
             get[key] = self.dict_pairs[key].resolve(context)
 
         if len(get) > 0:
-            list_arg = ["{0}={1}".format(key, value) for key in get.keys() for value in get.getlist(key)]
-            path += "?" + "&".join(list_arg)
+            list_arg = [u"{0}={1}".format(key, value) for key in get.keys() for value in get.getlist(key)]
+            path += u"?" + u"&".join(list_arg)
 
         return path
 
 
 @register.tag()
 @easy_tag
-def append_to_get(_tag_name, dictionary):
-    return AppendGetNode(dictionary)
+def append_to_get(_, arg_list):
+    """Render an URL appending argument to current GET address.
+
+    :param _: Tag name (not used)
+    :param arg_list: Argument list like `key1=var1,key2=var2`
+    :return: Template node.
+    """
+    return AppendGetNode(arg_list)
