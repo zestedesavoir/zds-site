c0eb22bba49c454fe9af00fd2201c5c7e9dedcb1
Christophe Gabard <cgabard@planorama.com>
2014-09-11 13:23:53 +0200

    Rename variable and members

--- zds/utils/templatetags/captureas.py
+++ zds/utils/templatetags/captureas.py
@@ -6,23 +6,23 @@ register = template.Library()
 @register.tag(name='captureas')
 def do_captureas(parser, token):
     try:
-        tag_name, args = token.split_contents()
+        tag_name, variable_name = token.split_contents()
     except ValueError:
         raise template.TemplateSyntaxError("'captureas' node requires a variable name.")
 
     nodelist = parser.parse(('endcaptureas',))
     parser.delete_first_token()
 
-    return CaptureasNode(nodelist, args)
+    return CaptureasNode(nodelist, variable_name)
 
 
 class CaptureasNode(template.Node):
 
-    def __init__(self, nodelist, varname):
-        self.nodelist = nodelist
-        self.varname = varname
+    def __init__(self, nodelist, variable_name):
+        self.__node_list = nodelist
+        self.__variable_name = variable_name
 
     def render(self, context):
-        output = self.nodelist.render(context)
-        context[self.varname] = output.strip(' \t\n\r')
+        output = self.__node_list.render(context)
+        context[self.__variable_name] = output.strip(' \t\n\r')
         return ''
