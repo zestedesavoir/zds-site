2b8bc50375159b91bf658ea5f643b40a0c75b000
Alex-D <demodealex@gmail.com>
2014-09-11 05:16:08 +0200

    Corrige et optimise les modales

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -15,26 +15,28 @@
         e.stopPropagation();
     });
 
-    var $modals = $("body").append($("<div/>", { "id": "modals" }));
+    var $modals = $("<div/>", { "id": "modals" });
+    $("body").append($modals);
     $modals.append($overlay);
 
     function buildModals($elems){
         $elems.each(function(){
             $modals.append($(this).addClass("tab-modalize"));
             $(this).append($("<a/>", {
-                "class": "btn btn-cancel " + ($(this).is("[data-modal-close]") ? "btn-modal-fullwidth" : ""),
-                "href": "#close-modal",
-                "text": $(this).is("[data-modal-close]") ? $(this).attr("data-modal-close") : "Annuler"
-            }).on("click", function(e){
-                closeModal();
-                e.preventDefault();
-                e.stopPropagation();
+                class: "btn btn-cancel " + ($(this).is("[data-modal-close]") ? "btn-modal-fullwidth" : ""),
+                href: "#close-modal",
+                text: $(this).is("[data-modal-close]") ? $(this).attr("data-modal-close") : "Annuler",
+                click: function(e){
+                    closeModal();
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
             }));
             var $link = $("[href=#"+$(this).attr("id")+"]:first");
             var linkIco = $link.hasClass("ico-after") ? " light " + $link.attr("class").replace(/btn[a-z-]*/g, "") : "";
             $(this).prepend($("<span/>", {
-                "class": "modal-title" + linkIco,
-                "text": $link.text()
+                class: "modal-title" + linkIco,
+                text: $link.text()
             }));
         });
     }
