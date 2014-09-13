b931fd81e2b005aead661300b8a93b44cc106110
Eskimon <eskimon@outlook.com>
2014-09-13 19:47:06 +0200

    ZEP-03: Initialisation

--- zds/tutorial/models.py
+++ zds/tutorial/models.py
@@ -119,6 +119,8 @@ class Tutorial(models.Model):
 
     helps = models.ManyToManyField(HelpWriting, verbose_name='Aides', db_index=True)
 
+    helps = models.ManyToManyField(HelpWriting, verbose_name='Aides', db_index=True)
+
     def __unicode__(self):
         return self.title
 
