e57e4da72aef012cd8065563afa3d826ea70d15d
Alex-D <demodealex@gmail.com>
2014-10-05 21:50:49 +0200

    Corrige la gestion de la roue dentée sur Firefox/IE

--- assets/js/data-click.js
+++ assets/js/data-click.js
@@ -4,26 +4,35 @@
    Author: Alex-D / Alexandre Demode
    ========================================================================== */
 
-(function($, undefined){
+(function($, document, undefined){
     "use strict";
     
     var dropdownMouseDown = false;
-    
-    $("body")
-    .on("mousedown", "[data-click]", function(){
-        dropdownMouseDown = true;
-    })
-    .on("mouseup", "[data-click]", function(){
-        dropdownMouseDown = false;
-    })
-    .on("click focus", "[data-click]", function(e){
-        if(e.type === "focus" && dropdownMouseDown)
-            return false;
 
-        if(!($(this).hasClass("dont-click-if-sidebar") && $(".header-container .mobile-menu-btn").is(":visible"))){
-            e.preventDefault();
-            e.stopPropagation();
-            $("#" + $(this).attr("data-click")).trigger("click");
-        }
+    function dataClick($elem){
+        $elem
+        .on("mousedown", function(){
+            dropdownMouseDown = true;
+        })
+        .on("mouseup", function(){
+            dropdownMouseDown = false;
+        })
+        .on("click focus", function(e){
+            if(e.type === "focus" && dropdownMouseDown)
+                return false;
+
+            if(!($(this).hasClass("dont-click-if-sidebar") && $(".header-container .mobile-menu-btn").is(":visible"))){
+                e.preventDefault();
+                e.stopPropagation();
+                $("#" + $(this).attr("data-click")).trigger("click");
+            }
+        });
+    }
+
+    $(document).ready(function(){
+        dataClick($("[data-click]"));
+        $("#content").on("DOMNodeInserted", "[data-click]", function(e){
+            dataClick($(e.target));
+        });
     });
-})(jQuery);
+})(jQuery, document);
