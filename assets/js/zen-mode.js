da5098cc74fd33cf5e3988fd060531f6402335d5
Situphen <situphen@gmail.com>
2014-12-01 17:46:15 +0100

    Place le bouton "Lecture zen" que sur les pages de lecture

--- assets/js/zen-mode.js
+++ assets/js/zen-mode.js
@@ -7,17 +7,14 @@
 (function($, undefined){
     "use strict";
 
-    if($(".article-content").length > 0){
-        $(".content-container .taglist ~ .authors").before($("<button/>", {
-            class: "btn btn-grey ico-after view open-zen-mode",
-            text: "Lecture zen",
-            click: function(e){
+    if($(".open-zen-mode").length > 0){
+        $(".open-zen-mode").on("click", function(e){
                 $(".content-container").toggleClass("zen-mode tab-modalize");
                 $(this).blur();
                 e.preventDefault();
                 e.stopPropagation();
             }
-        }));
+        );
 
         $("body").on("keydown", function(e){
             // Escape close modal
