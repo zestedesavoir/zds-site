aa59c8e7d4caee0a820667d61f667887205dc5d1
Eskimon <eskimon@outlook.com>
2014-12-06 20:59:40 +0100

    Correction d'un render_template devenu render

--- zds/tutorial/views.py
+++ zds/tutorial/views.py
@@ -3627,7 +3627,7 @@ def help_tutorial(request):
 
     aides = HelpWriting.objects.all()
 
-    return render_template("tutorial/tutorial/help.html", {
+    return render("tutorial/tutorial/help.html", {
         "tutorials": shown_tutos,
         "helps": aides,
         "pages": paginator_range(page, paginator.num_pages),
