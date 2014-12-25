f14d5b8903571a6f8f6e04fc1860fd43780d2242
Augustin Laville <augustin.laville@gustavi.net>
2014-12-25 21:13:42 +0100

    Nettoyage MP : admin.py

--- zds/mp/admin.py
+++ zds/mp/admin.py
@@ -2,9 +2,30 @@
 
 from django.contrib import admin
 
-from .models import PrivatePost, PrivateTopic, PrivateTopicRead
+from models import PrivatePost, PrivateTopic, PrivateTopicRead
 
 
-admin.site.register(PrivatePost)
-admin.site.register(PrivateTopic)
-admin.site.register(PrivateTopicRead)
+class PrivatePostAdmin(admin.ModelAdmin):
+
+    """Representation of PrivatePost model in the admin interface."""
+
+    list_display = ('privatetopic', 'author',  'pubdate', 'update', 'position_in_topic')
+
+
+class PrivateTopicAdmin(admin.ModelAdmin):
+
+    """Representation of PrivateTopic model in the admin interface."""
+
+    list_display = ('title', 'subtitle', 'author', 'last_message', 'pubdate')
+
+
+class PrivateTopicReadAdmin(admin.ModelAdmin):
+
+    """Representation of PrivateTopicRead model in the admin interface."""
+
+    list_display = ('privatetopic', 'privatepost', 'user')
+
+
+admin.site.register(PrivatePost, PrivatePostAdmin)
+admin.site.register(PrivateTopic, PrivateTopicAdmin)
+admin.site.register(PrivateTopicRead, PrivateTopicReadAdmin)
