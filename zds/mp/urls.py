aed317b7b5fef5d1b7e6c25e85846bfadbb258a8
Augustin Laville <augustin.laville@gustavi.net>
2014-12-25 09:44:52 +0100

    Nettoyage MP : urls.py

--- zds/mp/urls.py
+++ zds/mp/urls.py
@@ -5,18 +5,17 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
 
-                       # Viewing a thread
-                       url(r'^nouveau/$', 'zds.mp.views.new'),
-                       url(r'^editer/$', 'zds.mp.views.edit'),
-                       url(r'^quitter/$', 'zds.mp.views.leave'),
-                       url(r'^ajouter/$', 'zds.mp.views.add_participant'),
-                       url(r'^(?P<topic_pk>\d+)/(?P<topic_slug>.+)/$',
-                           'zds.mp.views.topic'),
+    # Viewing a thread
+    url(r'^nouveau/$', 'zds.mp.views.new'),
+    url(r'^editer/$', 'zds.mp.views.edit'),
+    url(r'^quitter/$', 'zds.mp.views.leave'),
+    url(r'^ajouter/$', 'zds.mp.views.add_participant'),
+    url(r'^(?P<topic_pk>\d+)/(?P<topic_slug>.+)/$', 'zds.mp.views.topic'),
 
-                       # Message-related
-                       url(r'^message/editer/$', 'zds.mp.views.edit_post'),
-                       url(r'^message/nouveau/$', 'zds.mp.views.answer'),
+    # Message-related
+    url(r'^message/editer/$', 'zds.mp.views.edit_post'),
+    url(r'^message/nouveau/$', 'zds.mp.views.answer'),
 
-                       # Home
-                       url(r'^$', 'zds.mp.views.index'),
-                       )
+    # Home
+    url(r'^$', 'zds.mp.views.index'),
+)
