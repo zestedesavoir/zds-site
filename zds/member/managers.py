f52e08ed7875dd5dfc4785fdde2b33c344615af5
Augustin Laville <augustin.laville@gustavi.net>
2015-01-02 22:50:04 +0100

    Nettoyage et commentaires sur le managers.py de member

--- zds/member/managers.py
+++ zds/member/managers.py
@@ -5,21 +5,34 @@ from django.db import models
 
 
 class ProfileManager(models.Manager):
+
     """
     Custom profile manager.
     """
 
     def all_members_ordered_by_date_joined(self):
+        """
+        Get all members ordered by date joined.
+
+        :return: All members ordered by date joined
+        :rtype: QuerySet
+        """
         return User.objects.order_by('-date_joined').all()
 
     def all_old_tutos_from_site_du_zero(self, profile):
+        """
+        Get all tutorials from Site du Zéro for a member if exist.
+
+        :param profile: the profile of a member
+        :type profile: QuerySet
+        :return: A list of tutorials from Site du Zéro for a member if exist.
+        :rtype: list
+        """
         from models import get_info_old_tuto
 
         if profile.sdz_tutorial:
-            olds = profile.sdz_tutorial.strip().split(":")
+            olds = profile.sdz_tutorial.strip().split(':')
         else:
             olds = []
-
-        oldtutos = []
-        [oldtutos.append(get_info_old_tuto(old)) for old in olds]
-        return oldtutos
+        old_tutos =  [get_info_old_tuto(old) for old in olds]
+        return old_tutos
