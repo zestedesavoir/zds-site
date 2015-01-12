262320959d30c73803cfeff575ac9ac666347a84
Gerard Paligot <gerard.paligot@gmail.com>
2015-01-13 00:06:01 +0100

    Handles forms for client request

--- zds/settings.py
+++ zds/settings.py
@@ -222,6 +222,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
         'rest_framework.parsers.XMLParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
