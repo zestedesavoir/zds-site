1829ae671ab6247076e71f8eb7d5ad410ddab942
Gerard Paligot <gerard.paligot@gmail.com>
2015-01-13 00:04:40 +0100

    Catches error when the parameter ls/ban-jrs is not a number.

--- zds/member/api/generics.py
+++ zds/member/api/generics.py
@@ -21,32 +21,12 @@ class CreateDestroyMemberSanctionAPIView(CreateAPIView, DestroyAPIView):
     permission_classes = (IsAuthenticated, IsAdminUser)
 
     def post(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.serializer_class(instance, data=request.data)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        state = self.get_state_instance(request)
-
-        try:
-            ban = state.get_sanction(request.user, instance.user)
-        except ValueError:
-            return Response({u'detail': u'Sanction could not be applied with received data.'},
-                            status=status.HTTP_400_BAD_REQUEST)
-
-        state.apply_sanction(instance, ban)
-        msg = state.get_message_sanction() \
-            .format(ban.user,
-                    ban.moderator,
-                    ban.type,
-                    state.get_detail(),
-                    ban.text,
-                    settings.ZDS_APP['site']['litteral_name'])
-
-        state.notify_member(ban, msg)
-        return Response(serializer.data)
+        return self.process_request(request)
 
     def delete(self, request, *args, **kwargs):
+        return self.process_request(request)
+
+    def process_request(self, request):
         instance = self.get_object()
         serializer = self.serializer_class(instance, data=request.data)
         if not serializer.is_valid():
@@ -54,13 +34,12 @@ class CreateDestroyMemberSanctionAPIView(CreateAPIView, DestroyAPIView):
 
         state = self.get_state_instance(request)
 
+        ban = state.get_sanction(request.user, instance.user)
         try:
-            ban = state.get_sanction(request.user, instance.user)
+            state.apply_sanction(instance, ban)
         except ValueError:
             return Response({u'detail': u'Sanction could not be applied with received data.'},
                             status=status.HTTP_400_BAD_REQUEST)
-
-        state.apply_sanction(instance, ban)
         msg = state.get_message_sanction() \
             .format(ban.user,
                     ban.moderator,
