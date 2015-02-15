59980cf1a44d63a0b2b554d3687337dcb0c054e4
A-312 <a3@12z.fr>
2015-02-15 10:35:27 +0100

    Fix #2058: Constante GA en dure dans le code
    Constante "gaTrackingId" en dure dans le code

--- assets/js/cookies-banner.js
+++ assets/js/cookies-banner.js
@@ -7,19 +7,21 @@
 
 (function(document, undefined) {
     var $banner = $("#cookies-banner");
+    var gaTrackingId = $("meta[name=gaTrackingId]").attr("content").split(":");
 
     function checkHasConsent(){
         if(document.cookie.indexOf("hasconsent=true") > -1){
-            $("#gtm").after(
-                "<script>" +
-                    "dataLayer = [{'gaTrackingId': 'UA-27730868-1'}];" +
-                    "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':" +
-                    "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0]," +
-                    "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=" +
-                    "'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);" +
-                    "})(window,document,'script','dataLayer','GTM-WH7642');" +
-                "</script>"
-            );
+            if (gaTrackingId && gaTrackingId.length === 2)
+                $("#gtm").after(
+                    "<script>" +
+                        "dataLayer = [{'gaTrackingId': '" + gaTrackingId[0] + "'}];" +
+                        "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':" +
+                        "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0]," +
+                        "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=" +
+                        "'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);" +
+                        "})(window,document,'script','dataLayer','" + gaTrackingId[1] + "');" +
+                    "</script>"
+                );
         } else if(document.cookie.indexOf("hasconsent=false") === -1){
             // Accept for the next page
             setHasConsent(true);
