590768eb6baab56642fe3a642d8a12eec71a7103
Situphen <situphen@gmail.com>
2015-02-23 17:08:03 +0100

    N'ajoute pas de saut de ligne après Ctrl + Enter

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -581,6 +581,8 @@
     $(".md-editor").on("keydown", function(e){
         // the message is submitted if the user is pressing Ctrl or Cmd with Enter and isn't pressing Alt or Shift
         if((e.ctrlKey || e.metaKey) && e.which === 13 && !e.altKey && !e.shiftKey){
+            e.preventDefault();
+
             $(".message-submit > button[name=answer]").click();
         }
     });
