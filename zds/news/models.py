3e189d1d27f9db193234b284b1437e3233c320e2
Situphen <situphen@gmail.com>
2015-03-09 20:07:04 +0100

    Traduction d'une partie du module de news

--- zds/news/models.py
+++ zds/news/models.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from zds.member.models import Profile
 from zds.news.managers import NewsManager
@@ -8,19 +9,19 @@ from zds.news.managers import NewsManager
 
 class News(models.Model):
     class Meta:
-        verbose_name = u'Une'
-        verbose_name_plural = u'Unes'
+        verbose_name = _(u'Une')
+        verbose_name_plural = _(u'Unes')
 
-    title = models.CharField(u'Titre', max_length=80)
-    type = models.CharField(u'Type', max_length=80)
-    authors = models.ManyToManyField(Profile, verbose_name=u'Auteurs', db_index=True)
+    title = models.CharField(_(u'Titre'), max_length=80)
+    type = models.CharField(_(u'Type'), max_length=80)
+    authors = models.ManyToManyField(Profile, verbose_name=_(u'Auteurs'), db_index=True)
     image_url = models.CharField(
-        u'URL de l\'image à la une', max_length=128, null=False, blank=False
+        _(u'URL de l\'image à la une'), max_length=128, null=False, blank=False
     )
     url = models.CharField(
-        u'URL de la une', max_length=128, null=False, blank=False
+        _(u'URL de la une'), max_length=128, null=False, blank=False
     )
-    pubdate = models.DateTimeField('Date de publication', blank=True, null=False, db_index=True)
+    pubdate = models.DateTimeField(_(u'Date de publication'), blank=True, null=False, db_index=True)
 
     objects = NewsManager()
 
