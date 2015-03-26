51fda4ffea3fa33292acf5a4a9e5243cdf8b8825
Francois Dambrine <fdambrine@fdambrine.goto.fr>
2015-03-26 13:55:37 +0100

    tests du déplacement terminés

--- zds/tutorialv2/views.py
+++ zds/tutorialv2/views.py
@@ -1031,10 +1031,17 @@ class MoveChild(LoginRequiredMixin, SingleContentPostMixin, FormView):
             if form.data['moving_method'][0:len(MoveElementForm.MOVE_AFTER)] == MoveElementForm.MOVE_AFTER:
                 target = form.data['moving_method'][len(MoveElementForm.MOVE_AFTER) + 1:]
                 if not parent.has_child_with_path(target):
-                    target_parent = search_container_or_404(versionned, target.split("/")[1:-2])
+                    if "/" not in target:
+                        target_parent = versioned
+                    else:
+                        target_parent = search_container_or_404(versioned, "/".join(target.split("/")[:-1]))
+                        
+                        if target.split("/")[-1] not in target_parent.children_dict:
+                            raise Http404
                     child = target_parent.children_dict[target.split("/")[-1]]
-                    try_adopt_new_child(target_parent, parent[child_slug])
-                target_parent.move_child_after(child_slug, target)
+                    try_adopt_new_child(target_parent, parent.children_dict[child_slug])
+                    parent = target_parent
+                parent.move_child_after(child_slug, target.split("/")[-1])
             if form.data['moving_method'][0:len(MoveElementForm.MOVE_BEFORE)] == MoveElementForm.MOVE_BEFORE:
                 target = form.data['moving_method'][len(MoveElementForm.MOVE_BEFORE) + 1:]
                 if not parent.has_child_with_path(target):
