84962a41c9b2ab4e1894fca433a4eee143e29c21
SpaceFox <ujueseo.yeou@gmail.com>
2015-04-07 20:15:31 +0200

    django.contrib.sites n'est pas utilisé, il dégage.
    Raison : modifications à appliquer pour l'utiliser.
    Cf https://docs.djangoproject.com/en/1.8/releases/1.8/#django-contrib-sites

--- zds/settings.py
+++ zds/settings.py
@@ -143,7 +143,6 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
