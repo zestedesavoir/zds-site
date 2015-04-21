2a8572603b6187bc8884f4265c4f848c36904463
Quentin Gliech <quentingliech@gmail.com>
2015-04-21 16:35:04 +0200

    Automatic front build

---
+++ scripts/push_front.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Pushes front files to a new tag on GitHub
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 <tag to build>" >&2
+	exit 1
+fi
+
+echo -n "Pushing front to GitHub"
+
+# Detaching from head
+git checkout --detach
+
+echo -n "."
+
+# Adding dist/ and commiting
+git add --force dist/
+git commit -m "Automatic front build"
+
+echo -n "."
+
+# Creating tag and pushing
+TAG_NAME=$1-build
+git tag -a $TAG_NAME -m "$1 with built front files"
+git push origin $TAG_NAME
+
+echo ". done!"
