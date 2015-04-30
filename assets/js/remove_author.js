08cda9b556aed93a1fcf43de156fa13a6a0ef6ac
artragis <artragis@gmail.com>
2015-04-30 12:03:45 +0200

    plus de JS dans les auteurs.

--- assets/js/remove_author.js
+++
@@ -1,9 +0,0 @@
-(function(document, $, undefined){
-    "use strict";
-
-    var remove_buttons = $("#manage-author button");
-    var username_input = $("#remove-username");
-    remove_buttons.click(function(e){
-                    username_input.val(remove_buttons.attr("data-value"));
-                });
-})(document, jQuery);
