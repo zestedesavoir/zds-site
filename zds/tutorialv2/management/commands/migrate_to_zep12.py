34b7e2d8f32c77b258ce9fd0b5e9421799217375
artragis <artragis@gmail.com>
2015-05-21 13:32:30 +0200

    permet l'export des notes

--- zds/tutorialv2/management/commands/migrate_to_zep12.py
+++ zds/tutorialv2/management/commands/migrate_to_zep12.py
@@ -1,6 +1,6 @@
 try:
-    from zds.article.models import Article  # , ArticleRead, Reaction
-    from zds.tutorial.models import Tutorial, Part, Chapter  # , Comment
+    from zds.article.models import Article , ArticleRead, Reaction
+    from zds.tutorial.models import Tutorial, Part, Chapter, Note, TutorialRead
     from zds.tutorial.models import Extract as OldExtract
 except ImportError:
     print("The old stack is no more available on your zestedesavoir copy")
@@ -9,7 +9,8 @@ except ImportError:
 
 from zds.forum.models import Topic
 
-from zds.tutorialv2.models import PublishableContent, Extract, Container  # , VersionedContent
+from zds.tutorialv2.models.models_database import PublishableContent, ContentReaction, ContentRead
+from zds.tutorialv2.models.models_versioned import Extract, Container
 from django.core.management.base import BaseCommand
 from django.db import transaction
 from zds.gallery.models import Gallery, UserGallery
@@ -18,6 +19,38 @@ from zds.utils.models import Licence
 from datetime import datetime
 import shutil
 import sys
+
+
+def export_read_for_note(old_note, new_note, read_class):
+    queryset = read_class.objects
+    if read_class == ArticleRead:
+        queryset = queryset.filter(reaction__pk=old_note.pk)
+    else:
+        queryset = queryset.filter(note__pk=old_note.pk)
+    for read in queryset.all():
+        new_read = ContentRead()
+        new_read.content = new_note.related_content
+        new_read.note = new_note
+        new_read.user = read.user
+        new_read.save()
+
+
+def export_comments(reacts, exported, read_class):
+    c = 0
+    for note in reacts:
+        new_reac = ContentReaction()
+        new_reac.pubdate = note.pubdate
+        new_reac.author = note.author
+        c += 1
+        new_reac.position = c
+        new_reac.related_content = exported
+
+        new_reac.update_content(note.text)
+        new_reac.ip_address = note.ip_address
+        new_reac.save()
+        export_read_for_note(note, new_reac, read_class)
+    exported.last_note = new_reac
+    exported.save()
 
 
 def progressbar(it, prefix="", size=60):
@@ -88,6 +121,11 @@ def migrate_articles():
         exported.save()
         # todo  : generate mapping
         # todo: handle notes
+        reacts = Reaction.objects.filter(article__pk=current.pk)\
+                                 .select_related("author")\
+                                 .order_by("pubdate")\
+                                 .all()
+        export_comments(reacts, exported, ArticleRead)
         # todo: handle publication
 
 
@@ -141,6 +179,12 @@ def migrate_mini_tuto():
             text = text.replace(current.get_absolute_url_beta(), exported.get_absolute_url_beta())
             former_first_post.update_content(text)
             former_first_post.save()
+        # extract notes
+        reacts = Note.objects.filter(tutorial__pk=current.pk)\
+                             .select_related("author")\
+                             .order_by("pubdate")\
+                             .all()
+        export_comments(reacts, exported, TutorialRead)
 
 
 def migrate_big_tuto():
@@ -205,6 +249,11 @@ def migrate_big_tuto():
             former_first_post.save()
 
         # todo: handle publication, notes etc.
+    reacts = Note.objects.filter(tutorial__pk=current.pk)\
+                             .select_related("author")\
+                             .order_by("pubdate")\
+                             .all()
+    export_comments(reacts, exported, TutorialRead)
 
 
 @transaction.atomic
