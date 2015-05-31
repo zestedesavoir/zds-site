89f82ed57a9bae768f5ba32ea77278649a0c210c
Hugo <hugo.courtecuisse@gmail.com>
2015-05-31 12:28:01 +0200

    On indexe pas les dates

--- zds/forum/search_indexes.py
+++ zds/forum/search_indexes.py
@@ -15,7 +15,7 @@ class TopicIndex(indexes.SearchIndex, indexes.Indexable):
     title = indexes.CharField(model_attr='title')
     subtitle = indexes.CharField(model_attr='subtitle')
     author = indexes.CharField(model_attr='author')
-    pubdate = indexes.DateTimeField(model_attr='pubdate')
+    pubdate = indexes.DateTimeField(model_attr='pubdate', stored=True, indexed=False)
 
     # Groups authorized to read this topic. If no group is defined, the forum is public (and anyone can read it).
     permissions = indexes.MultiValueField()
