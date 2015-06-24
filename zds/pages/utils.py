bddeeccd32aad735ffe1bcb6851c67e22935c650
artragis <artragis@gmail.com>
2015-06-24 10:00:47 +0200

    réduit les requetes sur la HP.

--- zds/pages/utils.py
+++ zds/pages/utils.py
@@ -5,22 +5,23 @@ from zds.tutorialv2.models.models_database import PublishedContent
 def get_last_articles():
     n = settings.ZDS_APP['article']['home_number']
     return PublishedContent.objects\
+                                .filter(content_type="ARTICLE")\
                                 .prefetch_related("authors")\
                                 .prefetch_related("authors__profile")\
                                 .select_related("last_note")\
                                 .prefetch_related("subcategory")\
-                                .filter(content_type="ARTICLE").all()\
-        .order_by('-publication_date')[:n]
+                                .all()\
+                                .order_by('-publication_date')[:n]
 
 
 def get_last_tutorials():
     n = settings.ZDS_APP['tutorial']['home_number']
     tutorials = PublishedContent.objects\
+                                .filter(content_type="TUTORIAL")\
                                 .prefetch_related("authors")\
                                 .prefetch_related("authors__profile")\
                                 .select_related("last_note")\
                                 .prefetch_related("subcategory")\
-                                .filter(content_type="TUTORIAL")\
                                 .all()\
                                 .order_by('-publication_date')[:n]
 
