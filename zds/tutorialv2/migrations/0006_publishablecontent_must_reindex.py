243eadbdf667b1d9fcf8e726976a4569fde6cb4a
Hugo <hugo.courtecuisse@gmail.com>
2015-08-02 20:36:31 +0200

    Ajout des fichiers de migrations

---
+++ zds/tutorialv2/migrations/0006_publishablecontent_must_reindex.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tutorialv2', '0005_auto_20150510_2114'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='publishablecontent',
+            name='must_reindex',
+            field=models.BooleanField(default=True, verbose_name=b'Si le contenu doit-\xc3\xaatre r\xc3\xa9-indexe'),
+            preserve_default=True,
+        ),
+    ]
