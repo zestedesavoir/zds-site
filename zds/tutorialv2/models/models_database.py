ec321f9c3031530a07a5db2186dec8321f237576
pierre-24 <pierreb24@gmail.com>
2015-10-11 13:46:59 +0200

    Corrige une coquille (fix #3063)

--- zds/tutorialv2/models/models_database.py
+++ zds/tutorialv2/models/models_database.py
@@ -353,7 +353,7 @@ class PublishableContent(models.Model):
         """
 
         attrs = [
-            'pk', 'authors', 'authors', 'subcategory', 'image', 'creation_date', 'pubdate', 'update_date', 'source',
+            'pk', 'authors', 'subcategory', 'image', 'creation_date', 'pubdate', 'update_date', 'source',
             'sha_draft', 'sha_beta', 'sha_validation', 'sha_public'
         ]
 
