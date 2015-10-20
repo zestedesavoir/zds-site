e2a139b939aa943e1908499ef58ac8cb530b4a12
Emeric54 <fischer.emeric@gmail.com>
2015-10-20 13:34:08 +0200

    Masque les sujets fermés sur la page d'accueil

--- zds/forum/managers.py
+++ zds/forum/managers.py
@@ -61,7 +61,7 @@ class TopicManager(models.Manager):
 
     def get_last_topics(self):
         return self.order_by('-pubdate') \
-                   .exclude(Q(forum__group__isnull=False)) \
+                   .exclude(Q(forum__group__isnull=False)).exclude(Q(is_locked=True)) \
                    .all() \
                    .select_related('forum', 'author', 'last_message') \
                    .prefetch_related('tags')[:settings.ZDS_APP['topic']['home_number']]
