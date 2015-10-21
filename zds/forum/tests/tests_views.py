327fa27e83766b32df7f223597d51ab3a50ecae6
Emeric54 <fischer.emeric@gmail.com>
2015-10-21 13:24:04 +0200

    Ajout d'un test automatique

--- zds/forum/tests/tests_views.py
+++ zds/forum/tests/tests_views.py
@@ -46,7 +46,27 @@ class CategoriesForumsListViewTests(TestCase):
         self.assertEqual(category, current_category)
         self.assertEqual(forum, current_category.get_forums(profile.user)[0])
 
+    def test_topic_list_home_page(self):
+	staff = StaffProfileFactory()
 
+        profile = ProfileFactory()
+        category, forum = create_category()
+        topic = add_topic_in_a_forum(forum, profile)
+
+	nb_topics = len(Topic.objects.get_last_topics())
+
+        self.assertTrue(self.client.login(username=staff.user.username, password='hostel77'))
+        data = {
+            'lock': 'true',
+            'topic': topic.pk
+        }
+        response = self.client.post(reverse('topic-edit'), data, follow=False)
+
+        self.assertEqual(302, response.status_code)
+        self.assertTrue(Topic.objects.get(pk=topic.pk).is_locked)
+	 
+	self.assertTrue(len(Topic.objects.get_last_topics()) < nb_topics)
+	
 class CategoryForumsDetailViewTest(TestCase):
     def test_success_list_all_forums_of_a_category(self):
         profile = ProfileFactory()
