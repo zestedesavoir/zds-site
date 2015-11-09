ff00a9f63e41e60ba3cc6ff6dcffd55f6d7ce9b2
artragis <artragis@gmail.com>
2015-11-09 13:00:47 +0100

    échappe aussi le http de l'avatar

--- zds/utils/templatetags/remove_url_protocole.py
+++ zds/utils/templatetags/remove_url_protocole.py
@@ -5,11 +5,6 @@ from zds.settings import ZDS_APP
 register = template.Library()
 
 
-CONVERT_VALUES = ((1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'),
-                  (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'),
-                  (4, 'IV'), (1, 'I'))
-
-
 @register.filter('remove_url_protocole')
 def remove_url_protocole(input_url):
     """
