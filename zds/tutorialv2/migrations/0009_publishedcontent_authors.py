24f8c29170f1580eefad5cdb7e5fa6d7a30fa9f7
artragis <artragis@gmail.com>
2015-11-19 13:07:56 +0100

    migration de données pour les auteurs

--- zds/tutorialv2/migrations/0009_publishedcontent_authors.py
+++ zds/tutorialv2/migrations/0009_publishedcontent_authors.py
@@ -5,6 +5,14 @@ from django.db import models, migrations
 from django.conf import settings
 
 
+def put_authors(apps, schema_editor):
+    PublishedContent = apps.get_model("tutorialv2", "PublishedContent")
+    for content in PublishedContent.objects.filter(must_redirect=False).all():
+        for author in content.content.authors.all():
+            content.authors.add(author)
+        content.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -19,4 +27,5 @@ class Migration(migrations.Migration):
             field=models.ManyToManyField(to=settings.AUTH_USER_MODEL, verbose_name=b'Auteurs', db_index=True),
             preserve_default=True,
         ),
+        migrations.RunPython(put_authors)
     ]
