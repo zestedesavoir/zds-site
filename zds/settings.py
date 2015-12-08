b708521eeaad88393fc1d23fc25c885760295514
Augustin Laville <augustin.laville@gustavi.net>
2015-12-08 11:52:36 +0100

    Correction de la syntaxe des chemins des templates

--- zds/settings.py
+++ zds/settings.py
@@ -124,7 +124,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'APP_DIRS': True,
-        'DIRS': os.path.join(BASE_DIR, 'templates'),
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'OPTIONS': {
             'context_processors': [
                 # Default context processors
