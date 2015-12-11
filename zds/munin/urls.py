cf856eba334bff95fabb7bbf0c5b8726aa35ecbe
artragis <artragis@gmail.com>
2015-12-11 13:29:52 +0100

    corrige les erreurs de tutorialv2 et munin

--- zds/munin/urls.py
+++ zds/munin/urls.py
@@ -6,9 +6,9 @@ from zds.munin.views import total_topics, total_posts, total_mps, total_tutorial
 
 
 urlpatterns = [
-    url(r'^total_topics/$', total_topics),
-    url(r'^total_posts/$', total_posts),
-    url(r'^total_mps/$', total_mps),
-    url(r'^total_tutorials/$', total_tutorials),
-    url(r'^total_articles/$', total_articles),
+    url(r'^total_topics/$', total_topics, name="total_topics"),
+    url(r'^total_posts/$', total_posts, name="total_posts"),
+    url(r'^total_mps/$', total_mps, name="total_mp"),
+    url(r'^total_tutorials/$', total_tutorials, name="total_tutorial"),
+    url(r'^total_articles/$', total_articles, name="total_articles"),
 ]
