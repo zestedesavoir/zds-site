6dcdf770eef4b4c28f1cb186c18e3eab9104b9d2
GerardPaligot <gerard.paligot@gmail.com>
2015-12-19 23:03:14 +0100

    feat(model): Migrates all new models.
    Closes #27

--- zds/notification/managers.py
+++ zds/notification/managers.py
@@ -1,7 +1,102 @@
 # -*- coding: utf-8 -*-
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 
 from zds.utils import get_current_user
+
+
+class SubscriptionManager(models.Manager):
+    """
+    Custom subscription manager
+    """
+
+    def get_existing(self, profile, content_object, is_active=None):
+        content_type = ContentType.objects.get_for_model(content_object)
+        try:
+            if is_active is None:
+                existing = self.get(
+                    object_id=content_object.pk,
+                    content_type__pk=content_type.pk,
+                    profile=profile)
+            else:
+                existing = self.get(
+                    object_id=content_object.pk,
+                    content_type__pk=content_type.pk,
+                    profile=profile, is_active=is_active)
+        except ObjectDoesNotExist:
+            existing = None
+        return existing
+
+    def get_or_create_active(self, profile, content_object):
+        content_type = ContentType.objects.get_for_model(content_object)
+        try:
+            subscription = self.get(
+                object_id=content_object.pk,
+                content_type__pk=content_type.pk,
+                profile=profile)
+            if not subscription.is_active:
+                subscription.activate()
+        except ObjectDoesNotExist:
+            subscription = self.model(profile=profile, content_object=content_object)
+            subscription.save()
+
+        return subscription
+
+    def get_subscribers(self, content_object, only_by_email=False):
+        users = []
+
+        content_type = ContentType.objects.get_for_model(content_object)
+        if only_by_email:
+            # if I'm only interested by the email subscription
+            subscription_list = self.filter(
+                object_id=content_object.pk,
+                content_type__pk=content_type.pk,
+                by_email=True)
+        else:
+            subscription_list = self.filter(
+                object_id=content_object.pk,
+                content_type__pk=content_type.pk)
+
+        for subscription in subscription_list:
+            users.append(subscription.profile.user)
+        return users
+
+    def get_objects_followed_by(self, profile):
+        """
+        :return: All objects followed by this user.
+        """
+        followed_objects = []
+        subscription_list = self.filter(profile=self, is_active=True)\
+            .order_by('last_notification__pubdate')
+
+        for subscription in subscription_list:
+            followed_objects.append(subscription.content_object)
+
+        return followed_objects
+
+
+class NotificationManager(models.Manager):
+    """
+    Custom notification manager.
+    """
+
+    def get_unread_notifications_of(self, profile):
+        """
+        Gets all notifications for a user whose profile is passed as argument.
+
+        :param profile: user's profile object
+        :type profile: zds.members.models.Profile
+        :return: an iterable over notifications with user data already loaded
+        :rtype: QuerySet
+        """
+        return self.filter(subscription__profile=profile, is_read=False) \
+            .select_related("sender") \
+            .select_related("sender__user")
+
+    def filter_content_type_of(self, model):
+        content_subscription_type = ContentType.objects.get_for_model(model)
+        return self.filter(subscription__content_type__pk=content_subscription_type.pk)
 
 
 class TopicFollowedManager(models.Manager):
--- zds/notification/models.py
+++ zds/notification/models.py
@@ -1,10 +1,240 @@
 # -*- coding: utf-8 -*-s
-from django.contrib.auth.models import User
-from django.db import models
+from smtplib import SMTPException
 
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.core.mail import EmailMultiAlternatives
+from django.db import models
+from django.template.loader import render_to_string
+from django.utils.translation import ugettext_lazy as _
+
+from zds import settings
 from zds.forum.models import Topic
-from zds.notification.managers import TopicFollowedManager
+from zds.member.models import Profile
+from zds.notification.managers import TopicFollowedManager, NotificationManager
 from zds.utils import get_current_user
+from zds.utils.misc import convert_camel_to_underscore
+
+
+class Subscription(models.Model):
+    """
+    Model used to register the subscription of a user to a set of notifications (regarding a tutorial, a forum, ...)
+    """
+
+    class Meta:
+        verbose_name = _(u'Abonnement')
+        verbose_name_plural = _(u'Abonnements')
+
+    profile = models.ForeignKey(Profile, related_name='subscriber', db_index=True)
+    pubdate = models.DateTimeField(_(u'Date de création'), auto_now_add=True, db_index=True)
+    is_active = models.BooleanField(_(u'Actif'), default=True, db_index=True)
+    by_email = models.BooleanField(_(u'Recevoir un email'), default=False)
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    last_notification = models.ForeignKey(u'Notification', related_name="last_notification", null=True, default=None)
+
+    def __unicode__(self):
+        return _(u'<Abonnement du membre "{0}" aux notifications pour le {1}, #{2}>')\
+            .format(self.profile, self.content_type, self.object_id)
+
+    def activate(self):
+        """
+        Activates the subscription if it's inactive. Does nothing otherwise
+        """
+        if not self.is_active:
+            self.is_active = True
+            self.save()
+
+    def activate_email(self):
+        """
+        Acitvates the notifications by email (and the subscription itself if needed)
+        """
+        if not self.is_active or not self.by_email:
+            self.is_active = True
+            self.by_email = True
+            self.save()
+
+    def deactivate(self):
+        """
+        Deactivate the subscription if it is active. Does nothing otherwise
+        """
+        if self.is_active:
+            self.is_active = False
+            self.save()
+
+    def deactivate_email(self):
+        """
+        Deactivate the email if it is active. Does nothing otherwise
+        """
+        if self.is_active and self.by_email:
+            self.by_email = False
+            self.save()
+
+    def set_last_notification(self, notification):
+        """
+        Replace last_notification by the one given
+        """
+        self.last_notification = notification
+        self.save()
+
+    def send_email(self, notification):
+        """
+        Sends an email notification
+        """
+
+        assert hasattr(self, "module")
+
+        subject = _(u"{} - {} : {}").format(settings.ZDS_APP['site']['litteral_name'], self.module, notification.title)
+        from_email = _(u"{} <{}>").format(settings.ZDS_APP['site']['litteral_name'],
+                                          settings.ZDS_APP['site']['email_noreply'])
+
+        receiver = self.profile.user
+        context = {
+            'username': self.profile.user.username,
+            'title': notification.title,
+            'url': settings.ZDS_APP['site']['url'] + notification.url,
+            'author': notification.sender.user.username,
+            'site_name': settings.ZDS_APP['site']['litteral_name']
+        }
+        message_html = render_to_string(
+            'email/notification/' + convert_camel_to_underscore(self._meta.object_name) + '.html', context)
+        message_txt = render_to_string(
+            'email/notification/' + convert_camel_to_underscore(self._meta.object_name) + '.txt', context)
+
+        msg = EmailMultiAlternatives(subject, message_txt, from_email, [receiver.email])
+        msg.attach_alternative(message_html, "text/html")
+        try:
+            msg.send()
+        except SMTPException:
+            pass
+
+
+class SingleNotificationMixin(object):
+    """
+    Mixin for the subscription that can only have one active notification at a time
+    """
+    def send_notification(self, content=None, send_email=True, sender=None):
+        """
+        Sends the notification about the given content
+        :param content:  the content the notification is about
+        :param sender: the user whose action triggered the notification
+        :param send_email : whether an email must be sent if the subscription by email is active
+        """
+        assert hasattr(self, "get_notification_url")
+        assert hasattr(self, "get_notification_title")
+        assert hasattr(self, "send_email")
+
+        # If the last notification has not been read yet, no new notification is sent
+        if self.last_notification is None or self.last_notification.is_read:
+            notification = Notification(subscription=self, content_object=content, sender=sender)
+            notification.url = self.get_notification_url(content)
+            notification.title = self.get_notification_title(content)
+            notification.save()
+            self.set_last_notification(notification)
+            self.save()
+
+            if send_email and self.by_email:
+                self.send_email(notification)
+        elif self.last_notification is not None:
+            # Update last notif if the new content is older (case of unreading answer)
+            if not self.last_notification.is_read and self.last_notification.pubdate > content.pubdate:
+                self.last_notification.content_object = content
+                self.last_notification.save()
+
+    def mark_notification_read(self):
+        """
+        Marks the notification of the subscription as read.
+        As there's only one active unread notification at all time,
+        no need for more precision
+        """
+        if self.last_notification is not None:
+            self.last_notification.is_read = True
+            self.last_notification.save()
+
+
+class MultipleNotificationsMixin(object):
+
+    def send_notification(self, content=None, send_email=True, sender=None):
+        """
+        Sends the notification about the given content
+        :param content:  the content the notification is about
+        :param sender: the user whose action triggered the notification
+        :param send_email : whether an email must be sent if the subscription by email is active
+        """
+
+        assert hasattr(self, "get_notification_url")
+        assert hasattr(self, "get_notification_title")
+        assert hasattr(self, "send_email")
+
+        notification = Notification(subscription=self, content_object=content, sender=sender)
+        notification.url = self.get_notification_url(content)
+        notification.title = self.get_notification_title(content)
+        notification.save()
+        self.set_last_notification(notification)
+
+        if send_email and self.by_email:
+            self.send_email(notification)
+
+    def mark_notification_read(self, content):
+        """
+        Marks the notification of the subscription as read.
+        :param content : the content whose notification has been read
+        """
+        if content is None:
+            raise Exception('Object content of notification must be defined')
+
+        content_notification_type = ContentType.objects.get_for_model(content)
+        try:
+            notification = Notification.objects.get(subscription=self,
+                                                    content_type__pk=content_notification_type.pk,
+                                                    object_id=content.pk, is_read=False)
+            if notification is not None:
+                notification.is_read = True
+                notification.save()
+        except Notification.DoesNotExist:
+            pass
+
+
+class AnswerSubscription(Subscription):
+    """
+    Subscription to new answer, either in a topic, a article or a tutorial
+    NOT used directly, use one of its subtype
+    """
+    def __unicode__(self):
+        return _(u'<Abonnement du membre "{0}" aux réponses au {1} #{2}>')\
+            .format(self.profile, self.content_type, self.object_id)
+
+    def get_notification_url(self, answer):
+        return answer.get_absolute_url()
+
+    def get_notification_title(self, answer):
+        return self.content_object.title
+
+
+class Notification(models.Model):
+    """
+    A notification
+    """
+    class Meta:
+        verbose_name = _(u'Notification')
+        verbose_name_plural = _(u'Notifications')
+
+    subscription = models.ForeignKey(Subscription, related_name='subscription', db_index=True)
+    pubdate = models.DateTimeField(_(u'Date de création'), auto_now_add=True, db_index=True)
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    is_read = models.BooleanField(_(u'Lue'), default=False, db_index=True)
+    url = models.CharField('URL', max_length=255)
+    sender = models.ForeignKey(Profile, related_name='sender', db_index=True)
+    title = models.CharField('Titre', max_length=200)
+    objects = NotificationManager()
+
+    def __unicode__(self):
+        return _(u'Notification du membre "{0}" à propos de : {1} #{2} ({3})')\
+            .format(self.subscription.profile, self.content_type, self.content_object.pk, self.subscription)
 
 
 class TopicFollowed(models.Model):
