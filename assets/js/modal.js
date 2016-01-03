95b2c3e19a289e1cacd8c95f84b104612ecaaaf6
Quentin Gliech <quentingliech@gmail.com>
2016-01-03 01:05:33 +0100

    Rewrite du module de Modales
    Les modales utilisent maintenant des flexbox
    API JS Modal()
    WIP

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -4,69 +4,129 @@
    Author: Alex-D / Alexandre Demode
    ========================================================================== */
 
-(function(document, $, undefined){
+(function(document, $, undefined) {
     "use strict";
-    
+
+    var Modal = function(options) {
+        this.options = $.extend(options, {
+            titleIcon: "",
+            closeText: "Annuler"
+        });
+        if(!Modal._initialized) this.firstRun();
+        this.init();
+    };
+
+    Modal.closeCurrent = function() {
+        Modal.current.modal.removeClass("open");
+        Modal.container.removeClass("open");
+        $("html").removeClass("dropdown-active");
+        Modal.current = null;
+    };
+
+    Modal.openModal = function(id) {
+       if(Modal.list[id]) {
+           Modal.list[id].open();
+       }
+    };
+
+    Modal.prototype = {
+        firstRun: function() {
+            Modal.container = $("<div>", { class: "modals-container" });
+            Modal.wrapper = $("<div>", { class: "modals-wrapper" });
+            Modal.overlay = $("<div>", { class: "modals-overlay" });
+            Modal.container.append(Modal.wrapper).append(Modal.overlay).appendTo($("body"));
+            Modal.list = [];
+            Modal._initialized = true;
+            Modal.nextId = 0;
+
+            Modal.overlay.on("click", Modal.closeCurrent);
+
+            $("body").on("click", ".open-modal", function(e) {
+                Modal.openModal($(this).attr("href").substring(1));
+
+                e.preventDefault();
+                e.stopPropagation();
+            }).on("keydown", function(e) {
+                // Escape close modal
+                if(Modal.current && e.which === 27){
+                    Modal.closeCurrent();
+                    e.stopPropagation();
+                }
+            });
+        },
+
+        init: function() {
+            this.modal = this.options.modal || $("<div>", { class: "modal modal-flex" });
+            this.id = this.modal.attr("id") || "noid-" + (Modal.nextId++);
+            this.title = $("<div>", {
+                class: "modal-title" + this.options.titleIcon,
+                text: this.options.title
+            });
+
+            this.body = $("<div>", {
+                class: "modal-body"
+            }).append(this.options.body);
+
+            this.footer = $("<div>", {
+                class: "modal-footer"
+            }).append(this.options.footer).append($("<a>", {
+                class: "btn btn-cancel",
+                href: "#close-modal",
+                text: this.options.closeText,
+                click: function(e){
+                    Modal.closeCurrent();
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
+            }));
+
+            this.modal.addClass("tab-modalize").append(this.title, this.body, this.footer).appendTo(Modal.wrapper);
+
+            Modal.list[this.id] = this;
+        },
+
+        open: function() {
+            if(Modal.current) Modal.closeCurrent();
+            this.modal.addClass("open");
+            Modal.container.addClass("open");
+
+            Modal.current = this;
+
+            this.body.find("input:visible, select, textarea").first().focus();
+            if(!$("html").hasClass("enable-mobile-menu"))
+                $("html").addClass("dropdown-active");
+        },
+
+        close: function() {
+            Modal.closeCurrent();
+        }
+    };
+
+    /*
     var $overlay = $("<div/>", {
         "id": "modals-overlay"
     }).on("click", function(e){
         closeModal();
         e.preventDefault();
         e.stopPropagation();
-    });
-
-    var $modals = $("<div/>", { "id": "modals" });
-    $("body").append($modals);
-    $modals.append($overlay);
+    });*/
 
     function buildModals($elems){
         $elems.each(function(){
-            $modals.append($(this).addClass("tab-modalize"));
-            $(this).append($("<a/>", {
-                class: "btn btn-cancel " + ($(this).is("[data-modal-close]") ? "btn-modal-fullwidth" : ""),
-                href: "#close-modal",
-                text: $(this).is("[data-modal-close]") ? $(this).attr("data-modal-close") : "Annuler",
-                click: function(e){
-                    closeModal();
-                    e.preventDefault();
-                    e.stopPropagation();
-                }
-            }));
             var $link = $("[href=#"+$(this).attr("id")+"]:first");
             var linkIco = $link.hasClass("ico-after") ? " light " + $link.attr("class").replace(/btn[a-z-]*/g, "") : "";
-            $(this).prepend($("<span/>", {
-                class: "modal-title" + linkIco,
-                text: $link.text()
-            }));
+            new Modal({
+                title: $link.text(),
+                footer: $(this).find(".btn, [type=submit]").detach(),
+                body: $(this).children(),
+                modal: $(this),
+                closeText: $(this).is("[data-modal-close]") ? $(this).attr("data-modal-close") : "Annuler",
+                titleIcon: linkIco
+            });
         });
     }
 
-    $("body").on("click", ".open-modal", function(e){
-        $overlay.show();
-        $($(this).attr("href")).show(0, function(){
-            $(this).find("input:visible, select, textarea").first().focus();
-        });
-        if(!$("html").hasClass("enable-mobile-menu"))
-            $("html").addClass("dropdown-active");
-
-        e.preventDefault();
-        e.stopPropagation();
-    });
-
-    $("body").on("keydown", function(e){
-        // Escape close modal
-        if($(".modal:visible", $modals).length > 0 && e.which === 27){
-            closeModal();
-            e.stopPropagation();
-        }
-    });
-
-    function closeModal(){
-        $(".modal:visible", $modals).fadeOut(150);
-        $overlay.fadeOut(150);
-        $("html").removeClass("dropdown-active");
-    }
-
+    window.Modal = Modal;
 
     $(document).ready(function(){
         buildModals($(".modal"));
