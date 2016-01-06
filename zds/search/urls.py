8798768ebd91cb9d2e813597742a28f03a6fece9
Augustin Laville <augustin.laville@gustavi.net>
2016-01-06 15:25:45 +0100

    Correction des URL de la recherche

--- zds/search/urls.py
+++ zds/search/urls.py
@@ -2,10 +2,10 @@
 
 from django.conf.urls import url
 
-from zds.search.views import CustomSearchView
+from zds.search.views import CustomSearchView, opensearch
 
 
 urlpatterns = [
     url(r'^$', CustomSearchView.as_view(), name='haystack_search'),
-    url(r'^opensearch\.xml$', 'zds.search.views.opensearch', name='search-opensearch'),
+    url(r'^opensearch\.xml$', opensearch, name='search-opensearch'),
 ]
