70f40e573f4d407cf408bdfab6de8fda87cc0769
Quentin Gliech <quentingliech@gmail.com>
2016-01-16 16:46:50 +0100

    gulp-newer + errors css

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,19 +1,27 @@
 var gulp = require("gulp"),
-    $ = require("gulp-load-plugins")(),
-    sprite = require("sprity"),
+    concat = require("gulp-concat"),
+    del = require("del"),
+    gulpIf = require("gulp-if"),
+    imagemin = require("gulp-imagemin"),
+    jshint = require("gulp-jshint"),
+    livereload = require("gulp-livereload"),
+    newer = require("gulp-newer"),
+    notify = require("gulp-notify"),
     path = require("path"),
-    del = require("del");
+    postcss = require("gulp-postcss"),
+    rename = require("gulp-rename"),
+    sourcemaps = require("gulp-sourcemaps"),
+    sprite = require("sprity"),
+    uglify = require("gulp-uglify");
 
 var postcssProcessors = [
   require("postcss-import"), // Must be first
   require("postcss-advanced-variables"),
-  require("postcss-atroot"),
   require("postcss-calc"),
   require("postcss-color-function"),
   require("postcss-custom-media"),
   require("postcss-mixins"),
   require("postcss-nested"),
-  require("postcss-nesting"),
   require("autoprefixer")({ browsers: ["last 1 version", "> 1%", "ie >= 10"] }),
   require("cssnano")({ discardComments: { removeAll: true }})
 ];
@@ -25,7 +33,6 @@ var sourceDir = "assets",
     imagesDir = "images",
     scriptsDir = "js",
     spriteDir = "sprite",
-    stylesFiles = ["main.css"],
     vendorsJS = ["node_modules/jquery/dist/jquery.js", "node_modules/cookies-eu-banner/dist/cookies-eu-banner.js"],
     imageminConfig = { optimizationLevel: 3, progressive: true, interlaced: true };
 
@@ -54,14 +61,14 @@ gulp.task("clean-errors", function() {
  */
 gulp.task("vendors", function() {
   return gulp.src(vendorsJS)
-    .pipe($.sourcemaps.init())
-      .pipe($.concat("vendors.js"))
-      .pipe($.uglify().on('error', $.notify.onError({
+    .pipe(newer(path.join(destDir, scriptsDir, "vendors.js")))
+    .pipe(sourcemaps.init())
+      .pipe(concat("vendors.js"))
+      .pipe(uglify().on('error', notify.onError({
         title: "Javascript error",
         message: "<%= error.message %>"
       })))
-    .pipe($.sourcemaps.write(".", { includeContent: true }))
-    .pipe($.size({ title: "Scripts (vendors)" }))
+    .pipe(sourcemaps.write(".", { includeContent: true }))
     .pipe(gulp.dest(path.join(destDir, scriptsDir)));
 });
 
@@ -69,16 +76,12 @@ gulp.task("vendors", function() {
  * Compiles SASS files
  */
 gulp.task("stylesheet", ["sprite"], function() {
-  var files = stylesFiles.map(function(filename) {
-    return path.join(sourceDir, cssDir, filename);
-  });
-
-  return gulp.src(files)
-    .pipe($.sourcemaps.init())
-      .pipe($.postcss(postcssProcessors))
-    .pipe($.sourcemaps.write(".", { includeContent: true, sourceRoot: path.join("../../", sourceDir, cssDir) }))
+  return gulp.src(path.join(sourceDir, cssDir, "main.css"))
+    .pipe(newer(path.join(destDir, cssDir)))
+    .pipe(sourcemaps.init())
+      .pipe(postcss(postcssProcessors))
+    .pipe(sourcemaps.write(".", { includeContent: true, sourceRoot: path.join("../../", sourceDir, cssDir) }))
     .on("error", function() { this.emit("end"); })
-    .pipe($.size({ title: "Stylesheet" }))
     .pipe(gulp.dest(path.join(destDir, "css/")));
 });
 
@@ -86,10 +89,10 @@ gulp.task("stylesheet", ["sprite"], function() {
  * Error-pages stylesheet
  */
 gulp.task("errors", ["clean-errors"], function() {
-  return gulp.src(path.join(errorsDir, cssDir, "main.css"))
-    .pipe($.sourcemaps.init())
-      .pipe($.postcss(postcssProcessors))
-    .pipe($.sourcemaps.write(".", { includeContent: true }))
+  return gulp.src(path.join(sourceDir, cssDir, "errors.css"))
+    .pipe(sourcemaps.init())
+      .pipe(postcss(postcssProcessors))
+    .pipe(sourcemaps.write(".", { includeContent: true }))
     .pipe(gulp.dest(path.join(errorsDir, "css/")));
 });
 
@@ -106,8 +109,8 @@ gulp.task("sprite", function() {
       style: "sprite.css",
       template: path.join(sourceDir, cssDir, "sprite-template.hbs")
     })
-    .pipe($.if("*.png", $.imagemin(imageminConfig)))
-    .pipe($.if("*.png", gulp.dest(path.join(destDir, imagesDir)), gulp.dest(path.join(sourceDir, cssDir))));
+    .pipe(gulpIf("*.png", imagemin(imageminConfig)))
+    .pipe(gulpIf("*.png", gulp.dest(path.join(destDir, imagesDir)), gulp.dest(path.join(sourceDir, cssDir))));
 });
 
 /**
@@ -115,8 +118,8 @@ gulp.task("sprite", function() {
  */
 gulp.task("images",  ["stylesheet"], function() {
   return gulp.src(path.join(sourceDir, "{" + imagesDir + ",smileys}", "*.{jpg,png,gif}"))
-    .pipe($.imagemin(imageminConfig))
-    .pipe($.size({ title: "Images" }))
+    .pipe(newer(destDir))
+    .pipe(imagemin(imageminConfig))
     .pipe(gulp.dest(destDir));
 });
 
@@ -125,14 +128,14 @@ gulp.task("images",  ["stylesheet"], function() {
  */
 gulp.task("scripts", function() {
   return gulp.src(path.join(sourceDir, scriptsDir, "*.js"))
-    .pipe($.sourcemaps.init())
-      .pipe($.concat("main.js", { newLine: "\r\n\r\n" }))
-      .pipe($.uglify().on('error', $.notify.onError({
+    .pipe(newer(path.join(destDir, scriptsDir, "main.js")))
+    .pipe(sourcemaps.init())
+      .pipe(concat("main.js", { newLine: "\r\n\r\n" }))
+      .pipe(uglify().on('error', notify.onError({
         title: "Javascript error",
         message: "<%= error.message %>"
       })))
-    .pipe($.sourcemaps.write(".", { includeContent: true, sourceRoot: path.join("../../", sourceDir, scriptsDir) }))
-    .pipe($.size({ title: "Scripts" }))
+    .pipe(sourcemaps.write(".", { includeContent: true, sourceRoot: path.join("../../", sourceDir, scriptsDir) }))
     .pipe(gulp.dest(path.join(destDir, scriptsDir)));
 });
 
@@ -141,8 +144,8 @@ gulp.task("scripts", function() {
  */
 gulp.task("jshint", function() {
   return gulp.src([path.join(sourceDir, scriptsDir, "*.js"), "!" + path.join(sourceDir, scriptsDir, "_custom.modernizr.js")])
-    .pipe($.jshint())
-    .pipe($.jshint.reporter("jshint-stylish"));
+    .pipe(jshint())
+    .pipe(jshint.reporter("jshint-stylish"));
 });
 
 /**
@@ -150,10 +153,10 @@ gulp.task("jshint", function() {
  */
 gulp.task("merge-scripts", ["vendors", "scripts"], function() {
   return gulp.src([path.join(destDir, scriptsDir, "vendors.js"), path.join(destDir, scriptsDir, "main.js")])
-    .pipe($.sourcemaps.init({ loadMaps: true }))
-      .pipe($.concat("all.js"))
-    .pipe($.sourcemaps.write(".", { includeContent: true, sourceRoot: path.join("../../", sourceDir, scriptsDir) }))
-    .pipe($.size({ title: "Scripts (all)" }))
+    .pipe(newer(path.join(destDir, scriptsDir, "all.js")))
+    .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(concat("all.js"))
+    .pipe(sourcemaps.write(".", { includeContent: true, sourceRoot: path.join("../../", sourceDir, scriptsDir) }))
     .pipe(gulp.dest(path.join(destDir, scriptsDir)));
 });
 
@@ -163,14 +166,14 @@ gulp.task("merge-scripts", ["vendors", "scripts"], function() {
 gulp.task("watch", function() {
   gulp.watch(path.join(sourceDir, scriptsDir, "*.js"), ["jshint", "scripts"]);
   gulp.watch([path.join(sourceDir, imagesDir, "*.png"), path.join(sourceDir, "smileys/*")], ["images"]);
-  gulp.watch([path.join(sourceDir, cssDir, "**/*.css"), "!" + path.join(sourceDir, cssDir, "_sprite.css")], ["stylesheet"]);
+  gulp.watch([path.join(sourceDir, cssDir, "**/*.css"), "!" + path.join(sourceDir, cssDir, "sprite.css")], ["stylesheet"]);
 
   gulp.watch("dist/*/**", function(file) {
     var filePath = path.join("static/", path.relative(path.join(__dirname, "dist/"), file.path)); // Pour que le chemin ressemble à static/.../...
-    $.livereload.changed(filePath);
+    livereload.changed(filePath);
   });
 
-  $.livereload.listen();
+  livereload.listen();
 });
 
 /**
