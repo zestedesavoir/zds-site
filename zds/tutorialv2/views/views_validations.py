5ea26db0d077c270ead242bec5bebc5fff9894e0
Emeric54 <fischer.emeric@gmail.com>
2016-02-27 22:29:14 +0100

    Envoi du commenatire de validation
    typo
    
    Ajout d'un Tu
    
    documentation
    
    fix

--- zds/tutorialv2/views/views_validations.py
+++ zds/tutorialv2/views/views_validations.py
@@ -20,6 +20,7 @@ from zds.tutorialv2.models.models_database import Validation, PublishableContent
 from zds.tutorialv2.publication_utils import publish_content, FailureDuringPublication, unpublish_content
 from zds.utils.models import SubCategory
 from zds.utils.mps import send_mp
+from zds.utils.templatetags.quote_for_mp import quote_for_mp
 import logging
 
 
@@ -418,7 +419,8 @@ class AcceptValidation(LoginRequiredMixin, PermissionRequiredMixin, ModalFormVie
                     {
                         'content': versioned,
                         'url': published.get_absolute_url_online(),
-                        'validator': validation.validator
+                        'validator': validation.validator,
+                        'message_validation': quote_for_mp(validation.comment_validator)
                     })
             else:
                 msg = render_to_string(
@@ -426,7 +428,8 @@ class AcceptValidation(LoginRequiredMixin, PermissionRequiredMixin, ModalFormVie
                     {
                         'content': versioned,
                         'url': published.get_absolute_url_online(),
-                        'validator': validation.validator
+                        'validator': validation.validator,
+                        'message_validation': quote_for_mp(validation.comment_validator)
                     })
 
             bot = get_object_or_404(User, username=settings.ZDS_APP['member']['bot_account'])
