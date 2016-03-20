a25c480b4fc5e6e8391629328ce6052da716b1d9
Augustin Laville <augustin.laville@gustavi.net>
2016-03-20 09:09:47 +0100

    ZEP-25 : mise à jour des migrations en accord avec upstream/dev

---
+++ zds/tutorialv2/migrations/0012_publishablecontent_tags.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('utils', '0001_initial'),
+        ('tutorialv2', '0011_auto_20160106_2231'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='publishablecontent',
+            name='tags',
+            field=models.ManyToManyField(db_index=True, to='utils.Tag', null=True, verbose_name=b'Tags du contenu', blank=True),
+            preserve_default=True,
+        ),
+    ]
---
+++ zds/tutorialv2/migrations/0013_auto_20160320_0908.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tutorialv2', '0012_publishablecontent_tags'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='publishablecontent',
+            name='tags',
+            field=models.ManyToManyField(to='utils.Tag', db_index=True, verbose_name=b'Tags du contenu', blank=True),
+        ),
+    ]
