e36f8799c2b713ca93b8a0de5dcef87818df4308
Augustin Laville <augustin.laville@gustavi.net>
2016-03-31 04:15:57 +0200

    Fix #3397 : corriges les incoherences du modele PublishableContent

---
+++ zds/tutorialv2/migrations/0014_auto_20160331_0415.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tutorialv2', '0013_auto_20160320_0908'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='publishablecontent',
+            name='beta_topic',
+            field=models.ForeignKey(default=None, blank=True, to='forum.Topic', null=True, verbose_name=b'Sujet beta associ\xc3\xa9'),
+        ),
+        migrations.AlterField(
+            model_name='publishablecontent',
+            name='helps',
+            field=models.ManyToManyField(to='utils.HelpWriting', db_index=True, verbose_name=b'Aides', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='publishablecontent',
+            name='source',
+            field=models.CharField(max_length=200, null=True, verbose_name=b'Source', blank=True),
+        ),
+    ]
