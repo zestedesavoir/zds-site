b597da7947d36293209929a4f1824a83e6b6d9d8
Quentin Gliech <quentingliech@gmail.com>
2016-04-05 00:17:24 +0200

    fix #3492: Corrige le design des content-item de la home

--- zds/settings.py
+++ zds/settings.py
@@ -473,7 +473,7 @@ ZDS_APP = {
         'repo_path': os.path.join(BASE_DIR, 'tutoriels-private'),
         'repo_public_path': os.path.join(BASE_DIR, 'tutoriels-public'),
         'default_license_pk': 7,
-        'home_number': 5,
+        'home_number': 3,
         'helps_per_page': 20,
         'content_per_page': 42,
         'feed_length': 5,
