73bbcff892fd77c824b28f4afc552c0313412b9f
Quentin Gliech <quentingliech@gmail.com>
2016-04-05 19:56:14 +0200

    Trailing slash des ressources de l'API optionnel

--- zds/forum/api/urls.py
+++ zds/forum/api/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from .views import PostKarmaView
 
 urlpatterns = [
-    url(r'^message/(?P<pk>\d+)/karma$', PostKarmaView.as_view(), name='post-karma'),
+    url(r'^message/(?P<pk>\d+)/karma/?$', PostKarmaView.as_view(), name='post-karma'),
 ]
