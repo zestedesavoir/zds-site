0e26c7bc9329a8f9f60f3ab7f93458b85a1643c7
Victor Felder <victorfelder@gmail.com>
2016-07-12 21:59:34 +0200

    Optimise les tests

---
+++ zds/settings_test_local.py
@@ -0,0 +1,2 @@
+from zds.settings import *
+from zds.settings_test import *
---
+++ zds/settings_test_travis.py
@@ -0,0 +1,13 @@
+from zds.settings_test import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zds_test',
+        'USER': 'travis',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+        'CONN_MAX_AGE': 600,
+    }
+}
