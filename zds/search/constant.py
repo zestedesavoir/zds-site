34ebd40634024427876882600b662eda9491c344
Gabriel Scherer <gabriel.scherer@gmail.com>
2016-07-23 18:38:08 -0400

    constant.py: retrait des offsets explicite pour simplifier la maintenance

--- zds/search/constant.py
+++ zds/search/constant.py
@@ -1,42 +1,44 @@
+MODEL_TOPIC='sujets'
+MODEL_POST='messages'
+MODEL_ARTICLE='article'
+MODEL_TUTORIAL='tutorial'
+MODEL_OPINION='opinion'
+MODEL_PART='parts'
+MODEL_CHAPTER='chapters'
+MODEL_EXTRACT='extracts'
 
 
-MODEL_NAMES = [('sujets', 'Sujets du forum'),
-               ('messages', 'Messages du forum'),
-               ('article', 'Articles'),
-               ('tutorial', 'Tutoriels'),
-               ('opinion', 'Billets'),
-               ('parts', 'Parties des tutoriels'),
-               ('chapters', 'Chapitres des tutoriels'),
-               ('extracts', 'Sections des tutoriels')]
-
+MODEL_NAMES=[
+    (MODEL_TOPIC, 'Sujets du forum'),
+    (MODEL_POST, 'Messages du forum'),
+    (MODEL_ARTICLE, 'Articles'),
+    (MODEL_TUTORIAL, 'Tutoriels'),
+    (MODEL_OPINION, 'Billets'),
+    (MODEL_PART, 'Parties des tutoriels'),
+    (MODEL_CHAPTER, 'Chapitres des tutoriels'),
+    (MODEL_EXTRACT, 'Sections des tutoriels')
+]
 
 def model_topic():
-    return MODEL_NAMES[0][0]
-
+    return MODEL_TOPIC
 
 def model_post():
-    return MODEL_NAMES[1][0]
-
+    return MODEL_POST
 
 def model_article():
-    return MODEL_NAMES[2][0]
-
+    return MODEL_ARTICLE
 
 def model_tutorial():
-    return MODEL_NAMES[3][0]
-
+    return MODEL_TUTORIAL
 
 def model_opinion():
-    return MODEL_NAMES[4][0]
-
+    return MODEL_OPINION
 
 def model_part():
-    return MODEL_NAMES[5][0]
-
+    return MODEL_PART
 
 def model_chapter():
-    return MODEL_NAMES[6][0]
-
+    return MODEL_CHAPTER
 
 def model_extract():
-    return MODEL_NAMES[7][0]
+    return MODEL_EXTRACT
