2c2e364834474485655b0b0503f48168d134b1b5
Emeric54 <fischer.emeric@gmail.com>
2016-07-31 14:42:46 +0200

    Suppression d'un warning

--- zds/poll/urls.py
+++ zds/poll/urls.py
@@ -6,10 +6,10 @@ from django.conf.urls import patterns, url
 from zds.poll.views import ListPoll, NewPoll, DetailsPoll,\
     DeletePoll, UpdatePoll
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', ListPoll.as_view(), name='poll-list'),
     url(r'^nouveau/$', NewPoll.as_view(), name='poll-new'),
     url(r'^(?P<pk>\d+)/$', DetailsPoll.as_view(), name='poll-details'),
     url(r'^(?P<pk>\d+)/update', UpdatePoll.as_view(), name='poll-update'),
     url(r'^(?P<pk>\d+)/delete$', DeletePoll.as_view(), name='poll-delete'),
-)
+]
