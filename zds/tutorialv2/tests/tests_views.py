c4182dad92092f5e29bdd86361e4064a125169e9
Chantal Gribaumont <cgribaumont@gmail.com>
2016-08-03 08:53:07 +0200

    Test correction

--- zds/tutorialv2/tests/tests_views.py
+++ zds/tutorialv2/tests/tests_views.py
@@ -1696,7 +1696,7 @@ class ContentTests(TestCase):
         # check links:
         text = versioned.children[0].get_text()
         for img in Image.objects.filter(gallery=new_article.gallery).all():
-            self.assertTrue('![]({})'.format(settings.ZDS_APP['site']['url'] + img.physical.url) in text)
+            self.assertTrue('![]({})'.format(settings.ZDS_APP['site']['secure_url'] + img.physical.url) in text)
 
         # import into first article (that will only change the images)
         result = self.client.post(
@@ -1723,7 +1723,7 @@ class ContentTests(TestCase):
         # check links:
         text = versioned.children[0].get_text()
         for img in Image.objects.filter(gallery=new_version.gallery).all():
-            self.assertTrue('![]({})'.format(settings.ZDS_APP['site']['url'] + img.physical.url) in text)
+            self.assertTrue('![]({})'.format(settings.ZDS_APP['site']['secure_url'] + img.physical.url) in text)
 
         # clean up
         os.remove(draft_zip_path)
