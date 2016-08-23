6bb2f75a9ae99666ac259610898f048529b7335b
victor felder <victorfelder@gmail.com>
2016-08-23 20:15:10 +0200

    Résout le bug de notification lors du déplacement de sujet (#3786)

---
+++ zds/notification/management/commands/fix_persistent_notifications.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+from django.core.management import BaseCommand
+from django.contrib.contenttypes.models import ContentType
+
+from zds.notification.models import Notification
+from zds.forum.models import Topic
+
+
+class Command(BaseCommand):
+    help = 'Fix all persistent notifications.'
+
+    def handle(self, *args, **options):
+        content_type = ContentType.objects.get_for_model(Topic)
+        notifications = Notification.objects.filter(content_type=content_type, is_read=False).all()
+        count = 0
+        print ""
+        for notification in notifications:
+            if notification.subscription.content_object != notification.content_object.forum:
+                notification.is_read = True
+                notification.is_dead = True
+                notification.save()
+                print notification
+                count += 1
+        print '{} notifications have been fixed'.format(count)
