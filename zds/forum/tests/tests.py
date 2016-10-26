118f59d2571c91a68dae95424e7fdcf4c06fc4fd
Victor Felder <victorfelder@gmail.com>
2016-10-26 14:19:58 +0200

    Ajoute import manquant

--- zds/forum/tests/tests.py
+++ zds/forum/tests/tests.py
@@ -14,6 +14,7 @@ from zds.forum.models import Forum, TopicRead, Post, Topic, is_read
 from zds.member.factories import ProfileFactory, StaffProfileFactory
 from zds.notification.models import TopicAnswerSubscription
 from zds.utils import slugify
+from zds.utils.forums import get_tag_by_title
 from zds.utils.models import Alert, Tag
 from zds import settings as zds_settings
 
