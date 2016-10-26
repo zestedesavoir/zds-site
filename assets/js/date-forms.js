a5b224db4ffcf0eb2c297461f9e6e2cf012657ac
pierre-24 <pierreb24@gmail.com>
2016-10-26 20:49:55 +0200

    Correct the way to handle position in the case of modal flexbox !

--- assets/js/date-forms.js
+++ assets/js/date-forms.js
@@ -3,7 +3,20 @@
     $(".date_picker_field").each(function() {
         new Pikaday({
             field: $(this)[0],
-            format: "YYYY-MM-DD"
+            format: "YYYY-MM-DD",
+            minDate : new Date(),
+            onOpen : function () {
+                // correct the way to handle position in the case of modal flexbox
+                var field = $(this._o.field);
+                if (field.parents('.modal').length > 0) {
+                    var offset = field.offset();
+                    var $window = $(window);
+                    var el = $(this.el);
+                    el.css('position', 'fixed');
+                    el.css('left', (offset.left - $window.scrollLeft()) + 'px');
+                    el.css('top', (offset.top - $window.scrollTop() + field.height()) + 'px');
+                }
+            }
         });
     });
 })(jQuery);
