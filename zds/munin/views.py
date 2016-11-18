a6f548cae0bb478df61dffb912e10a057a04e9fc
artragis <artragis@gmail.com>
2016-11-18 10:55:02 +0100

    pep8 + not published

--- zds/munin/views.py
+++ zds/munin/views.py
@@ -2,6 +2,7 @@ from munin.helpers import muninview
 from zds.forum.models import Topic, Post
 from zds.mp.models import PrivateTopic, PrivatePost
 from zds.tutorialv2.models.models_database import PublishableContent, ContentReaction
+from django.db.models import Q
 
 
 @muninview(config="""graph_title Total Topics
@@ -50,12 +51,16 @@ def total_articles(request):
 
 @muninview(config="""graph_title Total Tribunes
 graph_vlabel #tribunes
-not_promoted.label Not promoted
+not_promoted.label Not published yet
 not_promoted.draw LINE1
+not_promoted.label Not promoted
+not_promoted.draw STACK
 promoted.label Promoted as articles
 promoted.draw STACK""")
-def total_articles(request):
+def total_tribunes(request):
     tribunes = PublishableContent.objects.filter(type="OPINION").all()
-    return [("not_promoted", articles.filter(Q(promotion_content__isnull=True) | 
+    return [("not_published", tribunes.filter(sha_public__isnull=True)),
+            ("not_promoted", tribunes.filter(sha_public__isnull=False)
+                                     .filter(Q(promotion_content__isnull=True) |
                                              Q(promotion_content__sha_public__isnull=True)).count()),
-            ("promoted", articles.filter(promotion_content__sha_public__isnull=False).count())]
+            ("promoted", tribunes.filter(promotion_content__sha_public__isnull=False).count())]
