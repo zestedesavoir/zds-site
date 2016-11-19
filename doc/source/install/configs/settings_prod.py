a6a70b493a9cce161eb421f229cdba05ea59b508
Victor Felder <victorfelder@gmail.com>
2016-11-19 20:47:00 +0100

    [ci skip] conflict

--- doc/source/install/configs/settings_prod.py
+++ doc/source/install/configs/settings_prod.py
@@ -269,4 +269,4 @@ ENABLE_HTTPS_DECORATOR = True
 
 # visual changes
 #ZDS_APP['visual_changes'] = ['snow', 'clem-christmas']
-ZDS_APP['visual_changes'] = ['clem-halloween']
+#ZDS_APP['visual_changes'] = ['clem-halloween']
