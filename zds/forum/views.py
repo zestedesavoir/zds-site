2d74f2d57223b1023c4f0bc0e618bcac894a6753
AmarOk <blin.sebastien@enconn.fr>
2016-11-20 15:00:29 -0500

    enleve print

--- zds/forum/views.py
+++ zds/forum/views.py
@@ -103,7 +103,6 @@ class ForumTopicsListView(FilterMixin, ForumEditMixin, ZdSPagingListView, Update
         # Add a topic.is_followed attribute
         followed_query_set = TopicAnswerSubscription.objects.get_objects_followed_by(self.request.user.id)
         followed_topics = list(set(followed_query_set) & set(context['topics'] + sticky))
-        print(followed_topics)
         for topic in set(context['topics'] + sticky):
             topic.is_followed = topic in followed_topics
 
