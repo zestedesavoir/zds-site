e6480c645db3ca0d3bc769ba1d7c6683e033e7df
Gabriel Scherer <gabriel.scherer@gmail.com>
2016-11-20 23:30:02 -0800

    add opinions to the sitemap (#3997)
    * add opinions to the sitemap
    
    * factorize the sitemap code for content types

--- zds/urls.py
+++ zds/urls.py
@@ -15,32 +15,34 @@ from . import settings
 
 
 # SiteMap data
-class TutoSitemap(Sitemap):
+class ContentSitemap(Sitemap):
     changefreq = 'weekly'
     priority = 1
 
     def items(self):
-        return PublishedContent.objects.filter(must_redirect=False, content_type='TUTORIAL').prefetch_related('content')
+        return (
+            PublishedContent.objects
+            .filter(must_redirect=False, content_type=self.content_type)
+            .prefetch_related('content')
+        )
 
-    def lastmod(self, tuto):
-        return tuto.update_date or tuto.publication_date
+    def lastmod(self, content):
+        return content.update_date or content.publication_date
 
-    def location(self, tuto):
-        return tuto.get_absolute_url_online()
+    def location(self, content):
+        return content.get_absolute_url_online()
 
 
-class ArticleSitemap(Sitemap):
-    changefreq = 'weekly'
-    priority = 1
+class TutoSitemap(ContentSitemap):
+    content_type = "TUTORIAL"
 
-    def items(self):
-        return PublishedContent.objects.filter(must_redirect=False, content_type='ARTICLE').prefetch_related('content')
 
-    def lastmod(self, article):
-        return article.update_date or article.publication_date
+class ArticleSitemap(ContentSitemap):
+    content_type = "ARTICLE"
 
-    def location(self, article):
-        return article.get_absolute_url_online()
+
+class OpinionSitemap(ContentSitemap):
+    content_type = "OPINION"
 
 
 class PageSitemap(Sitemap):
@@ -58,6 +60,7 @@ class PageSitemap(Sitemap):
 sitemaps = {
     'tutos': TutoSitemap,
     'articles': ArticleSitemap,
+    'opinions': OpinionSitemap,
     'categories': GenericSitemap(
         {'queryset': Category.objects.all()},
         changefreq='yearly',
