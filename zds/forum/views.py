4aebba7d9ea23679b86616ceda11fefbc53babcd
tleb <tleb@openmailbox.org>
2016-11-23 21:27:56 +0100

    Update le nombre de followers sur les requêtes AJAX follow (#3920)
    * Requêtes AJAX follow sur forum retournent count
    
    * Update le nombre d'abonnés sur req AJAX follow
    
    * Retire print()
    
    * Utilise subscriber_count plutôt que count
    
    * subscriberCount pour respecter camelCase front
    
    * Ajoute CSS sur bouton

--- zds/forum/views.py
+++ zds/forum/views.py
@@ -81,6 +81,7 @@ class ForumTopicsListView(FilterMixin, ForumEditMixin, ZdSPagingListView, Update
         response = {}
         if 'follow' in request.POST:
             response['follow'] = self.perform_follow(self.object, request.user)
+            response['subscriberCount'] = NewTopicSubscription.objects.get_subscriptions(self.object).count()
         elif 'email' in request.POST:
             response['email'] = self.perform_follow_by_email(self.object, request.user)
 
@@ -305,6 +306,7 @@ class TopicEdit(UpdateView, SingleObjectMixin, TopicEditMixin):
         response = {}
         if 'follow' in request.POST:
             response['follow'] = self.perform_follow(self.object, request.user).is_active
+            response['subscriberCount'] = TopicAnswerSubscription.objects.get_subscriptions(self.object).count()
         elif 'email' in request.POST:
             response['email'] = self.perform_follow_by_email(self.object, request.user).is_active
         elif 'solved' in request.POST:
--- zds/tutorialv2/views/views_published.py
+++ zds/tutorialv2/views/views_published.py
@@ -651,8 +651,10 @@ class FollowContentReaction(LoggedWithReadWriteHability, SingleOnlineContentView
         response = {}
         self.public_content_object = self.get_public_object()
         if 'follow' in request.POST:
-            response['follow'] = ContentReactionAnswerSubscription.objects. \
-                toggle_follow(self.get_object(), self.request.user).is_active
+            response['follow'] = ContentReactionAnswerSubscription.objects\
+                .toggle_follow(self.get_object(), self.request.user).is_active
+            response['subscriberCount'] = ContentReactionAnswerSubscription\
+                .objects.get_subscriptions(self.get_object()).count()
         if self.request.is_ajax():
             return HttpResponse(json_writer.dumps(response), content_type='application/json')
         return redirect(self.get_object().get_absolute_url())
