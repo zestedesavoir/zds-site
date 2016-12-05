1b813cc73e44443dc3f30e7d446171c2bd737870
Antonin <antonin59290@hotmail.com>
2016-12-05 14:29:27 +0000

    ajout de routes, netoyage, test

--- zds/forum/api/serializer.py
+++ zds/forum/api/serializer.py
@@ -1,4 +1,5 @@
-from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from zds.forum.models import Forum, Topic, Post
 from dry_rest_permissions.generics import DRYPermissionsField
 from dry_rest_permissions.generics import DRYPermissions
@@ -8,8 +9,8 @@ class ForumSerializer(ModelSerializer):
     class Meta:
         model = Forum
         permissions_classes = DRYPermissions
-        
 
+# Renomer en ForumPostSerializer
 class ForumActionSerializer(ModelSerializer):
     """
     Serializer to create a new forum
@@ -21,11 +22,31 @@ class ForumActionSerializer(ModelSerializer):
         #fields = ('id', 'text', 'text_html', 'permissions')
     #    read_only_fields = ('text_html', 'permissions')
         read_only_fields = ('slug',)
-        
+
     def create(self, validated_data):
         new_forum = Forum.objects.create(**validated_data)
         return new_forum
 
+class ForumUpdateSerializer(ModelSerializer):
+    """
+    Serializer to update a forum.
+    """
+    can_be_empty = True
+    title = serializers.CharField(required=False, allow_blank=True)
+    subtitle = serializers.CharField(required=False, allow_blank=True)
+    # Ajouter category et eventuellement autre TODO
+    permissions = DRYPermissionsField()
+
+    class Meta:
+        model = Forum
+        fields = ('id', 'title', 'subtitle','permissions',)
+        read_only_fields = ('id','slug','permissions',) # TODO a voir si besoin d'autres champs
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
 
 
 class TopicSerializer(ModelSerializer):
@@ -34,7 +55,7 @@ class TopicSerializer(ModelSerializer):
         #fields = ('id', 'title', 'subtitle', 'slug', 'category', 'position_in_category')
         permissions_classes = DRYPermissions
 
-
+# Idem renommer
 class TopicActionSerializer(ModelSerializer):
     """
     Serializer to create a new topic.
@@ -43,12 +64,11 @@ class TopicActionSerializer(ModelSerializer):
 
     class Meta:
         model = Topic
-        read_only_fields = ('slug','author')
-        
+        read_only_fields = ('slug','author',)
+
     def create(self, validated_data):
         new_topic = Topic.objects.create(**validated_data)
         return new_topic
-
 
 
 class PostSerializer(ModelSerializer):
@@ -56,7 +76,8 @@ class PostSerializer(ModelSerializer):
         model = Post
         #fields = ('id', 'title', 'subtitle', 'slug', 'category', 'position_in_category')
         permissions_classes = DRYPermissions
-        
+
+
 class PostActionSerializer(ModelSerializer):
     """
     Serializer to send a post in a topic
@@ -73,10 +94,7 @@ class PostActionSerializer(ModelSerializer):
         # Get topic
         pk_topic = validated_data.get('topic_id')
         topic = get_object_or_404(Topic, pk=(pk_topic))
-
-        new_post = Post.objects.create(**validated_data)
-
-
+        Post.objects.create(**validated_data)
         return topic.last_message
 
     # Todo a t on besoin d'un validateur
