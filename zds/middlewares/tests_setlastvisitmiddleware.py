f953c2e81b97e72301dab2c70f88d0ef385b5fc7
Guillaume <GCodeur@users.noreply.github.com>
2016-12-08 21:52:08 +0100

    Crée un paramètre pour l'intervalle entre deux mises à jour de dernière visite (#4005)
    * Crée un paramètre pour l'intervalle entre deux mises à jour de dernière visite
    
    * Ajout d'une virgule
    
    * Chemin d'importation des settings
    
    * Fichier passé en minuscules
    
    * Complète et corrige le test sur setlastvisitmiddleware
    
    * PEP-8

---
+++ zds/middlewares/tests_setlastvisitmiddleware.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+
+from datetime import datetime, timedelta
+
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.test.utils import override_settings
+from django.shortcuts import get_object_or_404
+
+from zds.member.factories import ProfileFactory
+from zds.member.models import Profile
+from zds import settings
+
+
+overrided_zds_app = settings.ZDS_APP
+overrided_zds_app['member']['update_last_visit_interval'] = 30
+
+
+@override_settings(ZDS_APP=overrided_zds_app)
+class SetLastVisitMiddlewareTest(TestCase):
+    def setUp(self):
+        self.user = ProfileFactory()
+
+    def test_process_response(self):
+        profile_pk = self.user.pk
+
+        # login
+        self.assertEqual(
+            self.client.login(
+                username=self.user.user.username,
+                password='hostel77'),
+            True)
+
+        # set last login to a recent date
+        self.user.last_visit = datetime.now() - timedelta(seconds=10)
+        self.user.save()
+
+        # load a page
+        self.client.get(reverse('homepage'))
+
+        # the date of last visit should not have been updated
+        profile = get_object_or_404(Profile, pk=profile_pk)
+        self.assertTrue(datetime.now() - profile.last_visit > timedelta(seconds=5))
+
+        # set last login to an old date
+        self.user.last_visit = datetime.now() - timedelta(seconds=45)
+        self.user.save()
+
+        # load a page
+        self.client.get(reverse('homepage'))
+
+        # the date of last visit should have been updated
+        profile = get_object_or_404(Profile, pk=profile_pk)
+        self.assertTrue(datetime.now() - profile.last_visit < timedelta(seconds=5))
