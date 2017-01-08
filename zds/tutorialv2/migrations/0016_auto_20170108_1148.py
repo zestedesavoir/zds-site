28c36775a4e9cf0a0c1bd80405ab4bdbe5d0dbcf
pierre-24 <pierreb24@gmail.com>
2017-01-08 11:36:39 +0100

    Fix migration conflict

--- zds/tutorialv2/migrations/0016_auto_20170108_1148.py
+++ zds/tutorialv2/migrations/0016_auto_20170108_1148.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tutorialv2', '0014_auto_20160331_0415'),
+        ('tutorialv2', '0015_publishedcontent_nb_letter'),
     ]
 
     operations = [
@@ -20,5 +20,10 @@ class Migration(migrations.Migration):
             model_name='publishedcontent',
             name='es_flagged',
             field=models.BooleanField(default=True, db_index=True, verbose_name=b'Doit \xc3\xaatre (r\xc3\xa9)index\xc3\xa9 par ES'),
+        ),
+        migrations.AlterField(
+            model_name='publishedcontent',
+            name='nb_letter',
+            field=models.IntegerField(default=None, null=True, verbose_name=b'Nombre de lettres du contenu', blank=True),
         ),
     ]
