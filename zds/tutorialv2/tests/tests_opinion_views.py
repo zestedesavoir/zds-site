e400a0ccc0fa1f2c0afe57fa089c6569cf9018df
pierre-24 <pierreb24@gmail.com>
2017-01-08 21:56:29 +0100

    add test to signal opinion

--- zds/tutorialv2/tests/tests_opinion_views.py
+++ zds/tutorialv2/tests/tests_opinion_views.py
@@ -12,6 +12,7 @@ from zds.member.factories import ProfileFactory, StaffProfileFactory
 from zds.settings import BASE_DIR
 from zds.tutorialv2.factories import PublishableContentFactory, ExtractFactory, LicenceFactory, PublishedContentFactory
 from zds.tutorialv2.models.models_database import PublishableContent
+from zds.utils.models import Alert
 
 overrided_zds_app = settings.ZDS_APP
 overrided_zds_app['content']['repo_private_path'] = os.path.join(BASE_DIR, 'contents-private-test')
@@ -454,6 +455,90 @@ class PublishedContentTests(TestCase):
             },
             follow=False)
         self.assertEqual(result.status_code, 302)
+
+    def test_opinion_alert(self):
+        """Test content alert"""
+
+        text_publication = u'Aussi tôt dit, aussi tôt fait !'
+
+        opinion = PublishableContentFactory(type='OPINION')
+
+        opinion.authors.add(self.user_author)
+        UserGalleryFactory(gallery=opinion.gallery, user=self.user_author, mode='W')
+        opinion.licence = self.licence
+        opinion.save()
+
+        opinion_draft = opinion.load_version()
+        ExtractFactory(container=opinion_draft, db_object=opinion)
+        ExtractFactory(container=opinion_draft, db_object=opinion)
+
+        self.assertEqual(
+            self.client.login(
+                username=self.user_author.username,
+                password='hostel77'),
+            True)
+
+        result = self.client.post(
+            reverse('validation:publish', kwargs={'pk': opinion.pk, 'slug': opinion.slug}),
+            {
+                'text': text_publication,
+                'source': '',
+                'version': opinion.load_version().current_version
+            },
+            follow=False)
+        self.assertEqual(result.status_code, 302)
+
+        # Alert content
+        random_user = ProfileFactory().user
+
+        self.assertEqual(
+            self.client.login(
+                username=random_user.username,
+                password='hostel77'),
+            True)
+
+        result = self.client.post(
+            reverse('content:alert-content', kwargs={'pk': opinion.pk}),
+            {
+                "signal_text": u'Yeurk !'
+            }, follow=False
+        )
+
+        self.assertEqual(result.status_code, 302)
+        self.assertIsNotNone(Alert.objects.filter(author__pk=random_user.pk, content__pk=opinion.pk).first())
+
+        alert = Alert.objects.filter(author__pk=random_user.pk, content__pk=opinion.pk).first()
+        self.assertFalse(alert.solved)
+
+        result = self.client.post(
+            reverse('content:resolve-content', kwargs={'pk': opinion.pk}),
+            {
+                "alert_pk": alert.pk,
+                "text": u'Je peux ?'
+            }, follow=False
+        )
+        self.assertEqual(result.status_code, 403)  # solving the alert by yourself wont work
+
+        alert = Alert.objects.get(pk=alert.pk)
+        self.assertFalse(alert.solved)
+
+        self.assertEqual(
+            self.client.login(
+                username=self.user_staff.username,
+                password='hostel77'),
+            True)
+
+        result = self.client.post(
+            reverse('content:resolve-content', kwargs={'pk': opinion.pk}),
+            {
+                "alert_pk": alert.pk,
+                "text": u'Anéfé!'
+            }, follow=False
+        )
+        self.assertEqual(result.status_code, 302)
+
+        alert = Alert.objects.get(pk=alert.pk)
+        self.assertTrue(alert.solved)
 
     def tearDown(self):
 
