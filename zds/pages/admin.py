f5cd8638c688f2a2781412bf1c344f5945cc3405
Anto59290 <antonin59290@hotmail.com>
2017-01-12 20:11:24 +0100

    Update admin.py

--- zds/pages/admin.py
+++ zds/pages/admin.py
@@ -7,6 +7,6 @@ class GroupContactAdmin(admin.ModelAdmin):
 
     """Representation of GroupContact model in the admin interface."""
 
-    raw_id_fields = ('group', 'person_in_charge')
+    raw_id_fields = ('person_in_charge',)
 
 admin.site.register(GroupContact, GroupContactAdmin)
