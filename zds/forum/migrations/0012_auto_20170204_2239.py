0fda5a8996ecda3e17ef52dae84ee5f802c421c4
pierre-24 <pierreb24@gmail.com>
2017-02-04 22:39:46 +0100

    fix migrations (again :p)

--- zds/forum/migrations/0012_auto_20170204_2239.py
+++ zds/forum/migrations/0012_auto_20170204_2239.py
@@ -7,23 +7,28 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tutorialv2', '0015_publishedcontent_nb_letter'),
+        ('forum', '0011_auto_20170130_1823'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='publishedcontent',
+            model_name='post',
             name='es_already_indexed',
             field=models.BooleanField(default=False, db_index=True, verbose_name=b'D\xc3\xa9j\xc3\xa0 index\xc3\xa9 par ES'),
         ),
         migrations.AddField(
-            model_name='publishedcontent',
+            model_name='post',
             name='es_flagged',
             field=models.BooleanField(default=True, db_index=True, verbose_name=b'Doit \xc3\xaatre (r\xc3\xa9)index\xc3\xa9 par ES'),
         ),
-        migrations.AlterField(
-            model_name='publishedcontent',
-            name='nb_letter',
-            field=models.IntegerField(default=None, null=True, verbose_name=b'Nombre de lettres du contenu', blank=True),
+        migrations.AddField(
+            model_name='topic',
+            name='es_already_indexed',
+            field=models.BooleanField(default=False, db_index=True, verbose_name=b'D\xc3\xa9j\xc3\xa0 index\xc3\xa9 par ES'),
+        ),
+        migrations.AddField(
+            model_name='topic',
+            name='es_flagged',
+            field=models.BooleanField(default=True, db_index=True, verbose_name=b'Doit \xc3\xaatre (r\xc3\xa9)index\xc3\xa9 par ES'),
         ),
     ]
--- zds/tutorialv2/migrations/0017_auto_20170204_2239.py
+++ zds/tutorialv2/migrations/0017_auto_20170204_2239.py
@@ -7,27 +7,17 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('forum', '0010_auto_20161112_1823'),
+        ('tutorialv2', '0016_auto_20170201_1940'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='post',
+            model_name='publishedcontent',
             name='es_already_indexed',
             field=models.BooleanField(default=False, db_index=True, verbose_name=b'D\xc3\xa9j\xc3\xa0 index\xc3\xa9 par ES'),
         ),
         migrations.AddField(
-            model_name='post',
-            name='es_flagged',
-            field=models.BooleanField(default=True, db_index=True, verbose_name=b'Doit \xc3\xaatre (r\xc3\xa9)index\xc3\xa9 par ES'),
-        ),
-        migrations.AddField(
-            model_name='topic',
-            name='es_already_indexed',
-            field=models.BooleanField(default=False, db_index=True, verbose_name=b'D\xc3\xa9j\xc3\xa0 index\xc3\xa9 par ES'),
-        ),
-        migrations.AddField(
-            model_name='topic',
+            model_name='publishedcontent',
             name='es_flagged',
             field=models.BooleanField(default=True, db_index=True, verbose_name=b'Doit \xc3\xaatre (r\xc3\xa9)index\xc3\xa9 par ES'),
         ),
