45b4f1a2a9db2630fdf02035d1601d1f05753b8f
GCodeur <guillaume@gcodeur.fr>
2017-03-03 19:30:15 +0100

    Issue -> Ticket (modèle)

--- zds/utils/migrations/0011_comment_github_issue.py
+++ zds/utils/migrations/0011_comment_github_issue.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='comment',
             name='github_issue',
-            field=models.IntegerField(null=True, verbose_name='Issue GitHub', blank=True),
+            field=models.IntegerField(null=True, verbose_name='Ticket GitHub', blank=True),
         ),
     ]
--- zds/utils/models.py
+++ zds/utils/models.py
@@ -179,7 +179,7 @@ class Comment(models.Model):
         auto_now=True,
         db_index=True)
 
-    github_issue = models.IntegerField('Issue GitHub', null=True, blank=True)
+    github_issue = models.IntegerField('Ticket GitHub', null=True, blank=True)
 
     is_visible = models.BooleanField('Est visible', default=True)
     text_hidden = models.CharField(
