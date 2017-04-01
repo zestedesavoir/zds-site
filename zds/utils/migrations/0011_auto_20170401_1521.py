f71e45e833da3442c3cd185f81b4ce39aa655d99
Victor Felder <victor@draft.li>
2017-04-01 15:22:22 +0200

    Refait les migrations suite au rebase

---
+++ zds/utils/migrations/0011_auto_20170401_1521.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tutorialv2', '0020_auto_20170401_1521'),
+        ('utils', '0010_auto_20170203_2100'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='alert',
+            name='content',
+            field=models.ForeignKey(related_name='alerts_on_this_content', verbose_name='Contenu', blank=True, to='tutorialv2.PublishableContent', null=True),
+        ),
+        migrations.AlterField(
+            model_name='alert',
+            name='comment',
+            field=models.ForeignKey(related_name='alerts_on_this_comment', verbose_name='Commentaire', blank=True, to='utils.Comment', null=True),
+        ),
+        migrations.AlterField(
+            model_name='alert',
+            name='scope',
+            field=models.CharField(db_index=True, max_length=10, choices=[('FORUM', 'Forum'), ('CONTENT', 'Contenu'), (b'TUTORIAL', 'Tutoriel'), (b'ARTICLE', 'Article'), (b'OPINION', 'Billet')]),
+        ),
+    ]
