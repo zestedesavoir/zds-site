12ccc1f5d86b944b3881afb8b8fdbcc24001813e
GCodeur <guillaume@gcodeur.fr>
2017-04-07 14:50:51 +0200

    Historique corrigé + republication interdite

--- assets/js/tribune-pick.js
+++ assets/js/tribune-pick.js
@@ -1,24 +1,24 @@
 (function ($, undefined) {
     var $opinions = $(".opinion");
     $opinions.on("click", ".unpick-action", function () {
-       var $button = $(this);
-       var $row = $button.parent().parent();
-       var csrfmiddlewaretoken = $("input[name='csrfmiddlewaretoken']").val();
-       var data = {
-           csrfmiddlewaretoken: csrfmiddlewaretoken
-       };
-       if ($button.data("operation")) {
-           data.operation = $button.data("operation");
-       }
-       $.post($button.attr("data-url"), data).done(function () {
-           if (data.operation) {
-               $row.remove();
-           }
-           else {
-               $button.prop("disabled", true);
-               var $stateCol = $row.find(".state");
-               $stateCol.text($stateCol.data("toogle"));
-           }
-       });
+        var $button = $(this);
+        var $row = $button.parent().parent();
+        var csrfmiddlewaretoken = $("input[name='csrfmiddlewaretoken']").val();
+        var data = {
+            csrfmiddlewaretoken: csrfmiddlewaretoken
+        };
+        if ($button.data("operation")) {
+            data.operation = $button.data("operation");
+        }
+        $.post($button.attr("data-url"), data).done(function () {
+            if (data.operation) {
+                $row.remove();
+            }
+            else {
+                $button.prop("disabled", true);
+                var $stateCol = $row.find(".state");
+                $stateCol.text($stateCol.data("toggle"));
+            }
+        });
     });
 })(jQuery);
