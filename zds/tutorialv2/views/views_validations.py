e73fd054cdc5ba9c737981cb65052add1eea6eb2
artragis <artragis@gmail.com>
2017-04-19 18:46:35 +0200

    [v23]Fix les retours de firm1 (#4310)
    * fix 4305
    
    * fix 4309
    
    * fix 4308
    
    * use is_opinion
    
    * fix #4311
    
    * remet la date dans la liste globale
    
    * fix la liste des tags dans le contenue et simplifie la condition
    
    * use good condition
    
    * Corrige le nom d'une variable

--- zds/tutorialv2/views/views_validations.py
+++ zds/tutorialv2/views/views_validations.py
@@ -930,7 +930,9 @@ class PromoteOpinionToArticle(PermissionRequiredMixin, NoValidationBeforeFormVie
                                      public_version=None,
                                      licence=db_object.licence,
                                      sha_validation=db_object.sha_public,
-                                     sha_draft=db_object.sha_public
+                                     sha_draft=db_object.sha_public,
+                                     image=db_object.image,
+                                     source=db_object.source
                                      )
 
         opinion_url = db_object.get_absolute_url_online()
