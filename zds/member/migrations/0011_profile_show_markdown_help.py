6d38beb01c21c01ceaa9e71e12e251f56184c125
GCodeur <guillaume@gcodeur.fr>
2017-05-09 19:23:49 +0200

    Nom et dépendance du fichier de migration

--- zds/member/migrations/0011_profile_show_markdown_help.py
+++ zds/member/migrations/0011_profile_show_markdown_help.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('member', '0009_profile_show_staff_badge'),
+        ('member', '0010_bannedemailprovider_newemailprovider'),
     ]
 
     operations = [
