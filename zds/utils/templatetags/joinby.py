e1e2a842d42d6c33bb8cc12ff240910c064e39a5
Michael Marx <michael-marx@protonmail.ch>
2017-07-04 01:05:40 +0200

    Ajoute des champs DCMI aux pages des contenus (#4261)

---
+++ zds/utils/templatetags/joinby.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+from django import template
+from django.utils.translation import ugettext_lazy as _
+
+register = template.Library()
+
+
+@register.simple_tag
+def joinby(values, separator=", ", same_final_separator=False):
+    """
+    Returns a human readable list (of type string) of values separated
+    by commas (excepted the last preceded by "et") from an iterable list.
+    Set 'same_final_separator' to True to make the last also preceded
+    by a comma.
+    """
+    if not values:
+        return ""
+    if len(values) == 1:
+        return values[0].__str__()
+    if same_final_separator:
+        final_sep = separator
+    else:
+        final_sep = unicode(_(" et "))
+    return separator.join(map(str, values[:len(values)-1])) + (
+        final_sep + values.last().__str__()
+    )
