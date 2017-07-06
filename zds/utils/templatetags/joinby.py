950821bfe94d84731beccc185ffb1082436ae9c3
Michael Marx <michael-marx@protonmail.ch>
2017-07-06 18:20:08 +0200

    Corrige le commit précédent pour les champs DCMI (#4392)

--- zds/utils/templatetags/joinby.py
+++ zds/utils/templatetags/joinby.py
@@ -7,21 +7,22 @@ register = template.Library()
 
 
 @register.simple_tag
-def joinby(values, separator=", ", same_final_separator=False):
+def joinby(values, separator=', ', same_final_separator=False):
     """
     Returns a human readable list (of type string) of values separated
-    by commas (excepted the last preceded by "et") from an iterable list.
+    by a string definable with 'separator' (commas default, excepted
+    the last preceded by "et") from an iterable.
     Set 'same_final_separator' to True to make the last also preceded
-    by a comma.
+    by the same separator.
     """
     if not values:
-        return ""
+        return ''
     if len(values) == 1:
-        return values[0].__str__()
+        return str(values[0])
     if same_final_separator:
         final_sep = separator
     else:
-        final_sep = unicode(_(" et "))
-    return separator.join(map(str, values[:len(values)-1])) + (
-        final_sep + values.last().__str__()
+        final_sep = _(" et ")
+    return separator.join(map(str, values[:len(values) - 1])) + (
+        final_sep + str(values.last())
     )
