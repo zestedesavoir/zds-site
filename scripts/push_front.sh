18acebbca90722f4e29c8ecb49d3df0e09d9459f
Victor Felder <victor@draft.li>
2017-07-09 22:39:49 +0200

    travis fix

--- scripts/push_front.sh
+++ scripts/push_front.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 #
-# Pushes front files to a new tag on GitHub
+# Push front files to a new tag on GitHub
 
 if [ "$#" -ne 1 ]; then
-	echo "Usage: $0 <tag to build>" >&2
-	exit 1
+  echo "Usage: $0 <tag to build>" >&2
+  exit 1
 fi
 
-# Adding GitHub OAuth token to login
-git config --global user.name "ZDS-Bot"
-git config --global user.email "zestedesavoir@gmail.com"
-git config --global push.default simple
+# Adding GitHub login
+git config user.name "ZDS-Bot"
+git config user.email "zestedesavoir@gmail.com"
 
 echo "Pushing front to GitHub"
 
@@ -25,6 +24,7 @@ git commit -m "Automatic front build"
 TAG_NAME=$1-build
 git tag -a $TAG_NAME -m "$1 with built front files"
 git push https://${GITHUB_TOKEN}@github.com/zestedesavoir/zds-site $TAG_NAME
+
 if [ $? -eq 0 ]
 then
   echo "Front pushed to tag $TAG_NAME !" >&2
