9a5c3863173d0dc549090f92485955fac60aaaff
artragis <artragis@gmail.com>
2017-07-20 19:30:01 +0200

    prepare epub_work

--- zmarkdown/markdown-server.js
+++ zmarkdown/markdown-server.js
@@ -74,6 +74,43 @@ const server = new zerorpc.Server({
       reply(null, [content, metadata], streaming)
     })
   },
+  toLatex: function toLatex(markdown, opts = {}, reply) {
+    const s = Date.now()
+    if (typeof markdown !== 'string') markdown = String(markdown)
+
+
+    /* if this zmd parser doesn't exist, we create it with the right config */
+    const key = JSON.stringify(opts)
+    if (!processors.hasOwnProperty(key)) {
+      const config = clone(defaultConfig)
+
+      config.headingShifter = 0
+      config.ping.pingUsername = () => false
+
+      if (opts.disable_jsfiddle && opts.disable_jsfiddle === true) {
+        config.iframes['jsfiddle.net'].disabled = true
+        config.iframes['www.jsfiddle.net'].disabled = true
+      }
+
+      processors[key] = zmarkdown(config, 'latex')
+    }
+
+
+    processors[key].renderString(String(markdown), (err, {content, metadata}) => {
+      if (err) return reply(err, markdown, streaming)
+
+      i += 1
+      times.push(Date.now() - s)
+
+      if (i % 500 === 0) {
+        i = 0
+        t += 1
+        console.log(`${t * 500} avg`, times.reduce((sum, value) => sum + value, 0) / 500)
+        times = []
+      }
+      reply(null, [content, metadata], streaming)
+    })
+  },
 }, heartbeat)
 
 server.bind('tcp://0.0.0.0:24242')
