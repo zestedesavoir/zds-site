4f96df6144268f2fcb27813d955e89e93e2ae904
fdambrine <francois.dambrine@vadesecure.com>
2017-07-31 10:01:23 +0200

    fix settings issue

--- zds/middlewares/tests_setlastvisitmiddleware.py
+++ zds/middlewares/tests_setlastvisitmiddleware.py
@@ -12,11 +12,11 @@ from zds.member.models import Profile
 from django.conf import settings
 from copy import deepcopy
 
-overrided_zds_app = deepcopy(settings.ZDS_APP)
-overrided_zds_app['member']['update_last_visit_interval'] = 30
+overriden_zds_app = deepcopy(settings.ZDS_APP)
+overriden_zds_app['member']['update_last_visit_interval'] = 30
 
 
-@override_settings(ZDS_APP=overrided_zds_app)
+@override_settings(ZDS_APP=overriden_zds_app)
 class SetLastVisitMiddlewareTest(TestCase):
     def setUp(self):
         self.user = ProfileFactory()
