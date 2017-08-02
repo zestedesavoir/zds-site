11890c81030fb8b2432a9ceb708f24624ffac532
Quentin Gliech <quentingliech@gmail.com>
2017-08-02 20:44:55 +0200

    Correction du chemin vers le CSS des EPUBs
    Pas testé.

--- zds/settings.py
+++ zds/settings.py
@@ -499,7 +499,7 @@ ZDS_APP = {
         'https://zestedesavoir.com/articles/222/la-ligne-editoriale-officielle-de-zeste-de-savoir/',
         'epub_stylesheets': {
             'toc': 'toc.css',
-            'full': 'full.css'
+            'full': os.path.join(BASE_DIR, 'dist', 'css', 'zmd.css'),
         }
     },
     'forum': {
@@ -642,4 +642,5 @@ if DEBUG:
     INSTALLED_APPS += (
         'debug_toolbar',
     )
-    MIDDLEWARE_CLASSES = ('debug_toolbar.middleware.DebugToolbarMiddleware',) + MIDDLEWARE_CLASSES
+    MIDDLEWARE_CLASSES = (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',) + MIDDLEWARE_CLASSES
