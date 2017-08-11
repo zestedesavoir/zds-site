85b4ddff9348b1892c204a6fbd82c742b43bfd5a
Victor Felder <victor@draft.li>
2017-08-11 20:34:12 +0200

    Update lockfile
    Adapte travis suite au rebase
    
    Enlève une tâche gulp qui n'existe plus
    
    Installe les dépendances pour zmd
    
    Fix les feuilles de style
    
    Supprime classe CSS inutilisée wf-active
    
    zmarkdown: extrait les controlleurs express
    
    zmarkdown: upgrade
    
    Fix a few tests
    
    Améliore le reporting sentry node
    
    Memoize séparément les compilos html et latex
    
    Upgrade zmarkdown

--- zmarkdown/markdown-server.js
+++ zmarkdown/markdown-server.js
@@ -1,162 +1,63 @@
-// TODO zmd: Implement Sentry monitoring
-const clone = require('clone')
+/* eslint-disable no-console */
 const express = require('express')
 const bodyParser = require('body-parser')
+const {dirname, join} = require('path')
 
-const zmarkdown = require('zmarkdown')
-/* TODO zmd: use proper config for zestedesavoir */
-const defaultConfig = require('zmarkdown/config')
+const zmdVersion = require(join(dirname(require.resolve('zmarkdown')), 'package.json')).version
 
+const git = require('git-rev')
 
-const app = express()
-app.use(bodyParser.json())
+const Raven = require('raven')
+Raven.config(process.env.SENTRY_DSN, {
+  release: zmdVersion,
+  environment: process.env.SENTRY_ENVIRONMENT || process.env.ZDS_ENVIRONMENT
+}).install()
 
-/* temporary fix to prevent tests from hitting the API */
-defaultConfig.ping.pingUsername = () => true
-/* TODO zmd: above section should be gone by the time we release */
-
-const processors = {}
-let times = []
-let i = 0
-let t = 0
-
-app.post('/latex', (req, res) => {
-  toLatex(req.body.md, req.body.opts, (err, result) => {
-    res.json(result)
-  })
-})
-
-app.post('/latex-document', (req, res) => {
-  toLatexDocument(req.body.md, req.body.opts, (err, result) => {
-    res.json(result)
-  })
-})
-
-app.post('/html', (req, res) => {
-  toHTML(req.body.md, req.body.opts, (err, result) => {
-    res.json(result)
-  })
-})
-
-const server = app.listen(27272, () => {
-  const host = server.address().address
-  const port = server.address().port
-
-  console.warn("zmarkdown server listening at http://%s:%s", host, port)
-})
-
-function toHTML(markdown, opts = {}, callback) {
-  const s = Date.now()
-
-  if (typeof markdown !== 'string') markdown = String(markdown)
-
-  /* if this zmd parser doesn't exist, we create it with the right config */
-  const key = JSON.stringify(opts)
-  if (!processors.hasOwnProperty(key)) {
-    const config = clone(defaultConfig)
-
-    config.headingShifter = 2
-
-    /* presets */
-    if (opts.disable_ping && opts.disable_ping === true) {
-      config.ping.pingUsername = () => false
+git.long((commit) => {
+  Raven.mergeContext({
+    release: process.env.SENTRY_RELEASE || zmdVersion,
+    tags: {
+      'zds-site_git-commit': commit,
     }
+  })
 
-    if (opts.disable_jsfiddle && opts.disable_jsfiddle === true) {
-      config.iframes['jsfiddle.net'].disabled = true
-      config.iframes['www.jsfiddle.net'].disabled = true
-    }
+  serverStart()
+})
 
-    if (opts.inline && opts.inline === true) {
-      config.disableTokenizers = {
-        block: [
-          'indentedCode',
-          'fencedCode',
-          'blockquote',
-          'atxHeading',
-          'setextHeading',
-          'footnote',
-          'table',
-          'custom_blocks'
-        ]
+function serverStart () {
+  const {
+    toHTML,
+    toLatex,
+    toLatexDocument,
+  } = require('./markdown-handlers')(Raven)
+
+  const app = express()
+
+  app.use(Raven.requestHandler())
+  app.use(Raven.errorHandler())
+
+  app.use(bodyParser.json())
+  app.post('/latex', controllerFactory(toLatex))
+  app.post('/latex-document', controllerFactory(toLatexDocument))
+  app.post('/html', controllerFactory(toHTML))
+
+  const server = app.listen(27272, () => {
+    const host = server.address().address
+    const port = server.address().port
+
+    console.warn('zmarkdown server listening at http://%s:%s', host, port)
+  })
+}
+
+function controllerFactory (handler) {
+  return (req, res) => {
+    handler(req.body.md, req.body.opts, (err, result) => {
+      if (err) {
+        Raven.captureException(err, {req})
+        res.status(500).send(err)
       }
-    }
 
-    processors[key] = zmarkdown(config, 'html')
+      res.json(result)
+    })
   }
-
-  processors[key].renderString(String(markdown), (err, {content, metadata}) => {
-    if (err) return callback(err, markdown)
-
-    i += 1
-    times.push(Date.now() - s)
-
-    if (i % 500 === 0) {
-      i = 0
-      t += 1
-      console.log(`${t * 500} avg`, times.reduce((sum, value) => sum + value, 0) / 500)
-      times = []
-    }
-
-    callback(null, [content, metadata])
-  })
 }
-
-function toLatex(markdown, opts = {}, callback) {
-  const s = Date.now()
-  if (typeof markdown !== 'string') markdown = String(markdown)
-
-  /* if this zmd parser doesn't exist, we create it with the right config */
-  const key = JSON.stringify(opts)
-  if (!processors.hasOwnProperty(key)) {
-    const config = clone(defaultConfig)
-
-    config.headingShifter = 0
-    config.ping.pingUsername = () => false
-
-    if (opts.disable_jsfiddle && opts.disable_jsfiddle === true) {
-      config.iframes['jsfiddle.net'].disabled = true
-      config.iframes['www.jsfiddle.net'].disabled = true
-    }
-
-    processors[key] = zmarkdown(config, 'latex')
-  }
-
-  processors[key].renderString(String(markdown), (err, content) => {
-    if (err) return callback(err, markdown)
-
-    callback(null, [content, {}])
-  })
-}
-
-
-function toLatexDocument(markdown, opts = {}, callback) {
-  console.log({markdown})
-  toLatex(markdown, opts, (err, [content, metadata]) => {
-    if (err) return callback(err)
-    const {
-      contentType,
-      title,
-      authors,
-      license,
-      smileysDirectory,
-      toc = true,
-      latex = content,
-    } = opts
-    try {
-      const latexDocument = zmarkdown().latexDocumentTemplate({
-        contentType,
-        title,
-        authors,
-        license,
-        smileysDirectory,
-        toc,
-        latex,
-      })
-      callback(null, [latexDocument, {}])
-    } catch (e) {
-      callback(e)
-    }
-  })
-}
-
