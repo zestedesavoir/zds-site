c9daa978945eb33f9211c04797b511b290f373a6
Victor Felder <victor@draft.li>
2017-08-11 21:53:29 +0200

    Enlève une tâche gulp qui n'existe plus

--- Gulpfile.js
+++ Gulpfile.js
@@ -91,7 +91,7 @@ gulp.task('js', () =>
         .pipe(gulp.dest('dist/js/')));
 
 // Compiles the SCSS files to CSS
-gulp.task('css', ['css:sprite', 'css:vendors'], () =>
+gulp.task('css', ['css:sprite'], () =>
     gulp.src(['assets/scss/main.scss', 'assets/scss/zmd.scss'])
         .pipe(sourcemaps.init())
         .pipe(customSass())
