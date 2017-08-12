c46a053cdae8c83784be61257db51ca211e654ad
artragis <artragis@gmail.com>
2017-08-12 18:35:57 +0200

    review comment

--- zds/utils/templatetags/remove_url_scheme.py
+++ zds/utils/templatetags/remove_url_scheme.py
@@ -8,7 +8,16 @@ register = template.Library()
 @register.filter('remove_url_scheme')
 def remove_url_scheme(input_url):
     """
-    make every image url pointing to this website protocol independant so that https is not broken
+    make every image url pointing to this website protocol independant so that if we use https, we are sure
+    that all our media are served with this protocol.
+
+    .. notice::
+
+        this also removes the ``settings.ZDS_APP['site']['dns']`` from the url.
+
+    :return: the url without its scheme, e.g. ``http://zestedesavoir.com/media/gallery/1/1.png`` becomes
+    ``/media/gallery/1/1.png``
+
     """
     if settings.ZDS_APP['site']['dns'] in input_url or settings.ZDS_APP['site']['url'] in input_url:
         return input_url.replace('http:/', 'https:/')\
