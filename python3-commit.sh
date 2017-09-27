92a049db125ff9715cd928362dc270da55b22d5a
Antoine Motet <antoine.motet@gmail.com>
2017-09-27 19:09:32 +0200

    Revert "REVERT ME LATER"
    This reverts commit e0582f7dc76a410795e63749e92e82ebe95ad920.

--- python3-commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-
-./python3.sh
-
-git commit -a -m ':snake: :three:  ·  2to3' -m 'Ce commit a été fait automatiquement par 2to3.'
-git revert --no-edit $(git log --oneline --grep='REVERT ME LATER' | cut -f1 -d' ')
