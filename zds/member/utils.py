4aea92971989a93c52b52f5e606ec90eabf96b83
tleb <tleb@openmailbox.org>
2017-10-08 21:10:04 +0200

    Minor refactorings

--- zds/member/utils.py
+++ zds/member/utils.py
@@ -5,6 +5,9 @@ from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse
 import logging
+
+from zds.notification.models import TopicAnswerSubscription
+
 logger = logging.getLogger(__name__)
 
 
@@ -23,3 +26,30 @@ class ZDSCustomizeSocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
 
     def get_redirect_uri(self, *_, **__):
         return reverse('member-login')
+
+
+def add_user_to_group(request, user, group):
+    user.groups.add(group)
+    messages.success(request, _('{0} appartient maintenant au groupe {1}.')
+                     .format(user.username, group.name))
+
+
+def remove_user_from_group(request, user, group):
+    user.groups.remove(group)
+    messages.warning(request, _('{0} n\'appartient maintenant plus au groupe {1}.')
+                     .format(user.username, group.name))
+    topics_followed = TopicAnswerSubscription.objects.get_objects_followed_by(user)
+    for topic in topics_followed:
+        if isinstance(topic, Topic) and group in topic.forum.groups.all():
+            TopicAnswerSubscription.objects.toggle_follow(topic, user)
+
+
+def remove_user_from_every_group(request, user, usergroups):
+    for group in usergroups:
+        topics_followed = TopicAnswerSubscription.objects.get_objects_followed_by(user)
+        for topic in topics_followed:
+            if isinstance(topic, Topic) and group in topic.forum.groups.all():
+                TopicAnswerSubscription.objects.toggle_follow(topic, user)
+    user.groups.clear()
+    messages.warning(request, _('{0} n\'appartient (plus ?) à aucun groupe.')
+                     .format(user.username))
