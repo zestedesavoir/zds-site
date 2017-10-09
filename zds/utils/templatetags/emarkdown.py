83cff1ed19eadf7b81dd132920978cc8c9768e41
Antoine Motet <antoine.motet@gmail.com>
2017-10-09 18:06:44 +0200

    Refactor utils.emarkdown.render_markdown
    J’ai essayé de séparer la logique rééssayant jusqu’à 3 fois de faire
    un rendu en cas d’erreur du reste. J’ai également essayé d’améliorer
    le logging des erreurs.

--- zds/utils/templatetags/emarkdown.py
+++ zds/utils/templatetags/emarkdown.py
@@ -19,18 +19,25 @@ Markdown related filters.
 """
 
 # Constants
-MAX_ATTEMPS = 3
+MAX_ATTEMPTS = 3
 MD_PARSING_ERROR = _('Une erreur est survenue dans la génération de texte Markdown. Veuillez rapporter le bug.')
 
 
-def render_markdown(md_input, **kwargs):
+def _render_markdown_once(md_input, **kwargs):
     """
-    Render a markdown string.
+    Returns None on error (error details are logged). No retry mechanism.
+
     """
-    attempts = kwargs.get('attempts', 0)
+    def log_args():
+        logger.error('md_input: {!r}'.format(md_input))
+        logger.error('kwargs: {!r}'.format(kwargs))
+
     inline = kwargs.get('inline', False) is True
     to_latex = kwargs.get('to_latex', False) is True
     to_latex_document = kwargs.get('to_latex_document', False) is True
+
+    if settings.ZDS_APP['zmd']['disable_pings'] is True:
+        kwargs['disable_ping'] = True
 
     endpoint = '/html'
     if to_latex:
@@ -38,36 +45,68 @@ def render_markdown(md_input, **kwargs):
     if to_latex_document:
         endpoint = '/latex-document'
 
-    metadata = {}
-
-    if settings.ZDS_APP['zmd']['disable_pings'] is True:
-        kwargs['disable_ping'] = True
-
     try:
         response = post('{}{}'.format(settings.ZDS_APP['zmd']['server'], endpoint), json={
             'opts': kwargs,
             'md': str(md_input),
         }, timeout=10)
+    except requests.HTTPError:
+        logger.exception('An HTTP error happened, markdown rendering failed')
+        log_args()
+        return None
+
+    if response.status_code != 200:
+        logger.error('The markdown server replied with status {} (expected 200)'.format(response.status_code))
+        log_args()
+        return None
+
+    try:
         content, metadata = response.json()
         content = content.strip()
         if inline:
             content = content.replace('</p>\n', '\n\n').replace('\n<p>', '\n')
         return mark_safe(content), metadata
-    except (requests.HTTPError, ValueError):
-        logger.info('Markdown render failed, attempt {}/{}'.format(attempts, MAX_ATTEMPS), md_input, kwargs)
-        logger.exception('Could not generate markdown')
     except:  # noqa
-        logger.exception('Unexpected exception raised, attempt {}/{}'.format(attempts, MAX_ATTEMPS), md_input, kwargs)
+        logger.exception('Unexpected exception raised')
+        log_args()
+        return None
 
-    if attempts < MAX_ATTEMPS:
+
+def render_markdown(md_input, **kwargs):
+    """Render a markdown string.
+
+    Returns a tuple ``(rendered_content, metadata)``, where
+    ``rendered_content`` is a string and ``metadata`` is a dict.
+
+    Handles errors gracefully by returning an user-friendly HTML
+    string which explains that the Markdown rendering has failed
+    (without any technical details).
+
+    """
+    result = _render_markdown_once(md_input, **kwargs)
+    if result is not None:
+        # Success!
+        return result
+
+    # Oops, something went wrong
+
+    attempts = kwargs.get('attempts', 0)
+    inline = kwargs.get('inline', False) is True
+
+    if attempts < MAX_ATTEMPTS:
         if not kwargs:
             kwargs = dict()
         return render_markdown(md_input, **dict(kwargs, attempts=attempts + 1))
 
+    logger.error('Max attempt count reached, giving up')
+    logger.error('md_input: {!r}'.format(md_input))
+    logger.error('kwargs: {!r}'.format(kwargs))
+
+    # FIXME: This cannot work with LaTeX.
     if inline:
-        return mark_safe('<p>{}</p>'.format(MD_PARSING_ERROR)), metadata
+        return mark_safe('<p>{}</p>'.format(MD_PARSING_ERROR)), {}
     else:
-        return mark_safe('<div class="error ico-after"><p>{}</p></div>'.format(MD_PARSING_ERROR)), metadata
+        return mark_safe('<div class="error ico-after"><p>{}</p></div>'.format(MD_PARSING_ERROR)), {}
 
 
 @register.filter(needs_autoescape=False)
