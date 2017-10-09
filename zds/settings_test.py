21055f17d086d48c3be9781aca26bad837f4496f
Antoine Motet <antoine.motet@gmail.com>
2017-10-09 19:17:47 +0200

    Rend Django bavard pendant les tests
    Avant ce commit, le logging était désactivé : Plutôt pénible.  Prends
    effet lors des tests en local et sur Travis.

--- zds/settings_test.py
+++ zds/settings_test.py
@@ -5,3 +5,31 @@ PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.MD5PasswordHasher',
     'django.contrib.auth.hashers.SHA1PasswordHasher',
 )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(module)s %(message)s',
+        },
+    },
+
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+        'zds': {
+            'handlers': ['console'],
+        },
+    }
+}
