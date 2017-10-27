fb04f6e4df0cef4f6b7943c3e9c97985d5ede4e9
fdambrine <francois.dambrine@vadesecure.com>
2017-10-27 17:29:09 +0200

    Use new zmd licence syntax

--- Gulpfile.js
+++ Gulpfile.js
@@ -112,10 +112,11 @@ gulp.task('css:sprite', () =>
         .pipe(gulp.dest('dist/')));
 
 // Optimizes the images
-gulp.task('images', ['css:sprite'], () =>
-    gulp.src('assets/{images,smileys}/**/*')
+gulp.task('images', ['css:sprite'], () => {
+    gulp.src('assets/{images,smileys,licenses}/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/')));
+        .pipe(gulp.dest('dist/'));
+});
 
 // Watch for file changes
 gulp.task('watch-runner', () => {
