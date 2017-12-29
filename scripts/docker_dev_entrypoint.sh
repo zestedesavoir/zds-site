f368522ed62545c1d799b7862dc57da199b9756a
Antoine Motet <antoine.motet@gmail.com>
2017-12-29 19:21:48 +0100

    Préférrer DJANGO_SETTINGS_MODULE à --settings
    Ça fait moins de répétitions.

--- scripts/docker_dev_entrypoint.sh
+++ scripts/docker_dev_entrypoint.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$0")"
 
 cd ..
 
-python manage.py migrate --settings zds.settings.docker_dev
-exec python manage.py runserver 0.0.0.0:8000 --settings zds.settings.docker_dev
+python manage.py migrate
+exec python manage.py runserver 0.0.0.0:8000
