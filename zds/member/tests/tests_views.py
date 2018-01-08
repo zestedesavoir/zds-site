8dc75cfdcce5127fdcaf674b410c00224bcfb6cf
Michael Marx <michael-marx@protonmail.ch>
2018-01-08 20:57:29 +0100

    Ajoute des tests unitaires

--- zds/member/tests/tests_views.py
+++ zds/member/tests/tests_views.py
@@ -341,6 +341,28 @@ class MemberTests(TestCase):
              'remember': 'remember'},
             follow=False)
         self.assertEqual(result.status_code, 200)
+        self.assertContains(result, _(
+                'Le mot de passe saisi est incorrect. '
+                'Cliquez sur le lien « Mot de passe oublié ? » '
+                'si vous ne vous en souvenez plus.'
+            )
+        )
+
+        # login failed with bad username then no redirection
+        # (status_code equals 200 and not 302).
+        result = self.client.post(
+            reverse('member-login'),
+            {'username': 'clem',
+             'password': 'hostel77',
+             'remember': 'remember'},
+            follow=False)
+        self.assertEqual(result.status_code, 200)
+        self.assertContains(result, _(
+                'Ce nom d’utilisateur est inconnu. '
+                'Si vous ne possédez pas de compte, '
+                'vous pouvez vous inscrire.'
+            )
+        )
 
         # login a user. Good password and next parameter then
         # redirection to the "next" page.
