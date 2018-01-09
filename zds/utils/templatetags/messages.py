5164ef41d192e8ccb111c8dab73526aefc27673b
Nils Van Zuijlen <nilsdu29@gmail.com>
2018-01-09 23:50:00 +0100

    Affichage des erreurs propre !
    Closes #4817

---
+++ zds/utils/templatetags/messages.py
@@ -0,0 +1,28 @@
+from django import template
+from django.template.base import VariableDoesNotExist
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+"""
+Define a tag allowing to print properly all messages sent to the user.
+"""
+
+
+@register.inclusion_tag('messages.html', name='messages')
+def messages(messages):
+    """
+    Define a tag allowing to print properly all messages sent to the user.
+
+    :param messages: The messages list
+    :return: dict
+    """
+
+    simple_messages = []
+    for message in messages:
+        simple_message = {'tags': str(message.tags), 'text': str(message)}
+        if simple_message['text'].startswith('[\'') and simple_message['text'].endswith('\']'):
+            simple_message['text'] = simple_message['text'][2:-2]
+        simple_messages.append(simple_message)
+
+    return {'messages': simple_messages}
