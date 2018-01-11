67ef304f659fb7e915099ff1de4884cba8da7a46
Michael Marx <michael-marx@protonmail.ch>
2018-01-11 23:45:40 +0100

    Corrige (encore) un test unitaire

--- zds/member/tests/tests_views.py
+++ zds/member/tests/tests_views.py
@@ -386,7 +386,7 @@ class MemberTests(TestCase):
              'password': 'hostel77',
              'remember': 'remember'},
             follow=False)
-        self.assertRedirects(result, reverse('home'))
+        self.assertRedirects(result, reverse('homepage'))
 
         # check if the login form will redirect if there is
         # a next parameter.
