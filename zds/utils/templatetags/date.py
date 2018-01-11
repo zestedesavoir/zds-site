65509709cb38d6ff397a697fa68341b8574896f7
Michael Marx <michael-marx@protonmail.ch>
2018-01-11 19:48:49 +0100

    Modifie une importation qui posait problème

--- zds/utils/templatetags/date.py
+++ zds/utils/templatetags/date.py
@@ -1,10 +1,10 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 from django import template
 from django.contrib.humanize.templatetags.humanize import naturaltime
 from django.template.defaultfilters import date
-from django.utils.datetime_safe import datetime
 from django.utils.timezone import get_default_timezone
+from django.utils.translation import ugettext_lazy as _
 
 register = template.Library()
 
@@ -14,10 +14,10 @@ Define a filter to format date.
 
 # Date formatting constants
 
-__DATE_FMT_FUTUR = 'Dans le futur'
-__ABS_DATE_FMT_SMALL = r'd/m/y à H\hi'       # Small format
-__ABS_DATE_FMT_NORMAL = r'l d F Y à H\hi'    # Normal format
-__ABS_HUMAN_TIME_FMT = '%d %b %Y, %H:%M:%S'
+__DATE_FMT_FUTUR = _('Dans le futur')
+__ABS_DATE_FMT_SMALL = _(r'd/m/y à H\hi')       # Small format
+__ABS_DATE_FMT_NORMAL = _(r'l d F Y à H\hi')    # Normal format
+__ABS_HUMAN_TIME_FMT = _('%d %b %Y, %H:%M:%S')
 
 
 def date_formatter(value, tooltip, small):
