6bd246ede9109088793bb45209d614a3bd601bf0
Antoine Motet <antoine.motet@gmail.com>
2018-01-20 11:27:49 +0100

    Transforme les templatetags des notifications en un context processor
    Je ne pense pas que ce soit une bonne pratique de faire des queries vers
    la DB dans des templatetags.
    
    Ce commit limite le nombre maximal de notifications affichées dans la
    liste déroulante en haut à droite du site. Il n’y en a pas plus de 10
    désormais. Toutefois, le nombre de notifications affiché dans le badge
    rouge correspond bien au nombre total des notifications.
    
    Le code est quasi le même pour les messages privés, les alertes de
    modération et les autres notifications.
    
    J’ai essayé de soigner les queries, il y a d’abord un `SELECT COUNT(*) …`
    puis un `SELECT * … LIMIT 10` et c’est tout.

---
+++ zds/utils/tests/tests_context_processors.py
@@ -0,0 +1,56 @@
+from datetime import datetime, timedelta
+
+from django.contrib.auth.models import AnonymousUser
+from django.test import TestCase
+
+from zds.forum.factories import CategoryFactory, ForumFactory, PostFactory, TopicFactory
+from zds.member.factories import ProfileFactory, StaffProfileFactory
+from zds.utils.context_processor import header_notifications as notifications_processor
+from zds.utils.models import Alert
+
+
+class AlertsTest(TestCase):
+    def setUp(self):
+        self.staff = StaffProfileFactory()
+        self.dummy_author = ProfileFactory()
+
+        self.category = CategoryFactory(position=1)
+        self.forum = ForumFactory(category=self.category, position_in_category=1)
+        self.topic = TopicFactory(forum=self.forum, author=self.dummy_author.user)
+        self.post = PostFactory(topic=self.topic, author=self.dummy_author.user, position=1)
+
+        self.alerts = []
+        for i in range(20):
+            alert = Alert(author=self.dummy_author.user,
+                          comment=self.post,
+                          scope='FORUM',
+                          text='pouet-{}'.format(i),
+                          pubdate=(datetime.now() + timedelta(minutes=i)))
+            alert.save()
+            self.alerts.append(alert)
+
+    def test_anonymous_user(self):
+        self.assertEqual({}, self.__notifications(AnonymousUser()))
+
+    def test_regular_user(self):
+        self.assertEqual(False, self.__alerts(self.dummy_author.user))
+
+    def test_staff(self):
+        alerts = self.__alerts(self.staff.user)
+        self.assertEqual(20, alerts['total'])
+        self.assertEqual(10, len(alerts['list']))
+        self.assertEqual(self.alerts[-1].text, alerts['list'][0]['text'])
+
+        self.alerts[5].delete()
+        alerts = self.__alerts(self.staff.user)
+        self.assertEqual(19, alerts['total'])
+        self.assertEqual(10, len(alerts['list']))
+
+    def __alerts(self, user):
+        return self.__notifications(user)['header_alerts']
+
+    def __notifications(self, user):
+        class Request(): pass
+        r = Request()
+        r.user = user
+        return notifications_processor(r)
