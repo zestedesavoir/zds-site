a1c1f31890fcfaee1b4b781aa08acf9e8bc259b4
Victor Felder <victor@draft.li>
2018-01-28 14:58:20 +0100

    Renomme Subject -> Topic

--- zds/searchv2/urls.py
+++ zds/searchv2/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from zds.searchv2.views import SearchView, opensearch, SimilarSubjectsView
+from zds.searchv2.views import SearchView, opensearch, SimilarTopicsView
 
 
 urlpatterns = [
     url(r'^$', SearchView.as_view(), name='query'),
-    url(r'^sujets-similaires/$', SimilarSubjectsView.as_view(), name='similar'),
+    url(r'^sujets-similaires/$', SimilarTopicsView.as_view(), name='similar'),
     url(r'^opensearch\.xml$', opensearch, name='opensearch'),
 ]
