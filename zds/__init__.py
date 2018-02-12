d4116d737c0ecb6029e1376d0fa07028acdad5e6
Michael Marx <michael-marx@protonmail.ch>
2018-02-12 19:00:24 +0100

    Vérifie l'existence des utilisateurs dans '__init__.py'

--- zds/__init__.py
+++ zds/__init__.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import User
+from django.conf import settings
+
 try:
     import ujson as json_handler
 except ImportError:
@@ -22,3 +25,13 @@ try:
 except ImportError:
     __version__ = 'dev'
     git_version = None
+=======
+for user in ('bot_account', 'anonymous_account', 'external_account'):
+    username = settings.ZDS_APP['member'][user]
+    try:
+        test = User.objects.get(username=username)
+    except User.DoesNotExist:
+        raise Exception(
+            'The {username!r} user does not exist. '
+            'You must create it to run the server.'.format(username=username)
+        )
