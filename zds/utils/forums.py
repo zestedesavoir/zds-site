ec74d74d739ccb21a2f44a1ed4b35e34c76ed3f2
fdambrine <francois.dambrine@vadesecure.com>
2018-02-26 11:19:40 +0100

    Remonte les messages d'erreur

--- zds/utils/forums.py
+++ zds/utils/forums.py
@@ -1,11 +1,12 @@
 import json
 from datetime import datetime
 
+from django.contrib.messages.context_processors import messages
 from django.http import HttpResponse, StreamingHttpResponse
 from django.shortcuts import render, render_to_response
 from django.views.generic import CreateView
 from django.views.generic.detail import SingleObjectMixin
-
+from django.utils.translation import gettext as _
 from zds.forum.models import Topic, Post
 from zds.member.views import get_client_ip
 from zds.utils.misc import contains_utf8mb4
@@ -104,7 +105,9 @@ def send_post(request, topic, author, text,):
         post.position = topic.last_message.position + 1
     else:
         post.position = 1
-    post.update_content(text)
+    post.update_content(text,
+                        on_error=lambda m: messages.error(request,
+                                                          _('Erreurs dans le Markdown: {}').format('\n- '.join(m))))
     post.ip_address = get_client_ip(request)
     post.hat = get_hat_from_request(request)
     post.save()
