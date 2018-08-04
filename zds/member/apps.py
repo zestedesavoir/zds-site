32304dd86cac6070c7a47eaea271f4c214aba908
Michael Marx <michael-marx@protonmail.ch>
2018-08-04 19:39:59 +0200

    Importe 'User' au lieu de 'get_user_model()'

--- zds/member/apps.py
+++ zds/member/apps.py
@@ -1,13 +1,13 @@
 from django.apps import AppConfig
 from django.conf import settings
-from django.contrib.auth import get_user_model
+
 
 class MemberConfig(AppConfig):
     name = 'zds.member'
 
     def ready(self):
         # Imported here cause apps aren't loaded yet.
-        User = get_user_model()
+        from django.contrib.auth.models import User
 
         for user in ('bot_account', 'anonymous_account', 'external_account'):
             username = settings.ZDS_APP['member'][user]
