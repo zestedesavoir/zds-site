e2684e5fe77ebb6827b64f8cc62b0656857a8e21
Michael Marx <michael-marx@protonmail.ch>
2018-08-05 12:46:38 +0200

    Tentative de correction

--- zds/member/apps.py
+++ zds/member/apps.py
@@ -1,22 +1,5 @@
 from django.apps import AppConfig
-from django.conf import settings
 
 
 class MemberConfig(AppConfig):
     name = 'zds.member'
-
-    def ready(self):
-        # Imported here cause apps aren't loaded yet.
-        from django.contrib.auth.models import User
-
-        for user in ('bot_account', 'anonymous_account', 'external_account'):
-            username = settings.ZDS_APP['member'][user]
-            try:
-                _ = User.objects.get(username=username)
-            except User.DoesNotExist:
-                raise Exception(
-                    'The {username!r} user does not exist. '
-                    'You must create it to run the server.'.format(
-                        username=username
-                    )
-                )
