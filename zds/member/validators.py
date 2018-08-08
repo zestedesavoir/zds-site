ae781a05c864074324adc930a68ab8a335876be2
Michael Marx <michael-marx@protonmail.ch>
2018-08-08 19:06:16 +0200

    Corrige le validateur 'validate_zds_username()'
    Un problème dans le validator 'zds.member.validators.validate_zds_username' rendait impossible le changement de casse d'un pseudo.
    'admin' pouvait donc se renommer 'superadmin' mais 'Admin'. Ce commit corrige ce comportement, et supprime également un paramètre optionnel qui n'était pas utilisé dans le projet.

--- zds/member/validators.py
+++ zds/member/validators.py
@@ -69,7 +69,7 @@ class ZdSEmailValidator(EmailValidator):
 validate_zds_email = ZdSEmailValidator()
 
 
-def validate_zds_username(value, check_username_available=True):
+def validate_zds_username(value):
     """
     Check if username is used by another user
 
@@ -78,16 +78,16 @@ def validate_zds_username(value, check_username_available=True):
     """
     msg = None
     user_count = User.objects.filter(username=value).count()
+    if not user_count:
+        return
     if ',' in value:
         msg = _('Le nom d\'utilisateur ne peut contenir de virgules')
     elif value != value.strip():
         msg = _('Le nom d\'utilisateur ne peut commencer ou finir par des espaces')
     elif contains_utf8mb4(value):
         msg = _('Le nom d\'utilisateur ne peut pas contenir des caractères utf8mb4')
-    elif check_username_available and user_count > 0:
+    elif user_count > 0:
         msg = _('Ce nom d\'utilisateur est déjà utilisé')
-    elif not check_username_available and user_count == 0:
-        msg = _('Ce nom d\'utilisateur n\'existe pas')
     if msg is not None:
         raise ValidationError(msg)
 
