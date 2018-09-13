fece287f6a0747a249ec8cdc6860145b73b885ca
Eskimon <eskimon@outlook.com>
2018-09-13 22:29:00 +0200

    Working datepicker for personnalized period (but ugly design for now)
    Signed-off-by: Quentin Gliech <quentingliech@gmail.com>

---
+++ zds/utils/templatetags/datedelta_from_day.py
@@ -0,0 +1,15 @@
+from datetime import date, datetime, timedelta
+
+from django import template
+
+register = template.Library()
+
+
+# TODO test me !
+@register.filter
+def datedelta_from_day(day_delta, date_from=None):
+    if not date_from:
+        reference = date.today()
+    else:
+        reference = datetime.strptime(date_from, '%Y-%m-%d').date()
+    return reference + timedelta(days=day_delta)
