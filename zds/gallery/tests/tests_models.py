4310498d70f5e80e8751a2af8720c1a0e034125c
artragis <artragis@gmail.com>
2018-10-30 11:03:25 +0100

    Evite les doubles slash

--- zds/gallery/tests/tests_models.py
+++ zds/gallery/tests/tests_models.py
@@ -53,7 +53,7 @@ class ImageTest(TestCase):
         self.gallery.delete()
 
     def test_get_absolute_url(self):
-        absolute_url = '{0}/{1}'.format(settings.MEDIA_URL, self.image.physical)
+        absolute_url = '{0}/{1}'.format(settings.MEDIA_URL, self.image.physical).replace('//', '/')
 
         self.assertEqual(absolute_url, self.image.get_absolute_url())
 
