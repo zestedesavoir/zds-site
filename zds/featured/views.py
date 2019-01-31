252737f9f919a7ab18d09f574ae7a6678ed6ed52
A-312 <A-312@users.noreply.github.com>
2019-01-31 16:31:45 +0100

    Insére directement le format en 228px

--- zds/featured/views.py
+++ zds/featured/views.py
@@ -72,7 +72,7 @@ class FeaturedResourceCreate(CreateView):
             return {}
         displayed_authors = ', '.join([str(x) for x in content.authors.all()])
         if content.content.image:
-            image_url = content.content.image.physical.url
+            image_url = content.content.image.physical['featured'].url
         else:
             image_url = None
         return {'title': content.title(),
