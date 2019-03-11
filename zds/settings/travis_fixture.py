0c51a5d34fb6491201424ac63a35615214a41517
A-312 <A-312@users.noreply.github.com>
2019-03-11 22:39:53 +0100

    Settings personnalisé

--- zds/settings/travis_fixture.py
+++ zds/settings/travis_fixture.py
@@ -1,10 +1,6 @@
 from .dev import *
 
 LOGGING['loggers']['zds.utils.templatetags.emarkdown'] = {
-    'loggers': {
-        'zds': {
-            'level': 'INFO',
-            'handlers': ['console'],
-        },
-    },
+    'level': 'INFO',
+    'handlers': ['console'],
 }
