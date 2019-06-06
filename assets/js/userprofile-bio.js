504d8e67f09a7c445f7a66cab62578477437e0be
Amaury Carrade <amaury@carrade.eu>
2019-06-06 18:17:41 +0200

    La biographie est tronquée si trop longue
    - Ajout d'une détection de la longueur de la biographie, la tronquant si elle dépasse
      une certaine hauteur. Un “lien” est alors présenté afin de mettre en évidence le fait
      que la biographie est partielle, et de permettre de l'afficher en entier.
    - Ajustements mineurs de style de code SCSS (& >).

---
+++ assets/js/userprofile-bio.js
@@ -0,0 +1,43 @@
+/* ===== Zeste de Savoir ====================================================
+   Truncates too long biographies on profile pages, with a link to see the whole bio.
+   ---------------------------------
+   Author: Amaury Carrade
+   ========================================================================== */
+
+(function($, undefined){
+    "use strict";
+
+    let $bioContainer = $("body.userprofilepage .user-bio-and-activity .bio-container");
+    if (!$bioContainer) return; // We are not on a profile page
+
+    let $bioTextContainer = $bioContainer.find(".message-content");
+    let $bioOverflowToggleHandle = $bioContainer.find(".biography-overflow");
+    let $bioOverflowToggleLabel = $bioOverflowToggleHandle.find("p");
+
+    // We detect overflow by comparing the scroll height (equal to the whole
+    // size, without max-width) to the inner height (equal to the visible size).
+    // But we must accept a margin as non-overflowing blocks may have a slightly
+    // bigger scroll height (e.g. 148 vs 147.6).
+    if ($bioTextContainer[0].scrollHeight - $bioTextContainer.innerHeight() > 10)
+    {
+        console.log("Bio overflow");
+        $bioContainer.addClass("too-long-biography");
+    }
+    else
+    {
+        // If overflow was not detected, we ensure that the whole biography is
+        // shown, as for edge cases, the end of the biography may be slightly
+        // cut without link to show it full.
+        $bioContainer.addClass("full-biography");
+    }
+
+    $bioOverflowToggleHandle.on("click", function()
+    {
+        $bioContainer.toggleClass("full-biography");
+
+        let oldLabel = $bioOverflowToggleLabel.text();
+
+        $bioOverflowToggleLabel.text($bioOverflowToggleLabel.attr("data-other-label"));
+        $bioOverflowToggleLabel.attr("data-other-label", oldLabel);
+    });
+})(jQuery);
