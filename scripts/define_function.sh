3c7e72c3a4059df19b96a57786474b09876566d1
A-312 <A-312@users.noreply.github.com>
2019-06-10 21:12:42 +0200

    Ajout le temps des tâches sur travis

--- scripts/define_function.sh
+++ scripts/define_function.sh
@@ -10,6 +10,54 @@ function _in {
 }
 
 
+## travis code
+# https://github.com/travis-ci/travis-build/blob/master/lib/travis/build/bash/travis_fold.bash
+zds_travis_fold() {
+    local action="${1}"
+    local name="${2}"
+    echo -en "travis_fold:${action}:${name}\\r${ANSI_CLEAR}"
+}
+
+
+# https://github.com/travis-ci/travis-build/blob/master/lib/travis/build/bash/travis_nanoseconds.bash
+zds_travis_nanoseconds() {
+  local cmd='date'
+  local format='+%s%N'
+
+  if hash gdate >/dev/null 2>&1; then
+    cmd='gdate'
+  elif [[ "${TRAVIS_OS_NAME}" == osx ]]; then
+    format='+%s000000000'
+  fi
+
+  "${cmd}" -u "${format}"
+}
+
+
+# https://github.com/travis-ci/travis-build/blob/master/lib/travis/build/bash/travis_time_start.bash
+# change : prefixed global variable with ZDS_
+zds_travis_time_start() {
+    ZDS_TRAVIS_TIMER_ID="ZDS_$(printf %08x $((RANDOM * RANDOM)))"
+    ZDS_TRAVIS_TIMER_START_TIME="$(zds_travis_nanoseconds)"
+    export ZDS_TRAVIS_TIMER_ID ZDS_TRAVIS_TIMER_START_TIME
+    echo -en "travis_time:start:${ZDS_TRAVIS_TIMER_ID}\\r${ANSI_CLEAR}"
+}
+
+
+# https://github.com/travis-ci/travis-build/blob/master/lib/travis/build/bash/travis_time_finish.bash
+# change : prefixed global variable with ZDS_
+zds_travis_time_finish() {
+    local result="${?}"
+    local travis_timer_end_time
+    travis_timer_end_time="$(zds_travis_nanoseconds)"
+    local duration
+    duration="$((travis_timer_end_time - ZDS_TRAVIS_TIMER_START_TIME))"
+    echo -en "travis_time:end:${ZDS_TRAVIS_TIMER_ID}:start=${ZDS_TRAVIS_TIMER_START_TIME},finish=${travis_timer_end_time},duration=${duration}\\r${ANSI_CLEAR}"
+    return "${result}"
+}
+##
+
+
 ## start fold for travis
 ZDS_SHOW_TRAVIS_FOLD=0
 if $(_in "--travis-output" $@); then
@@ -31,8 +79,9 @@ function zds_fold_start {
         fi
 
         zds_fold_current="$1"
-        echo "travis_fold:start:${zds_fold_current_cat}_${zds_fold_current}"
-        echo -en "\033[0K"
+        zds_travis_fold "start" "${zds_fold_current_cat}_${zds_fold_current}"
+
+        zds_travis_time_start
     fi
 
     print_info "$2" --bold
@@ -41,8 +90,9 @@ function zds_fold_start {
 
 function zds_fold_end {
     if [[ $ZDS_SHOW_TRAVIS_FOLD == 1 ]] && [[ $zds_fold_current =~ "" ]]; then
-        echo "travis_fold:end:${zds_fold_current_cat}_${zds_fold_current}"
-        echo -en "\033[0K"
+        zds_travis_time_finish
+
+        zds_travis_fold "end" "${zds_fold_current_cat}_${zds_fold_current}"
         zds_fold_current=""
     fi
 }
