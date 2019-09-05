9a9d65c8d2ae490580ef6a14c8358bc458ab04fe
artragis <artragis@gmail.com>
2019-09-05 13:32:47 +0200

    Fait en sorte que le bouton réagisse

--- assets/js/content-export.js
+++ assets/js/content-export.js
@@ -1,16 +1,27 @@
 (function ($) {
     $("[data-export-button]").click(function (e) {
         e.preventDefault();
-        var $button = $(e.target);
-        var url = $button.data("export-button");
-        var csrf = $("input[name=csrfmiddlewaretoken]").val();
+        const $button = $(e.target);
+        const url = $button.data("export-button");
+        const csrf = $("input[name=csrfmiddlewaretoken]").val();
         $button.prop("disabled", true);
+        $button.prop("aria-busy", true);
         $.ajax(url, {
             method: "POST",
             headers: {
                "X-CSRFToken": csrf,
             },
             data: {},
+        }).done(() => {
+            $button.prop("disabled", false);
+            $button.text($button.attr("data-success-text"));
+            $button.prop("aria-busy", false);
+            setTimeout(() => $button.text($button.attr("data-base-text")), 5000);
+        }).fail(() => {
+            $button.prop("disabled", false);
+            $button.text($button.attr("data-error-text"));
+            $button.prop("aria-busy", false);
+            setTimeout(() => $button.text($button.attr("data-base-text")), 5000);
         });
     });
 })(jQuery);
