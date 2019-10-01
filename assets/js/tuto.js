e22653acc80c120c1dfad2d829deb19b98ebc6fd
A-312 <A-312@users.noreply.github.com>
2019-10-01 02:13:13 +0200

    Premier support des containers pour le dnd

--- assets/js/tuto.js
+++ assets/js/tuto.js
@@ -5,31 +5,34 @@
   "use strict"
 
   $(document).ready(function(){
-    $("ol[data-children-type=section]").each(function () {
-      $(this).sortable({
-        group: 'section',
-        animation: 150,
-        onMove: (evt) => {
-          const $to = $(evt.related).parent()
-          const $from = $(evt.dragged).parent()
-          if (!$to.is($from)) {
-            console.log("ok")
-            // Element is dragged into the list from another list
+    $("ol[data-children-type=section]").sortable({
+      group: 'section',
+      filter: function (pointer, dragged) {
+        return $(dragged).is(".simple-create-button")
+      },
+      animation: 150,
+      onMove: (evt) => {
+        const $to = $(evt.related).parent()
+        const $from = $(evt.dragged).parent()
+        if (!$to.is($from)) {
+          // Element is dragged into the list from another list
 
-            if ($to.parent().is("div.part")) { // is: (CHAPTER) > section
-              $(evt.dragged).find("> h4 > a").unwrap().wrap("<h3></h3>")
-            } else { // is: (PART) > chapter > section 
-              $(evt.dragged).find("> h3 > a").unwrap().wrap("<h4></h4>")
-            }
+          if ($to.parent().is("div.article-part")) { // is: (CHAPTER) > section
+            $(evt.dragged).find("> h4 > a").unwrap().wrap("<h3></h3>")
+          } else { // is: (PART) > chapter > section 
+            $(evt.dragged).find("> h3 > a").unwrap().wrap("<h4></h4>")
           }
-
-          if ($(evt.related).is(".simple-create-button"))
-            return -1
-        },
-        onEnd: (evt) => {
-          $(evt.dragged).data("tmp-sortable-switch", false)
         }
-      })
+
+        if ($(evt.related).is(".simple-create-button"))
+          return false
+      },
+      onEnd: (evt) => {
+        $(evt.dragged).data("tmp-sortable-switch", false)
+      }
     })
+    $("*[data-children-type=container]").sortable({
+      group: 'container'
+    });
   })
 })(document, jQuery)
