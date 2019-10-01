898e9d2ccb7fee08274611e62f2211f4d166b4f4
A-312 <A-312@users.noreply.github.com>
2019-10-01 16:24:26 +0200

    Eslint

--- assets/js/content.js
+++ assets/js/content.js
@@ -2,61 +2,60 @@
  * Allow the user to compare two commits
  */
 (function(document, $, undefined){
-  "use strict"
+  'use strict'
 
   function hideMoveButton() {
     /* TODO : REMOVE this and update the selectlist with js */
     /* because the list is not updated */
-    console.log("ok")
-    $(".simple-move-button").hide();
+    $('.simple-move-button').hide();
   }
 
   $(document).ready(function(){
-    $("ol[data-children-type=section]").sortable({
+    $('ol[data-children-type=section]').sortable({
       group: 'section',
       filter: function (pointer, dragged) {
-        return $(dragged).is(".simple-create-button")
+        return $(dragged).is('.simple-create-button')
       },
       onMove: (evt) => {
         const $to = $(evt.related).parent()
         const $from = $(evt.dragged).parent()
         if (!$to.is($from)) {
           // Element is dragged into the list from another list
-          if ($from.parent().is(".article-containers > .article-part")) { // is: chapter > section
-            $(evt.dragged).find("> h4 > a").unwrap().wrap("<h3></h3>")
+          if ($from.parent().is('.article-containers > .article-part')) { // is: chapter > section
+            $(evt.dragged).find('> h4 > a').unwrap().wrap('<h3></h3>')
           } else { // is: part > chapter > section 
-            $(evt.dragged).find("> h3 > a").unwrap().wrap("<h4></h4>")
+            $(evt.dragged).find('> h3 > a').unwrap().wrap('<h4></h4>')
           }
         }
 
-        if ($(evt.related).is(".simple-create-button"))
+        if ($(evt.related).is('.simple-create-button'))
           return false
       },
       onEnd: hideMoveButton
     })
-    $("*[data-children-type=container]").sortable({
+    $('*[data-children-type=container]').sortable({
       group: 'container',
       filter: function (pointer, dragged) {
-        return $(dragged).is(".simple-create-part")
+        return $(dragged).is('.simple-create-part')
       },
       onMove: (evt) => {
         const $to = $(evt.related).parent()
         const $from = $(evt.dragged).parent()
         if (!$to.is($from)) {
           // Element is dragged into the list from another list
-          if ($to.is("section")) { // is: chapter > section
-            $(evt.dragged).find("> h3 > a").unwrap().wrap("<h2></h2>")
-            $(evt.dragged).find("ol h4 > a").unwrap().wrap("<h3></h3>")
+          if ($to.is('section')) { // is: chapter > section
+            $(evt.dragged).find('> h3 > a').unwrap().wrap('<h2></h2>')
+            $(evt.dragged).find('ol h4 > a').unwrap().wrap('<h3></h3>')
           } else { // is: part > chapter > section 
-            $(evt.dragged).find("> h2 > a").unwrap().wrap("<h3></h3>")
-            $(evt.dragged).find("ol h3 > a").unwrap().wrap("<h4></h4>")
+            $(evt.dragged).find('> h2 > a').unwrap().wrap('<h3></h3>')
+            $(evt.dragged).find('ol h3 > a').unwrap().wrap('<h4></h4>')
           }
         }
 
-        if ($(evt.related).is(".simple-create-part"))
+        if ($(evt.related).is('.simple-create-part'))
           return false
       },
       onEnd: hideMoveButton
     });
   })
-})(document, jQuery)
+})(document, jQuery);
