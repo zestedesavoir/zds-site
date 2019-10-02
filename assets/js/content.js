99155bbef933741dd98d7830dc03255091f82934
A-312 <A-312@users.noreply.github.com>
2019-10-02 11:00:51 +0200

    Eslint doublequote

--- assets/js/content.js
+++ assets/js/content.js
@@ -2,20 +2,20 @@
  * Allow the user to compare two commits
  */
 (function(document, $, undefined){
-  'use strict';
+  "use strict";
 
   function hideMoveButton() {
     /* TODO : REMOVE this and update the selectlist with js */
     /* because the list is not updated */
-    $('.simple-move-button').hide();
+    $(".simple-move-button").hide();
   }
 
-  $(document).ready(function(){
-    $('ol[data-children-type=section]').sortable({
-      group: 'section',
-      handle: 'a',
+  $(document).ready(function() {
+    $("ol[data-children-type=section]").sortable({
+      group: "section",
+      handle: "a",
       filter: function (pointer, dragged) {
-        return $(dragged).is('.simple-create-button');
+        return $(dragged).is(".simple-create-button");
       },
       onMove: (evt) => {
         const $to = $(evt.related).parent();
@@ -23,27 +23,27 @@
 
         if (!$to.is($from)) {
           // Element is dragged into the list from another list
-          if ($from.parent().is('.article-containers > .article-part')) { // is: chapter > section
-            $(evt.dragged).find('> h4 > a').unwrap().wrap('<h3></h3>');
+          if ($from.parent().is(".article-containers > .article-part")) { // is: chapter > section
+            $(evt.dragged).find("> h4 > a").unwrap().wrap("<h3></h3>");
           } else { // is: part > chapter > section 
-            $(evt.dragged).find('> h3 > a').unwrap().wrap('<h4></h4>');
+            $(evt.dragged).find("> h3 > a").unwrap().wrap("<h4></h4>");
           }
         }
 
-        if ($(evt.related).is('.simple-create-button')) {
+        if ($(evt.related).is(".simple-create-button")) {
           return -1;
         }
       },
       onEnd: hideMoveButton
     })
-    $('*[data-children-type=container]').sortable({
-      group: 'container',
+    $("*[data-children-type=container]").sortable({
+      group: "container",
       handle: ["h2", "h3 a"],
       filter: function (pointer, dragged) {
-        return $(dragged).is('.simple-create-part');
+        return $(dragged).is(".simple-create-part");
       },
       onMove: (evt) => {
-        if ($(evt.related).is('.simple-create-part')) {
+        if ($(evt.related).is(".simple-create-part")) {
           return false;
         }
 
@@ -52,12 +52,12 @@
 
         if (!$to.is($from)) {
           // Element is dragged into the list from another list
-          if ($to.is('section')) { // is: chapter > section
-            $(evt.dragged).find('> h3 > a').unwrap().wrap('<h2></h2>');
-            $(evt.dragged).find('ol h4 > a').unwrap().wrap('<h3></h3>');
+          if ($to.is("section")) { // is: chapter > section
+            $(evt.dragged).find("> h3 > a").unwrap().wrap("<h2></h2>");
+            $(evt.dragged).find("ol h4 > a").unwrap().wrap("<h3></h3>");
           } else { // is: part > chapter > section 
-            $(evt.dragged).find('> h2 > a').unwrap().wrap('<h3></h3>');
-            $(evt.dragged).find('ol h3 > a').unwrap().wrap('<h4></h4>');
+            $(evt.dragged).find("> h2 > a").unwrap().wrap("<h3></h3>");
+            $(evt.dragged).find("ol h3 > a").unwrap().wrap("<h4></h4>");
           }
         }
       },
