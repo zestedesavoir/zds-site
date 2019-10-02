c9b470e078b2fcece55492b10f6e83edc1148e71
A-312 <A-312@users.noreply.github.com>
2019-10-02 11:38:33 +0200

    Bloque les déplacements que le BACK ne supporte pas

--- assets/js/content.js
+++ assets/js/content.js
@@ -10,6 +10,22 @@
     $(".simple-move-button").hide();
   }
 
+  function canDrop($item, $to, $from) {
+    // TODO : [BACK] Change slug if we have the same.
+    const filter = "[data-slug=" + $item.attr("data-slug") + "]";
+    if ($to.children(filter).length >= (1 + $to.is($from))) {
+      console.log("ooo")
+      return false;
+    }
+
+    // TODO : [BACK] Add "beginning" moving_method
+    if (!$to.children(":not(.simple-create-button):not(.simple-create-part)")[0]) {
+      return false;
+    }
+
+    return true;
+  }
+
   $(document).ready(function() {
     $("ol[data-children-type=section]").sortable({
       group: "section",
@@ -18,15 +34,20 @@
         return $(dragged).is(".simple-create-button");
       },
       onMove: (evt) => {
+        const $item = $(evt.dragged);
         const $to = $(evt.related).parent();
         const $from = $(evt.dragged).parent();
+
+        if (!canDrop($item, $to, $from)) {
+          return false;
+        }
 
         if (!$to.is($from)) {
           // Element is dragged into the list from another list
           if ($from.parent().is(".article-containers > .article-part")) { // is: chapter > section
-            $(evt.dragged).find("> h4 > a").unwrap().wrap("<h3></h3>");
+            $item.find("> h4 > a").unwrap().wrap("<h3></h3>");
           } else { // is: part > chapter > section 
-            $(evt.dragged).find("> h3 > a").unwrap().wrap("<h4></h4>");
+            $item.find("> h3 > a").unwrap().wrap("<h4></h4>");
           }
         }
 
@@ -47,17 +68,22 @@
           return false;
         }
 
+        const $item = $(evt.dragged);
         const $to = $(evt.related).parent();
         const $from = $(evt.dragged).parent();
+
+        if (!canDrop($item, $to, $from)) {
+          return false;
+        }
 
         if (!$to.is($from)) {
           // Element is dragged into the list from another list
           if ($to.is("section")) { // is: chapter > section
-            $(evt.dragged).find("> h3 > a").unwrap().wrap("<h2></h2>");
-            $(evt.dragged).find("ol h4 > a").unwrap().wrap("<h3></h3>");
+            $item.find("> h3 > a").unwrap().wrap("<h2></h2>");
+            $item.find("ol h4 > a").unwrap().wrap("<h3></h3>");
           } else { // is: part > chapter > section 
-            $(evt.dragged).find("> h2 > a").unwrap().wrap("<h3></h3>");
-            $(evt.dragged).find("ol h3 > a").unwrap().wrap("<h4></h4>");
+            $item.find("> h2 > a").unwrap().wrap("<h3></h3>");
+            $item.find("ol h3 > a").unwrap().wrap("<h4></h4>");
           }
         }
       },
