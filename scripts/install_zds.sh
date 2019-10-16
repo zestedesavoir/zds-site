36d654a63661b69d233007f8405bfd1fab548b72
A-312 <A-312@users.noreply.github.com>
2019-10-16 08:40:37 +0200

    test #5300

--- scripts/install_zds.sh
+++ scripts/install_zds.sh
@@ -501,7 +501,7 @@ fi
 if  ! $(_in "-data" $@) && ( $(_in "+data" $@) || $(_in "+base" $@) || $(_in "+full" $@) ); then
     zds_fold_start "fixtures" "* [+data] fixtures"
 
-    npm run server --prefix zmd/node_modules/zmarkdown -- --silent; exVal=$?
+    # npm run server --prefix zmd/node_modules/zmarkdown -- --silent; exVal=$?
 
     if [[ $exVal != 0 ]]; then
         print_error "!! Cannot start zmd to generate-fixtures (use \`-data\` to skip)"
