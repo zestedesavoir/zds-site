220735f96621511521d26b1f98f21683ff0f11eb
cohelet <willy.konguem@gmail.com>
2019-10-17 17:57:26 +0200

    fix @artragis suggestions

--- assets/js/share.js
+++ assets/js/share.js
@@ -1,24 +1,22 @@
 
 (function($, undefined) {
     "use strict";
-    var shareModal = $("#share-to-mastodon");
-    var shareButton = shareModal.find("button");
+    let shareModal = $("#share-to-mastodon");
+    let shareButton = shareModal.find("button");
 
     shareModal.find("input[name='instance']").on("change paste keyup", function() {
         // start by chars (https://)
         if(this.value.startsWith("https://")) {
-            shareButton.attr("disabled", false);
+            shareButton.prop("disabled", false);
         } else {
-            shareButton.attr("disabled", true);
+            shareButton.prop("disabled", true);
         }
     });
 
     shareButton.on("click", function() {
-
-        var instance = shareModal.find("input[name='instance']").val();
-        var text = shareModal.find("input[name='text']").val();
-        var mastodonUrl = instance + "/share?text=" + encodeURIComponent(text);
-
+        let instance = shareModal.find("input[name='instance']").val();
+        let text = shareModal.find("input[name='text']").val();
+        let mastodonUrl = instance + "/share?text=" + encodeURIComponent(text);
         window.open(mastodonUrl);
     });
 })(jQuery);
