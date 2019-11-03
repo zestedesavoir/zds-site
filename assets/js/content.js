73ed90004ab14201fc97aa22e21874844a5953c4
A-312 <A-312@users.noreply.github.com>
2019-11-03 11:14:35 +0100

    Tuto move dragndrop (#5496)
    * Améliore le responsive design de header-container
    
    * Correction de la formulation de la documentation sur Windows (#5387)
    
    * Permet de drag'n'drop les sections (front js seulement)
    
    * Premier support des containers pour le dnd
    
    * drag'n'drop quasi complet
    
    * rename
    
    * Fix version publique
    
    * Alignement sur deux colonnes avec flex
    
    * Support & Exclusion des bouton
    
    * Affiche/Cache correctement les action-title buttons
    
    * rename
    
    * Rend le drag'n'drop non capricieux
    
    * Meilleur support pour les actions-title
    
    * Fix CSS pour les actions-title des blocs "ajouter un(e) xxx"
    
    * Bouton "ajouter une section plus joli"
    
    * Fix page mini-tuto
    
    * Eslint
    
    * Corrige le bug récurrent de drag'n'drop des containers
    J'annulais l'action de drag après avoir converti les niveaux des titres... Il fallait le faire au début... Et ne faire aucune conversion
    
    * Eslint
    
    * Fix: Permet de déplacer une section dans un chapitre vide
    Au lieu d'interdir complétement le drag, on autorise seulement au dessus donc `return -1`.
    
    * Corrige un bug visuel dans les mini-contenus
    
    * Rend plus strict (= rétréci) la zone draggable des éléments
    Seulement draggable sur le texte du titre, permet d'éviter les missclicks
    
    * Ajout des metadatas au tag HTML
    
    * Corrige le CSS des titres dans les sections
    
    * Metadata corrigé pour les containers
    
    * Eslint doublequote
    
    * La zone draggable fait toute la largeur des H2
    (avant on ne pouvait seulement sur la zone de texte)
    
    * Fix Metadata
    
    * Bloque les déplacements que le BACK ne supporte pas
    
    * Envoie le déplacement au serveur
    
    * Utilisation de la nomenclature "extract" au lieu de "section"
    
    * Fix: Permet de déplacer un chapitre dans une partie vide
    Au lieu d'interdir complétement le drag, on autorise seulement au dessus donc `return -1`.
    
    * Cache les boutons après un déplacement (car ne sont plus à jour)
    
    * Eslint
    
    * Fix: Permet de déplacer un chapitre dans une partie vide
    Au lieu d'interdir complétement le drag, on autorise seulement au dessus donc `return -1`.
    
    * Les boutons SUPPRIMER et EDITER sont mis à jour après un drag'n'drop
    
    * Met à jour les liens après un déplacement
    
    * Affichage "ajouter un chapitre/partie" en adéquation à la position

---
+++ assets/js/content.js
@@ -0,0 +1,181 @@
+/*
+ * Allow the user to compare two commits
+ */
+(function(document, $, undefined){
+  "use strict";
+
+  function hideMoveButton() {
+    /* TODO : REMOVE this and update the selectlist with js */
+    /* because the list is not updated */
+    $(".simple-move-button").hide();
+  }
+
+  function sendMoveAction(evt) {
+    hideMoveButton();
+
+    // sending
+    const $item = $(evt.item);
+    const $from = $(evt.from);
+    const movingMethod = (($prev) => {
+      const path = ((tree) => {
+        $item.parents("[data-children-type]:not([data-pk])").each((n, parent) => {
+          tree.push($(parent).attr("data-slug"));
+        });
+        tree = tree.reverse();
+        if (tree.length > 0) {
+          tree.push("");
+        }
+        return tree.join("/");
+      })([]);
+
+      if ($prev[0]) {
+        return "after:" + path + $prev.attr("data-slug");
+      } else {
+        return "before:" + path + $item.next().attr("data-slug");
+        // TODO : [BACK] Add support of beginning
+        // return "beginning:" + path;
+      }
+    })($item.prev());
+
+    const isExtract = ($from.attr("data-children-type") === "extract");
+
+    const pk = $item.parents("[data-pk]").attr("data-pk");
+    const slug = $item.attr("data-slug");
+
+    const form = {
+      // new 
+      "moving_method": movingMethod,
+      // old
+      "container_slug": $from.attr("data-slug"),
+      "first_level_slug": (isExtract) ? $from.parents("[data-children-type]").attr("data-slug") : undefined,
+      // current
+      "child_slug": slug,
+      "pk": pk,
+      // csrf
+      "csrfmiddlewaretoken": $("input[name=csrfmiddlewaretoken]").val()
+    };
+
+    $.post("/contenus/deplacer/", form);
+
+    // modifie les URLs
+    const path = ((tree) => {
+      $item.parents("[data-children-type]").each((n, parent) => {
+        tree.push($(parent).attr("data-slug"));
+      });
+      tree = tree.reverse();
+      tree.push(slug);
+      return tree.join("/");
+    })([]);
+
+    $item.find("> .actions-title a.edit").attr("href", `/contenus/editer-conteneur/${pk}/${path}/`);
+    $("form#delete-" + slug).attr("action", `/contenus/supprimer/${pk}/${path}/`);
+    $item.find("[data-children-type] > [data-slug] a").each(function () {
+      const $parent = $(this).parents("[data-slug]").eq(0);
+      const mySlug = $parent.attr("data-slug");
+      if (slug !== $(this).attr("data-slug")) {
+        $(this).attr("href", `/contenus/${pk}/${path}/#${$parent.index()+1}-${mySlug}`);
+      } else {
+        $(this).attr("href", `/contenus/${pk}/${path}/`);
+      }
+    });
+    $item.find(".simple-create-button")
+      .attr("href", `/contenus/nouvelle-section/${pk}/${path}/`);
+  }
+
+  function canDrop($item, $to, $from) {
+    // TODO : [BACK] Change slug if we have the same.
+    const filter = "[data-slug=" + $item.attr("data-slug") + "]";
+    if ($to.children(filter).length >= (1 + $to.is($from))) {
+      return false;
+    }
+
+    // TODO : [BACK] Add "beginning" moving_method
+    if (!$to.children(":not(.simple-create-button):not(.simple-create-part)")[0]) {
+      return false;
+    }
+
+    return true;
+  }
+
+  $(document).ready(function() {
+    $("ol[data-children-type=extract]").sortable({
+      group: "extract",
+      handle: "a",
+      filter: function (pointer, dragged) {
+        return $(dragged).is(".simple-create-button");
+      },
+      onMove: (evt) => {
+        const $item = $(evt.dragged);
+        const $to = $(evt.related).parent();
+        const $from = $(evt.dragged).parent();
+
+        if (!canDrop($item, $to, $from)) {
+          return false;
+        }
+
+        if (!$to.is($from)) {
+          // Element is dragged into the list from another list
+          if ($from.parent().is(".article-containers > .article-part")) { // is: chapter > extract
+            $item.find("> h4 > a").unwrap().wrap("<h3></h3>");
+          } else { // is: part > chapter > extract 
+            $item.find("> h3 > a").unwrap().wrap("<h4></h4>");
+          }
+        }
+
+        if ($(evt.related).is(".simple-create-button")) {
+          return -1;
+        }
+      },
+      onEnd: sendMoveAction
+    });
+
+    $("*[data-children-type=container]").sortable({
+      group: "container",
+      handle: ["h2", "h3 a"],
+      filter: function (pointer, dragged) {
+        return $(dragged).is(".simple-create-part");
+      },
+      onMove: (evt) => {
+        const $item = $(evt.dragged);
+        const $to = $(evt.related).parent();
+        const $from = $(evt.dragged).parent();
+
+        if (!canDrop($item, $to, $from)) {
+          return false;
+        }
+
+        if (!$to.is($from)) {
+          // Element is dragged into the list from another list
+          if ($to.is("section")) { // is: chapter > extract
+            $item.find("> h3 > a").unwrap().wrap("<h2></h2>");
+            $item.find("ol h4 > a").unwrap().wrap("<h3></h3>");
+
+            if ($item.find(".simple-create-part")[0]) {
+              $item.children(".article-containers").show();
+              $item.children("ol.summary-part").hide();
+            }
+          } else { // is: part > chapter > extract 
+            $item.find("> h2 > a").unwrap().wrap("<h3></h3>");
+            $item.find("ol h3 > a").unwrap().wrap("<h4></h4>");
+
+            if ($item.find(".simple-create-part")[0]) {
+              $item.children(".article-containers").hide();
+              $item.append(
+                `<ol class="summary-part" data-children-type="extract">
+                  <li class="simple-create-button">
+                    <a class="btn btn-grey">Ajouter une section</a>
+                  </li>
+                </ol>`
+              );
+            }
+          }
+        }
+
+        if ($(evt.related).is(".simple-create-part")) {
+          return -1;
+        }
+      },
+      onEnd: sendMoveAction
+    });
+  });
+})(document, jQuery);
