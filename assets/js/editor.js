48093b7216d976c91777aa44a490b792ab9308fb
firm1 <contact@firm1.eu>
2019-11-21 20:22:38 +0100

    priorité de la migration

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,6 +1,18 @@
 (function($, undefined){
     "use strict";
 
+    (function(){
+        /**
+        * Migration easymde
+        */
+        let source = localStorage.getItem("savedEditorText");
+        let entries = source ? JSON.parse(source) : [];
+        for (let n in entries) {
+            localStorage.setItem("smde_" + entries[n].id, entries[n].value);
+        }
+        localStorage.removeItem("savedEditorText");
+    })();
+
     /**
     * Preview the message on bottom
     */
