d6bc1f407b4a27201980c4311f2f97c52cafdffe
Situphen <situphen@gmail.com>
2019-12-31 23:32:53 +0100

    On garde en mémoire la version de Node JS dans le .nvmrc
    On garde en mémoire la version de Node JS dans le .nvmrc et non pas
    dans scripts/define_variable.sh. Ainsi, lorsqu'on modifie la version
    de Node JS, les développeurs sont avertis du changement par nvm et
    mettent à jour Node JS.

--- scripts/define_variable.sh
+++ scripts/define_variable.sh
@@ -4,9 +4,7 @@ if [[ $ZDS_VENV == "" ]]; then
     ZDS_VENV="zdsenv"
 fi
 
-if [[ $ZDS_NODE_VERSION == "" ]]; then
-    ZDS_NODE_VERSION="10.18.0"
-fi
+ZDS_NODE_VERSION=$(cat $ZDSSITE_DIR/.nvmrc)
 
 if [[ $ZDS_NVM_VERSION == "" ]]; then
     ZDS_NVM_VERSION="0.33.11"
