86abccd0ad89451e625d30b19f7760f4f897f30c
Arnaud-D <35631001+Arnaud-D@users.noreply.github.com>
2020-04-29 07:28:41 +0200

    Passe à un pathlib.Path pour STATIC_ROOT

--- zds/settings/abstract_base/django.py
+++ zds/settings/abstract_base/django.py
@@ -64,7 +64,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' 'static/' subdirectories and in STATICFILES_DIRS.
 # Example: '/home/media/media.lawrence.com/static/'
-STATIC_ROOT = str(BASE_DIR / 'static')
+STATIC_ROOT = BASE_DIR / 'static'
 
 # URL prefix for static files.
 # Example: 'http://media.lawrence.com/static/'
