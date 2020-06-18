458457328467700e4f71d7d0ca0a8c9a0fd478db
Amaury Carrade <amaury@carrade.eu>
2020-06-18 16:12:22 +0200

    Travis ne semble pas motivé à se connecter à MariaDB, on tente un truc à base de sudo

--- scripts/ci_mysql_setup.sh
+++ scripts/ci_mysql_setup.sh
@@ -4,9 +4,10 @@ sudo sed -i'' 's/\[mysqld\]/\[mysqld\]\ninnodb_file_per_table=on\ninnodb_file_fo
 sudo systemctl restart mysql
 
 # Travis should fail as soon as possible
-mysql -u root -e "SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES';"
+sudo mysql -u root -e "SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES';"
 # Avoid "mysql has gone away" errors
-mysql -u root -e "SET GLOBAL wait_timeout = 36000;"
-mysql -u root -e "SET GLOBAL max_allowed_packet = 134209536;"
+sudo mysql -u root -e "SET GLOBAL wait_timeout = 36000;"
+sudo mysql -u root -e "SET GLOBAL max_allowed_packet = 134209536;"
+
 # Create database with the correct charset and collation
-mysql -u root -e "CREATE DATABASE zds_test CHARACTER SET = utf8mb4 COLLATE utf8mb4_unicode_ci;"
+sudo mysql -u root -e "CREATE DATABASE zds_test CHARACTER SET = utf8mb4 COLLATE utf8mb4_unicode_ci;"
