2a0a4ec6c5cbd635a39f8889ffc6fdd659d08aae
Amaury Carrade <amaury@carrade.eu>
2020-06-18 18:43:16 +0200

    Écoutez, bon, on tente des trucs (force charset & syntaxe MariaDB)

--- scripts/ci_mysql_setup.sh
+++ scripts/ci_mysql_setup.sh
@@ -10,8 +10,12 @@ sudo mysql -u root -e "SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION,STRICT_TRAN
 sudo mysql -u root -e "SET GLOBAL wait_timeout = 36000;"
 sudo mysql -u root -e "SET GLOBAL max_allowed_packet = 134209536;"
 
+# Ensures correct charset and collation
+sudo mysql -u root -e "SET GLOBAL character_set_server = 'utf8mb4';"
+sudo mysql -u root -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci';"
+
 # Ensures the root user is able to connect without password
 sudo mysql -u root -e "SET Password=PASSWORD('')"
 
 # Create database with the correct charset and collation
-sudo mysql -u root -e "CREATE DATABASE zds_test CHARACTER SET = utf8mb4 COLLATE utf8mb4_unicode_ci;"
+sudo mysql -u root -e "CREATE DATABASE zds_test CHARACTER SET = 'utf8mb4' COLLATE = 'utf8mb4_unicode_ci';"
