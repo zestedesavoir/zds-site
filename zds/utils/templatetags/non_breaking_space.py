6aa7816b7bcd4bdbdbdf9128664849b79ff880e3
Helmasaur <contact@helmasaur.fr>
2020-07-19 04:47:52 +0200

    🎨 Utilisation d'espaces pour l'indentation

--- zds/utils/templatetags/non_breaking_space.py
+++ zds/utils/templatetags/non_breaking_space.py
@@ -4,15 +4,15 @@ register = template.Library()
 
 @register.filter('non_breaking_space')
 def non_breaking_space(str):
-	# Narrow non-breaking space
+    # Narrow non-breaking space
 	str = str.replace(' ;', ' ;')
-	str = str.replace(' ?', ' ?')
-	str = str.replace(' !', ' !')
-	str = str.replace(' %', ' %')
+    str = str.replace(' ?', ' ?')
+    str = str.replace(' !', ' !')
+    str = str.replace(' %', ' %')
 
-	# Non-breaking space
-	str = str.replace('« ', '« ')
-	str = str.replace(' »', ' »')
-	str = str.replace(' :', ' :')
+    # Non-breaking space
+    str = str.replace('« ', '« ')
+    str = str.replace(' »', ' »')
+    str = str.replace(' :', ' :')
 
-	return str
+    return str
