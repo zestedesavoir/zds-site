385a01069a2285bdc298bfb220347b691b8de28e
Helmasaur <contact@helmasaur.fr>
2020-07-20 01:54:52 +0200

    🎨 Ajout d'un saut de ligne pour Travis

--- zds/utils/templatetags/non_breaking_space.py
+++ zds/utils/templatetags/non_breaking_space.py
@@ -2,8 +2,8 @@ from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
 
-
 register = template.Library()
+
 
 @register.filter()
 @stringfilter
