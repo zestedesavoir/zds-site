b5de4abfe130f55298971fb4448edb58242de6ce
Helmasaur <contact@helmasaur.fr>
2020-07-19 16:47:56 +0200

    🎨 Amélioration du code
    - Remplacement de la dernière tabulation utilisée par des espaces
    - ajout de `@stringfilter`
    - utilisation des entités HTML à la place des caractères UTF-8

--- zds/utils/templatetags/non_breaking_space.py
+++ zds/utils/templatetags/non_breaking_space.py
@@ -1,18 +1,21 @@
 from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.filter('non_breaking_space')
+@stringfilter
 def non_breaking_space(str):
-    # Narrow non-breaking space
-	str = str.replace(' ;', ' ;')
-    str = str.replace(' ?', ' ?')
-    str = str.replace(' !', ' !')
-    str = str.replace(' %', ' %')
+    # Narrow non-breaking space: &#8239;
+    str = str.replace(' ;', '&#8239;;')
+    str = str.replace(' ?', '&#8239;?')
+    str = str.replace(' !', '&#8239;!')
+    str = str.replace(' %', '&#8239;%')
 
-    # Non-breaking space
-    str = str.replace('« ', '« ')
-    str = str.replace(' »', ' »')
-    str = str.replace(' :', ' :')
+    # Non-breaking space: &nbsp;
+    str = str.replace('« ', '«&nbsp;')
+    str = str.replace(' »', '&nbsp;»')
+    str = str.replace(' :', '&nbsp;:')
 
-    return str
+    return mark_safe(str)
