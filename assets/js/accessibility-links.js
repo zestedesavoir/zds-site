f0f749a389ca3c829d9b97b2df341a77700537c0
firm1 <contact@firm1.eu>
2020-10-11 23:41:31 +0200

    remarques de @artragis

--- assets/js/accessibility-links.js
+++ assets/js/accessibility-links.js
@@ -6,29 +6,29 @@
 
 (function() {
   function isHidden(el) {
-    var style = window.getComputedStyle(el)
+    const style = window.getComputedStyle(el)
     return (style.display === 'none')
   }
 
   function accessibility(element) {
-    element.addEventListener('focus', function() {
+    element.addEventListener('focus', () => {
       Array.from(document.querySelectorAll('.dropdown'))
         .filter(item => !isHidden(item))
         .forEach(item => item.parentElement.querySelector('.active').classList.remove('active'))
       document.querySelector('#accessibility').classList.add('focused')
     })
-    element.addEventListener('blur', function() {
+    element.addEventListener('blur', () => {
       document.querySelector('#accessibility').classList.remove('focused')
     })
-    element.addEventListener('click', function() {
-      var link = this.getAttribute('href')
-      setTimeout(function() { // Forces the focus on next tick
+    element.addEventListener('click', () => {
+      const link = this.getAttribute('href')
+      setTimeout(() => { // Forces the focus on next tick
         document.querySelectorAll(link).item(0).focus() // Focus the first focusable element
       })
     })
   }
 
-  window.addEventListener('DOMContentLoaded', function() {
+  window.addEventListener('DOMContentLoaded', () => {
     Array.from(document.querySelectorAll('#accessibility a')).forEach(item => accessibility(item))
   })
 })()
