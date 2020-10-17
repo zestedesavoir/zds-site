b5557299e6641cf44894ae0e794a769063e389ad
Arnaud-D <35631001+Arnaud-D@users.noreply.github.com>
2020-10-17 13:08:48 +0200

    Découple le module de MP des notifications

--- zds/mp/commons.py
+++ zds/mp/commons.py
@@ -6,7 +6,7 @@ from django.shortcuts import get_object_or_404
 
 from zds.mp.models import PrivateTopicRead, PrivatePost
 from zds.utils.templatetags.emarkdown import emarkdown
-from zds.notification import signals
+from zds.mp import signals
 
 
 class LeavePrivateTopic(object):
@@ -63,7 +63,7 @@ class UpdatePrivatePost(object):
             except PrivateTopicRead.DoesNotExist:  # record already removed, nothing to do
                 pass
 
-        signals.answer_unread.send(sender=post.privatetopic.__class__, instance=post, user=user)
+        signals.message_unread.send(sender=post.privatetopic.__class__, instance=post, user=user)
 
 
 class SinglePrivatePostObjectMixin(SingleObjectMixin):
