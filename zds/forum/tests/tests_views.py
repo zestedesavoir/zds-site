1634fe287fe0e047d74fb68dfd42aa4cfe34c2db
Arnaud-D <35631001+Arnaud-D@users.noreply.github.com>
2020-10-21 23:15:46 +0200

    Et de deux

--- zds/forum/tests/tests_views.py
+++ zds/forum/tests/tests_views.py
@@ -1912,17 +1912,20 @@ class MessageActionTest(TestCase):
 
 
 class PostUnreadTest(TestCase):
-    def test_failure_post_unread_require_method_get(self):
-        response = self.client.post(reverse("post-unread"), follow=False)
-
+    @patch('zds.forum.signals.post_unread')
+    def test_failure_post_unread_require_method_get(self, post_unread):
+        response = self.client.post(reverse('post-unread'), follow=False)
+        self.assertEqual(post_unread.send.call_count, 0)
         self.assertEqual(405, response.status_code)
 
-    def test_failure_post_unread_with_client_unauthenticated(self):
-        response = self.client.get(reverse("post-unread"), follow=False)
-
+    @patch('zds.forum.signals.post_unread')
+    def test_failure_post_unread_with_client_unauthenticated(self, post_unread):
+        response = self.client.get(reverse('post-unread'), follow=False)
+        self.assertEqual(post_unread.send.call_count, 0)
         self.assertEqual(302, response.status_code)
 
-    def test_failure_post_unread_with_sanctioned_user(self):
+    @patch('zds.forum.signals.post_unread')
+    def test_failure_post_unread_with_sanctioned_user(self, post_unread):
         profile = ProfileFactory()
         profile.can_read = False
         profile.can_write = False
@@ -1931,26 +1934,32 @@ class PostUnreadTest(TestCase):
         self.assertTrue(self.client.login(username=profile.user.username, password="hostel77"))
         response = self.client.get(reverse("post-unread"))
 
+        self.assertEqual(post_unread.send.call_count, 0)
         self.assertEqual(403, response.status_code)
 
-    def test_failure_post_unread_with_wrong_topic_pk(self):
+    @patch('zds.forum.signals.post_unread')
+    def test_failure_post_unread_with_wrong_topic_pk(self, post_unread):
         profile = ProfileFactory()
 
         self.assertTrue(self.client.login(username=profile.user.username, password="hostel77"))
         response = self.client.get(reverse("post-unread") + "?message=abc", follow=False)
 
+        self.assertEqual(post_unread.send.call_count, 0)
         self.assertEqual(404, response.status_code)
 
-    def test_failure_post_unread_with_a_topic_not_found(self):
+    @patch('zds.forum.signals.post_unread')
+    def test_failure_post_unread_with_a_topic_not_found(self, post_unread):
         profile = ProfileFactory()
 
         self.assertTrue(self.client.login(username=profile.user.username, password="hostel77"))
         response = self.client.get(reverse("post-unread") + "?message=99999", follow=False)
 
+        self.assertEqual(post_unread.send.call_count, 0)
         self.assertEqual(404, response.status_code)
 
-    def test_failure_post_unread_of_a_forum_we_cannot_read(self):
-        group = Group.objects.create(name="DummyGroup_1")
+    @patch('zds.forum.signals.post_unread')
+    def test_failure_post_unread_of_a_forum_we_cannot_read(self, post_unread):
+        group = Group.objects.create(name='DummyGroup_1')
 
         profile = ProfileFactory()
         _, forum = create_category_and_forum(group)
@@ -1959,9 +1968,11 @@ class PostUnreadTest(TestCase):
         self.assertTrue(self.client.login(username=profile.user.username, password="hostel77"))
         response = self.client.get(reverse("post-unread") + "?message={}".format(topic.last_message.pk))
 
+        self.assertEqual(post_unread.send.call_count, 0)
         self.assertEqual(403, response.status_code)
 
-    def test_success_post_unread(self):
+    @patch('zds.forum.signals.post_unread')
+    def test_success_post_unread(self, post_unread):
         profile = ProfileFactory()
         _, forum = create_category_and_forum()
         topic = create_topic_in_forum(forum, profile)
@@ -1971,6 +1982,7 @@ class PostUnreadTest(TestCase):
         self.assertTrue(self.client.login(username=profile.user.username, password="hostel77"))
         response = self.client.get(reverse("post-unread") + "?message={}".format(post.pk), follow=False)
 
+        self.assertEqual(post_unread.send.call_count, 1)
         self.assertEqual(302, response.status_code)
 
 
