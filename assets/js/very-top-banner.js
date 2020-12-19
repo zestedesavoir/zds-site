cdfba2365a6d89b8201e72ad8cf0436170123868
Amaury Carrade <amaury@carrade.eu>
2020-12-19 13:50:29 +0100

    Fermeture de la bannière synchronisée sur tous les onglets si définitif (#5945)
    Si la bannière est fermée avec Ctrl+clic ou Maj+clic, elle est fermée sur
    tous les onglets actifs instantanément.

--- assets/js/very-top-banner.js
+++ assets/js/very-top-banner.js
@@ -3,12 +3,24 @@
   if (!banner) {
     return
   }
+
   const slug = banner.dataset.slug
 
-  if (localStorage.getItem('hide-very-top-banner') === slug || sessionStorage.getItem('hide-very-top-banner') === slug) {
-    banner.remove()
-    document.body.classList.remove('has-top-banner')
-  } else {
+  /**
+   * Removes the banner if the local storage says so.
+   * Returns `true` if a banner vas removed.
+   */
+  function removeBannerIfNeeded() {
+    if (localStorage.getItem('hide-very-top-banner') === slug || sessionStorage.getItem('hide-very-top-banner') === slug) {
+      banner.remove()
+      document.body.classList.remove('has-top-banner')
+      return true
+    } else {
+      return false
+    }
+  }
+
+  if (!removeBannerIfNeeded()) {
     document.body.classList.add('has-top-banner')
 
     banner.querySelector('button').addEventListener('click', e => {
@@ -20,5 +32,11 @@
         localStorage.setItem('hide-very-top-banner', slug)
       }
     })
+
+    // Runs when the local storage is modified from another tab. So the banner
+    // will be removed from all tabs at once if removed permanently using ctrl
+    // or shift, but not in other cases as it's then stored on the session
+    // storage.
+    window.addEventListener('storage', removeBannerIfNeeded)
   }
 }())
