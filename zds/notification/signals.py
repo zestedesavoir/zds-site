ceef5ccaafd27d48e3f272e7a7d4f76d2469964c
Arnaud-D <35631001+Arnaud-D@users.noreply.github.com>
2020-12-19 12:46:03 +0100

    Supprime le signal inutilisé `edit_content`

--- zds/notification/signals.py
+++ zds/notification/signals.py
@@ -3,9 +3,6 @@ from django.dispatch import Signal
 # is sent whenever a resource is created.
 new_content = Signal(providing_args=["instance", "user", "by_email"])
 
-# is sent whenever a content is edited.
-edit_content = Signal(providing_args=["instance", "action"])
-
 # is sent when a content is read (topic, article or tutorial)
 content_read = Signal(providing_args=["instance", "user", "target"])
 
