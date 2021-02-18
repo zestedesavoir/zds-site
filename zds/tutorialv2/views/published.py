77dc443d20b1073c1c73625cbe6ec984b9e98dfc
Shiga <75756285+HighSkyStrength@users.noreply.github.com>
2021-02-18 17:03:37 +0100

    lines 109-112
    Etant débutant, j'ai essayé de régler ce problème. https://github.com/zestedesavoir/zds-site/issues/5793

--- zds/tutorialv2/views/published.py
+++ zds/tutorialv2/views/published.py
@@ -107,6 +107,8 @@ class DisplayOnlineContent(FeatureableMixin, SingleOnlineContentDetailViewMixin)
         if self.current_content_type in ('ARTICLE', 'OPINION'):
             queryset_pagination = PublishedContent.objects.filter(content_type=self.current_content_type,
                                                                   must_redirect=False)
+            if self.current_content_type == 'OPINION':
+    queryset_pagination = queryset_pagination.filter(content__sha_picked=F('sha_public'))
 
             context['previous_content'] = queryset_pagination \
                 .filter(publication_date__lt=self.public_content_object.publication_date) \
