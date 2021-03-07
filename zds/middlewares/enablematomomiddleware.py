fbcbbb0e36728c5adaba7c1e52230514de3f3248
artragis <artragis@gmail.com>
2021-03-07 20:26:00 +0100

    try with threads

--- zds/middlewares/enablematomomiddleware.py
+++ zds/middlewares/enablematomomiddleware.py
@@ -1,17 +1,19 @@
+from queue import Queue
+
 import requests
 
 from django.conf import settings
-from multiprocessing import Process, get_context
-from multiprocessing import Queue as MultiProcessingQueue
+from threading import Thread
 
 matomo_api_url = "{0}/matomo.php".format(settings.ZDS_APP["site"]["matomoUrl"])
 matomo_site_id = settings.ZDS_APP["site"]["matomoID"]
 matomo_api_version = 1
 
 
-def _background_process(queue: MultiProcessingQueue):
+def _background_process(queue: Queue):
     data = queue.get(block=True)
     while data:
+        print("toto")
         requests.get(
             matomo_api_url,
             params={
@@ -30,11 +32,13 @@ def _background_process(queue: MultiProcessingQueue):
 
 class EnableMatomoMiddleware:
     def __init__(self, get_response):
+
         self.get_response = get_response
-        ctx = get_context("fork")
-        self.queue = ctx.Queue()
-        self.worker = ctx.Process(target=_background_process, args=(self.queue,))
+        self.queue = Queue()
+        self.worker = Thread(target=_background_process, args=(self.queue,))
+        print("will start")
         self.worker.start()
+        print("started")
 
     def __call__(self, request):
         return self.process_response(request, self.get_response(request))
@@ -45,6 +49,7 @@ class EnableMatomoMiddleware:
         client_accept_language = request.META.get("HTTP_ACCEPT_LANGUAGE", "")
         client_url = "{0}://{1}{2}".format(request.scheme, request.get_host(), request.path)
         r_path = request.path
+        print("put")
         self.queue.put(
             {
                 "client_user_agent": client_user_agent,
@@ -54,6 +59,7 @@ class EnableMatomoMiddleware:
                 "r_path": r_path,
             }
         )
+        print("is async?")
 
     def process_response(self, request, response):
         exclude_paths = ["/contenus", "/mp", "/munin"]
