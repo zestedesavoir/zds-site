434e34ff3254bc7be683e04f1df49dbb54609f37
rowin <rowin@anhydride-acide.fr>
2021-03-07 17:55:46 +0100

    Créé un filtre pour trier les topics par date

---
+++ zds/utils/templatetags/topics_sort.py
@@ -0,0 +1,13 @@
+from django import template
+from django.core.cache import cache
+
+
+register = template.Library()
+
+
+@register.filter("topics_sort")
+def topics_sort(topics):
+    """
+    :return: the topics sorted by last update (last updated first)
+    """
+    return sorted(topics, key=lambda topic: topic.update, reverse=True)
