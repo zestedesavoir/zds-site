61c0ade60464ef945f2e7dbae07b8aa93b258a32
Arnaud-D <35631001+Arnaud-D@users.noreply.github.com>
2021-04-04 21:49:46 +0200

    Fix arguments in definition of signals

--- zds/mp/signals.py
+++ zds/mp/signals.py
@@ -1,14 +1,14 @@
 from django.dispatch import Signal
 
 # New message management
-message_added = Signal(providing_args=["instance", "user", "by_email"])
+message_added = Signal()  # providing_args=["post", "by_email", "no_notification_for"]
 
 # Read/Unread management
-topic_read = Signal(providing_args=["instance", "user", "target"])
-message_unread = Signal(providing_args=["instance", "user"])
+topic_read = Signal()  # providing_args=["instance", "user"]
+message_unread = Signal()  # providing_args=["instance", "user"]
 
 # Participant management
-participant_added = Signal(providing_args=["instance"])
-participant_removed = Signal(providing_args=["instance"])
+participant_added = Signal()  # providing_args=["topic", "silent"]
+participant_removed = Signal()  # providing_args=["topic"]
 
-topic_created = Signal(providing_args=["instance"])
+topic_created = Signal()  # providing_args=["topic", "by_email"]
