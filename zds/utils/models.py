3bb2b510fde97dc44471ccd664b6ea09c69e3b47
Philippe MILINK <philippe.milink@gmx.fr>
2021-04-27 17:44:27 +0200

    Met à jour black

--- zds/utils/models.py
+++ zds/utils/models.py
@@ -547,7 +547,7 @@ class Comment(models.Model):
         self.save()
 
     def get_user_vote(self, user):
-        """ Get a user vote (like, dislike or neutral) """
+        """Get a user vote (like, dislike or neutral)"""
         if user.is_authenticated:
             try:
                 user_vote = "like" if CommentVote.objects.get(user=user, comment=self).positive else "dislike"
@@ -559,7 +559,7 @@ class Comment(models.Model):
         return user_vote
 
     def set_user_vote(self, user, vote):
-        """ Set a user vote (like, dislike or neutral) """
+        """Set a user vote (like, dislike or neutral)"""
         if vote == "neutral":
             CommentVote.objects.filter(user=user, comment=self).delete()
         else:
@@ -569,7 +569,7 @@ class Comment(models.Model):
         self.dislike = CommentVote.objects.filter(positive=False, comment=self).count()
 
     def get_votes(self, type=None):
-        """ Get the non-anonymous votes """
+        """Get the non-anonymous votes"""
         if not hasattr(self, "votes"):
             self.votes = (
                 CommentVote.objects.filter(comment=self, id__gt=settings.VOTES_ID_LIMIT).select_related("user").all()
@@ -578,11 +578,11 @@ class Comment(models.Model):
         return self.votes
 
     def get_likers(self):
-        """ Get the list of the users that liked this Comment """
+        """Get the list of the users that liked this Comment"""
         return [vote.user for vote in self.get_votes() if vote.positive]
 
     def get_dislikers(self):
-        """ Get the list of the users that disliked this Comment """
+        """Get the list of the users that disliked this Comment"""
         return [vote.user for vote in self.get_votes() if not vote.positive]
 
     def get_absolute_url(self):
