190a7db60398d3ae9f2b8f272c992b7fb09f8f7a
Arnaud-D <35631001+Arnaud-D@users.noreply.github.com>
2022-02-20 08:03:01 +0100

    Déplace les tests et factories de featured dans un dossier dédié

--- zds/featured/tests/tests.py
+++ zds/featured/tests/tests.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from django.utils.translation import gettext as _
 
 from zds.member.factories import StaffProfileFactory, ProfileFactory
-from zds.featured.factories import FeaturedResourceFactory
+from zds.featured.tests.factories import FeaturedResourceFactory
 from zds.featured.models import FeaturedResource, FeaturedMessage, FeaturedRequested
 from zds.forum.factories import ForumCategoryFactory, ForumFactory, TopicFactory
 from zds.gallery.factories import GalleryFactory, ImageFactory
