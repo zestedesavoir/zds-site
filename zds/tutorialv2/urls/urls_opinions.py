972f44690d44737bdc1d6b6b54674238c3a11ead
Arnaud-D <35631001+Arnaud-D@users.noreply.github.com>
2022-05-07 10:26:22 +0200

    Refactorise les URL des contenus

--- zds/tutorialv2/urls/urls_opinions.py
+++ zds/tutorialv2/urls/urls_opinions.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path
+from django.urls import path, path
 
 from zds.tutorialv2.feeds import LastOpinionsFeedRSS, LastOpinionsFeedATOM
 from zds.tutorialv2.views.lists import ListOpinions, ContentOfAuthor
@@ -7,35 +7,27 @@ from zds.tutorialv2.views.display import DisplayOnlineOpinion
 
 urlpatterns = [
     # Flux
-    re_path(r"^flux/rss/$", LastOpinionsFeedRSS(), name="feed-rss"),
-    re_path(r"^flux/atom/$", LastOpinionsFeedATOM(), name="feed-atom"),
+    path("flux/rss/", LastOpinionsFeedRSS(), name="feed-rss"),
+    path("flux/atom/", LastOpinionsFeedATOM(), name="feed-atom"),
     # View
-    re_path(r"^(?P<pk>\d+)/(?P<slug>.+)/$", DisplayOnlineOpinion.as_view(), name="view"),
+    path("<int:pk>/<slug:slug>/", DisplayOnlineOpinion.as_view(), name="view"),
     # downloads:
-    re_path(
-        r"^md/(?P<pk>\d+)/(?P<slug>.+)\.md$", DownloadOnlineOpinion.as_view(requested_file="md"), name="download-md"
-    ),
-    re_path(
-        r"^html/(?P<pk>\d+)/(?P<slug>.+)\.html$",
+    path("md/<int:pk>/<slug:slug>.md", DownloadOnlineOpinion.as_view(requested_file="md"), name="download-md"),
+    path(
+        "html/<int:pk>/<slug:slug>.html",
         DownloadOnlineOpinion.as_view(requested_file="html"),
         name="download-html",
     ),
-    re_path(
-        r"^pdf/(?P<pk>\d+)/(?P<slug>.+)\.pdf$", DownloadOnlineOpinion.as_view(requested_file="pdf"), name="download-pdf"
-    ),
-    re_path(
-        r"^epub/(?P<pk>\d+)/(?P<slug>.+)\.epub$",
+    path("pdf/<int:pk>/<slug:slug>.pdf", DownloadOnlineOpinion.as_view(requested_file="pdf"), name="download-pdf"),
+    path(
+        "epub/<int:pk>/<slug:slug>.epub",
         DownloadOnlineOpinion.as_view(requested_file="epub"),
         name="download-epub",
     ),
-    re_path(
-        r"^zip/(?P<pk>\d+)/(?P<slug>.+)\.zip$", DownloadOnlineOpinion.as_view(requested_file="zip"), name="download-zip"
-    ),
-    re_path(
-        r"^tex/(?P<pk>\d+)/(?P<slug>.+)\.tex$", DownloadOnlineOpinion.as_view(requested_file="tex"), name="download-tex"
-    ),
+    path("zip/<int:pk>/<slug:slug>.zip", DownloadOnlineOpinion.as_view(requested_file="zip"), name="download-zip"),
+    path("tex/<int:pk>/<slug:slug>.tex", DownloadOnlineOpinion.as_view(requested_file="tex"), name="download-tex"),
     # Listing
-    re_path(r"^$", ListOpinions.as_view(), name="list"),
+    path("", ListOpinions.as_view(), name="list"),
     path(
         "voir/<str:username>/",
         ContentOfAuthor.as_view(type="OPINION", context_object_name="opinions", sort="creation"),
--- zds/tutorialv2/urls/urls_validations.py
+++ zds/tutorialv2/urls/urls_validations.py
@@ -1,5 +1,4 @@
-from django.urls import re_path
-
+from django.urls import path
 from zds.tutorialv2.views.validations_contents import (
     AskValidationForContent,
     ReserveValidation,
@@ -23,28 +22,28 @@ from zds.tutorialv2.views.validations_opinions import (
 )
 
 urlpatterns = [
-    re_path(r"^historique/(?P<pk>\d+)/(?P<slug>.+)/$", ValidationHistoryView.as_view(), name="history"),
+    path("historique/<int:pk>/<slug:slug>/", ValidationHistoryView.as_view(), name="history"),
     # VALIDATION BEFORE PUBLICATION
     # 1. ask validation
-    re_path(r"^proposer/(?P<pk>\d+)/(?P<slug>.+)/$", AskValidationForContent.as_view(), name="ask"),
+    path("proposer/<int:pk>/<slug:slug>/", AskValidationForContent.as_view(), name="ask"),
     # 2. take (or cancel) validation
-    re_path(r"^reserver/(?P<pk>\d+)/$", ReserveValidation.as_view(), name="reserve"),
-    re_path(r"^annuler/(?P<pk>\d+)/$", CancelValidation.as_view(), name="cancel"),
+    path("reserver/<int:pk>/", ReserveValidation.as_view(), name="reserve"),
+    path("annuler/<int:pk>/", CancelValidation.as_view(), name="cancel"),
     # 3. accept or reject validation
-    re_path(r"^refuser/(?P<pk>\d+)/$", RejectValidation.as_view(), name="reject"),
-    re_path(r"^accepter/(?P<pk>\d+)/$", AcceptValidation.as_view(), name="accept"),
+    path("refuser/<int:pk>/", RejectValidation.as_view(), name="reject"),
+    path("accepter/<int:pk>/", AcceptValidation.as_view(), name="accept"),
     # 4. cancel validation after publication
-    re_path(r"^revoquer/(?P<pk>\d+)/(?P<slug>.+)/$", RevokeValidation.as_view(), name="revoke"),
+    path("revoquer/<int:pk>/<slug:slug>/", RevokeValidation.as_view(), name="revoke"),
     # NO VALIDATION BEFORE PUBLICATION
-    re_path(r"^publier/(?P<pk>\d+)/(?P<slug>.+)/$", PublishOpinion.as_view(), name="publish-opinion"),
-    re_path(r"^depublier/(?P<pk>\d+)/(?P<slug>.+)/$", UnpublishOpinion.as_view(), name="unpublish-opinion"),
-    re_path(r"^choisir/(?P<pk>\d+)/(?P<slug>.+)/$", PickOpinion.as_view(), name="pick-opinion"),
-    re_path(r"^ignorer/(?P<pk>\d+)/(?P<slug>.+)/$", DoNotPickOpinion.as_view(), name="ignore-opinion"),
-    re_path(r"^operation/annuler/(?P<pk>\d+)/$", RevokePickOperation.as_view(), name="revoke-ignore-opinion"),
-    re_path(r"^retirer/(?P<pk>\d+)/(?P<slug>.+)/$", UnpickOpinion.as_view(), name="unpick-opinion"),
-    re_path(r"^promouvoir/(?P<pk>\d+)/(?P<slug>.+)/$", PromoteOpinionToArticle.as_view(), name="promote-opinion"),
-    re_path(r"^marquer-obsolete/(?P<pk>\d+)/$", MarkObsolete.as_view(), name="mark-obsolete"),
+    path("publier/<int:pk>/<slug:slug>/", PublishOpinion.as_view(), name="publish-opinion"),
+    path("depublier/<int:pk>/<slug:slug>/", UnpublishOpinion.as_view(), name="unpublish-opinion"),
+    path("choisir/<int:pk>/<slug:slug>/", PickOpinion.as_view(), name="pick-opinion"),
+    path("ignorer/<int:pk>/<slug:slug>/", DoNotPickOpinion.as_view(), name="ignore-opinion"),
+    path("operation/annuler/<int:pk>/", RevokePickOperation.as_view(), name="revoke-ignore-opinion"),
+    path("retirer/<int:pk>/<slug:slug>/", UnpickOpinion.as_view(), name="unpick-opinion"),
+    path("promouvoir/<int:pk>/<slug:slug>/", PromoteOpinionToArticle.as_view(), name="promote-opinion"),
+    path("marquer-obsolete/<int:pk>/", MarkObsolete.as_view(), name="mark-obsolete"),
     # VALIDATION VIEWS FOR STAFF
-    re_path(r"^billets/$", ValidationOpinionListView.as_view(), name="list-opinion"),
-    re_path(r"^$", ValidationListView.as_view(), name="list"),
+    path("billets/", ValidationOpinionListView.as_view(), name="list-opinion"),
+    path("", ValidationListView.as_view(), name="list"),
 ]
