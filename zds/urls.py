0f4040a9fe197e795c40fa28ba5c32c112b5dcf4
Arnaud-D <35631001+Arnaud-D@users.noreply.github.com>
2022-05-07 09:51:13 +0200

    Refactorise les URL de recherche

--- zds/urls.py
+++ zds/urls.py
@@ -89,7 +89,7 @@ urlpatterns = [
     re_path(r"^admin/", admin.site.urls),
     re_path(r"^pages/", include(("zds.pages.urls", ""))),
     re_path(r"^galerie/", include(("zds.gallery.urls", ""))),
-    re_path(r"^rechercher/", include(("zds.searchv2.urls", "zds.searchv2"), namespace="search")),
+    path("rechercher/", include("zds.searchv2.urls")),
     re_path(r"^munin/", include(("zds.munin.urls", ""))),
     re_path(r"^mise-en-avant/", include(("zds.featured.urls", ""))),
     re_path(r"^notifications/", include(("zds.notification.urls", ""))),
