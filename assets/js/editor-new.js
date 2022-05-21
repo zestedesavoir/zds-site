1e0aae23cc3fcfa1bfdb79e345c41555500c9841
codeure24 <85784092+antoineB24@users.noreply.github.com>
2022-05-21 18:16:05 +0200

    Ajoute les smiley popcorn et bounce dans les menus déroulants de l'éditeur (#6306)

--- assets/js/editor-new.js
+++ assets/js/editor-new.js
@@ -657,6 +657,22 @@
               },
               className: 'emoji ange',
               title: ':ange:'
+            },
+            {
+              name: 'char9',
+              action: (e) => {
+                toggleEmoji(e.codemirror, ':popcorn:')
+              },
+              className: 'emoji popcorn',
+              title: ':popcorn:'
+            },
+            {
+              name: 'char10',
+              action: (e) => {
+                toggleEmoji(e.codemirror, ':bounce:')
+              },
+              className: 'emoji bounce',
+              title: ':bounce:'
             }
           ]
         },
