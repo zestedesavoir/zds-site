2f18d2b96bcf0b3715be5cafa6c9b8388187fa42
Philippe MILINK <philippe.milink@gmx.fr>
2022-06-04 17:58:51 +0200

    Ajout de tests pour les flux RSS et ATOM des billets

--- zds/tutorialv2/tests/tests_feeds.py
+++ zds/tutorialv2/tests/tests_feeds.py
@@ -7,7 +7,14 @@ from zds.gallery.tests.factories import UserGalleryFactory
 from zds.member.tests.factories import ProfileFactory
 from zds.forum.tests.factories import TagFactory
 from zds.tutorialv2.models.database import PublishedContent
-from zds.tutorialv2.feeds import LastTutorialsFeedRSS, LastTutorialsFeedATOM, LastArticlesFeedRSS, LastArticlesFeedATOM
+from zds.tutorialv2.feeds import (
+    LastTutorialsFeedRSS,
+    LastTutorialsFeedATOM,
+    LastArticlesFeedRSS,
+    LastArticlesFeedATOM,
+    LastOpinionsFeedRSS,
+    LastOpinionsFeedATOM,
+)
 from zds.tutorialv2.tests.factories import (
     PublishableContentFactory,
     ContainerFactory,
@@ -354,3 +361,145 @@ class LastArticlesFeedsTest(TutorialTestMixin, TestCase):
 
         self.articlefeed.query_params = {"tag": "invalid"}
         self.assertRaises(Http404, self.articlefeed.items)
+
+
+@override_settings(ZDS_APP=overridden_zds_app)
+class LastOpinionsFeedsTest(TutorialTestMixin, TestCase):
+    def setUp(self):
+        self.overridden_zds_app = overridden_zds_app
+        # don't build PDF to speed up the tests
+        overridden_zds_app["content"]["build_pdf_when_published"] = False
+
+        self.subcategory = SubCategoryFactory()
+        self.tag = TagFactory()
+        self.user_author = ProfileFactory().user
+
+        # create an opinion
+        self.opinion = PublishedContentFactory(author_list=[self.user_author], type="OPINION")
+        self.opinion.subcategory.add(self.subcategory)
+        self.opinion.tags.add(self.tag)
+        self.opinion.save()
+
+        self.opinionfeed = LastOpinionsFeedRSS()
+
+    def test_is_well_setup(self):
+        """Test that base parameters are Ok"""
+
+        self.assertEqual(self.opinionfeed.link, "/tribunes/")
+        reftitle = "Tribunes sur {}".format(overridden_zds_app["site"]["literal_name"])
+        self.assertEqual(self.opinionfeed.title, reftitle)
+        refdescription = "Les derniers billets des tribunes parus sur {}.".format(
+            overridden_zds_app["site"]["literal_name"]
+        )
+        self.assertEqual(self.opinionfeed.description, refdescription)
+
+        atom = LastOpinionsFeedATOM()
+        self.assertEqual(atom.subtitle, refdescription)
+
+    def test_get_items(self):
+        """basic test sending back the article"""
+
+        ret = list(self.opinionfeed.items())
+        self.assertEqual(ret[0].content, self.opinion)
+
+    def test_get_pubdate(self):
+        """test the return value of pubdate"""
+
+        ref = PublishedContent.objects.get(content__pk=self.opinion.pk).publication_date
+        opinion = list(self.opinionfeed.items())[0]
+        ret = self.opinionfeed.item_pubdate(item=opinion)
+        self.assertEqual(ret.date(), ref.date())
+
+    def test_get_title(self):
+        """test the return value of title"""
+
+        ref = self.opinion.title
+        opinion = list(self.opinionfeed.items())[0]
+        ret = self.opinionfeed.item_title(item=opinion)
+        self.assertEqual(ret, ref)
+
+    def test_get_description(self):
+        """test the return value of description"""
+
+        ref = self.opinion.description
+        opinion = list(self.opinionfeed.items())[0]
+        ret = self.opinionfeed.item_description(item=opinion)
+        self.assertEqual(ret, ref)
+
+    def test_get_author_name(self):
+        """test the return value of author name"""
+
+        ref = self.user_author.username
+        opinion = list(self.opinionfeed.items())[0]
+        ret = self.opinionfeed.item_author_name(item=opinion)
+        self.assertEqual(ret, ref)
+
+    def test_get_item_link(self):
+        """test the return value of item link"""
+
+        ref = self.opinion.get_absolute_url_online()
+        opinion = list(self.opinionfeed.items())[0]
+        ret = self.opinionfeed.item_link(item=opinion)
+        self.assertEqual(ret, ref)
+
+    def test_filters(self):
+        """Test filtering by category & tag"""
+        subcategory2 = SubCategoryFactory()
+        subcategory3 = SubCategoryFactory()
+        tag2 = TagFactory()
+        tag3 = TagFactory()
+
+        # Add a new opinion & publish it
+
+        opinion2 = PublishedContentFactory(author_list=[self.user_author], type="OPINION")
+        opinion2.subcategory.add(subcategory2)
+        opinion2.tags.add(self.tag)
+        opinion2.tags.add(tag2)
+        opinion2.save()
+
+        # Default view
+
+        ret = [item.content for item in self.opinionfeed.items()]
+        self.assertEqual(ret, [opinion2, self.opinion])
+
+        # Filter by subcategory
+
+        self.opinionfeed.query_params = {"subcategory": self.subcategory.slug}
+        ret = [item.content for item in self.opinionfeed.items()]
+        self.assertEqual(ret, [self.opinion])
+
+        self.opinionfeed.query_params = {"subcategory": f" {self.subcategory.slug} "}
+        ret = [item.content for item in self.opinionfeed.items()]
+        self.assertEqual(ret, [self.opinion])
+
+        self.opinionfeed.query_params = {"subcategory": subcategory2.slug}
+        ret = [item.content for item in self.opinionfeed.items()]
+        self.assertEqual(ret, [opinion2])
+
+        self.opinionfeed.query_params = {"subcategory": subcategory3.slug}
+        ret = [item.content for item in self.opinionfeed.items()]
+        self.assertEqual(ret, [])
+
+        self.opinionfeed.query_params = {"subcategory": "invalid"}
+        self.assertRaises(Http404, self.opinionfeed.items)
+
+        # Filter by tag
+
+        self.opinionfeed.query_params = {"tag": self.tag.slug}
+        ret = [item.content for item in self.opinionfeed.items()]
+        self.assertEqual(ret, [opinion2, self.opinion])
+
+        self.opinionfeed.query_params = {"tag": tag2.slug}
+        ret = [item.content for item in self.opinionfeed.items()]
+        self.assertEqual(ret, [opinion2])
+
+        self.opinionfeed.query_params = {"tag": f" {tag2.slug} "}
+        ret = [item.content for item in self.opinionfeed.items()]
+        self.assertEqual(ret, [opinion2])
+
+        self.opinionfeed.query_params = {"tag": tag3.slug}
+        ret = [item.content for item in self.opinionfeed.items()]
+        self.assertEqual(ret, [])
+
+        self.opinionfeed.query_params = {"tag": "invalid"}
+        self.assertRaises(Http404, self.opinionfeed.items)
