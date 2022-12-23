b485f74c2cfd3778c94599dd231428162085c279
Arnaud-D <35631001+Arnaud-D@users.noreply.github.com>
2022-12-23 19:12:09 +0100

    Permet d'écrire des billets ou articles avec une structure de tutoriel

--- zds/tutorialv2/urls/urls_opinions.py
+++ zds/tutorialv2/urls/urls_opinions.py
@@ -3,21 +3,32 @@ from django.urls import path
 from zds.tutorialv2.feeds import LastOpinionsFeedRSS, LastOpinionsFeedATOM
 from zds.tutorialv2.views.lists import ListOpinions, ContentOfAuthor
 from zds.tutorialv2.views.download_online import DownloadOnlineOpinion
-from zds.tutorialv2.views.display import OpinionOnlineView
+from zds.tutorialv2.views.display import OpinionOnlineView, ContainerOnlineView
 
-urlpatterns = [
-    # Flux
+feed_patterns = [
     path("flux/rss/", LastOpinionsFeedRSS(), name="feed-rss"),
     path("flux/atom/", LastOpinionsFeedATOM(), name="feed-atom"),
-    # View
+]
+
+display_patterns = [
     path("<int:pk>/<slug:slug>/", OpinionOnlineView.as_view(), name="view"),
-    # downloads:
+    path(
+        "<int:pk>/<slug:slug>/<slug:parent_container_slug>/<slug:container_slug>/",
+        ContainerOnlineView.as_view(),
+        name="view-container",
+    ),
+    path("<int:pk>/<slug:slug>/<slug:container_slug>/", ContainerOnlineView.as_view(), name="view-container"),
+]
+
+download_patterns = [
     path("md/<int:pk>/<slug:slug>.md", DownloadOnlineOpinion.as_view(requested_file="md"), name="download-md"),
     path("pdf/<int:pk>/<slug:slug>.pdf", DownloadOnlineOpinion.as_view(requested_file="pdf"), name="download-pdf"),
     path("epub/<int:pk>/<slug:slug>.epub", DownloadOnlineOpinion.as_view(requested_file="epub"), name="download-epub"),
     path("zip/<int:pk>/<slug:slug>.zip", DownloadOnlineOpinion.as_view(requested_file="zip"), name="download-zip"),
     path("tex/<int:pk>/<slug:slug>.tex", DownloadOnlineOpinion.as_view(requested_file="tex"), name="download-tex"),
-    # Listing
+]
+
+listing_patterns = [
     path("", ListOpinions.as_view(), name="list"),
     path(
         "voir/<str:username>/",
@@ -25,3 +36,5 @@ urlpatterns = [
         name="find-opinion",
     ),
 ]
+
+urlpatterns = feed_patterns + display_patterns + download_patterns + listing_patterns
