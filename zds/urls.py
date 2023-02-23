36d16d566fdd9b927e88c6b8c1b2f154d95efd64
LOHEZIC VICTOR <vlohezic@enseirb-matmeca.fr>
2023-02-23 14:20:01 +0100

    Modification pour rendre searchv3 indépendant de searchv2

--- zds/urls.py
+++ zds/urls.py
@@ -88,7 +88,7 @@ urlpatterns = [
     re_path(r"^admin/", admin.site.urls),
     path("pages/", include("zds.pages.urls")),
     path("galerie/", include("zds.gallery.urls")),
-    path("rechercher/", include("zds.searchv2.urls")),
+    path("rechercher/", include("zds.searchv3.urls")),
     path("munin/", include(("zds.munin.urls", ""))),
     path("mise-en-avant/", include("zds.featured.urls")),
     path("notifications/", include("zds.notification.urls")),
