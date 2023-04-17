1725289047d97a2bd395c4a5f1e531e985fcc076
LOHEZIC VICTOR <vlohezic@enseirb-matmeca.fr>
2023-04-17 15:01:09 +0200

    Ajout d'une description des champs du label

--- zds/tutorialv2/models/labels.py
+++ zds/tutorialv2/models/labels.py
@@ -11,9 +11,13 @@ class Label(models.Model):
         verbose_name = "Label"
         verbose_name_plural = "Labels"
 
-    name = models.CharField("Nom", max_length=80)
-    description = models.TextField("Description", blank=True)
-    slug = models.SlugField(max_length=80, unique=True)
+    name = models.CharField("Nom", max_length=80, help_text="Nom du lable")
+    description = models.TextField("Description", blank=True, help_text="Description du label")
+    slug = models.SlugField(
+        max_length=80,
+        unique=True,
+        help_text="L'URL pour voir les contenus associés au label est de la forme contenus/labels/slug",
+    )
 
     def __str__(self):
         return self.name
