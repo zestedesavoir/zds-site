b01d9ba343b00ab88ac34d812c737c432a5005e3
Naël De Luca <ndeluca@bordeaux-inp.fr>
2023-05-14 19:45:38 +0200

    add highlight for forum topics and messages and change highlight style to match previous one

--- zds/utils/templatetags/highlight.py
+++ zds/utils/templatetags/highlight.py
@@ -10,8 +10,10 @@ def highlight(input_string):
     stripped_string = input_string.replace("<mark>", "___MARK_TAG___ ")
     stripped_string = stripped_string.replace("</mark>", "___/MARK_TAG___")
     stripped_string = strip_tags(stripped_string)
-    stripped_string = stripped_string.replace("___MARK_TAG___", "<mark>")
+    stripped_string = stripped_string.replace("___MARK_TAG___", '<mark class="highlighted">')
     stripped_string = stripped_string.replace("___/MARK_TAG___", "</mark>")
     # adding the tag that allows highlighting
-    highlighted_string = stripped_string.replace("&lt;mark&gt;", "<mark>").replace("&lt;/mark&gt;", "</mark>")
+    highlighted_string = stripped_string.replace("&lt;mark&gt;", '<mark class="highlighted">').replace(
+        "&lt;/mark&gt;", "</mark>"
+    )
     return mark_safe(highlighted_string)
