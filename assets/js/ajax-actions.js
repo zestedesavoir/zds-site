91e250eb98ab62b50d95ac401d578a3735b0ec1a
Migwel <blog@migwel.dev>
2023-05-26 23:37:17 +0200

    Ajouter la copie du permalien dans le menu dropdown des messages

--- assets/js/ajax-actions.js
+++ assets/js/ajax-actions.js
@@ -320,48 +320,4 @@
       editor.focus()
     }
   })
-
-  /*
-     * Mark a message useful
-     */
-  $('.topic-message').on('click', "[data-ajax-input='mark-message-as-useful']", function(e) {
-    const $button = $(this)
-    const $form = $button.parents('form:first')
-    const $message = $form.parents('article')
-    const $usefulText = $message.find("[data-ajax-output='mark-message-as-useful']")
-    const csrfmiddlewaretoken = $form.find('input[name=csrfmiddlewaretoken]').val()
-
-    const $desktopText = $button.find('.desktop')
-    const $mobileText = $button.find('.until-desktop')
-
-    $.ajax({
-      url: $form.attr('action'),
-      type: 'POST',
-      data: {
-        csrfmiddlewaretoken: csrfmiddlewaretoken
-      },
-      success: function() {
-        $message.toggleClass('helpful')
-        $usefulText.toggleClass('hidden')
-        $button.blur()
-
-        // Toggle both mobile and desktop texts
-        if ($desktopText.length !== 0 && $mobileText.length !== 0) {
-          const currentDesktopText = $desktopText.text()
-          const currentMobileText = $mobileText.text()
-
-          $desktopText.text($button.data('content-on-click'))
-          $mobileText.text($button.data('content-on-click-mobile'))
-
-          $button.data('content-on-click', currentDesktopText)
-          $button.data('content-on-click-mobile', currentMobileText)
-        } else {
-          $button.toggleText('content-on-click')
-        }
-      }
-    })
-
-    e.stopPropagation()
-    e.preventDefault()
-  })
 })(jQuery)
