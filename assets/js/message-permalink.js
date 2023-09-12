5c1dadd7782a31cbc270c66f940de06315e12354
Migwel <blog@migwel.dev>
2023-09-12 23:10:58 +0200

    Simplifie les data attributes utilisés pour la copie du permalien

--- assets/js/message-permalink.js
+++ assets/js/message-permalink.js
@@ -1,11 +1,11 @@
 (function() {
   'use strict'
 
-  $('.topic-message').on('click', "[data-ajax-input='copy-hyperlink']", function(e) {
+  $('.topic-message').on('click', '[data-copy-permalink]', function(e) {
     e.stopPropagation()
     e.preventDefault()
-    navigator.clipboard.writeText($(this).attr("data-message-url"));
-    const dropdown = e.target.closest(".dropdown")
+    navigator.clipboard.writeText($(this).attr('data-copy-permalink'))
+    const dropdown = e.target.closest('.dropdown')
     dropdown.removeAttribute('open')
   })
 })(jQuery)
