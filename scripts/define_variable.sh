672354b20a204af53cea84b84a022e677a914f29
Philippe MILINK <philippe.milink@gmx.fr>
2024-03-17 17:01:25 +0100

    Mise à jour de Typesense

--- scripts/define_variable.sh
+++ scripts/define_variable.sh
@@ -15,7 +15,7 @@ if [[ $ZDS_NVM_VERSION == "" ]]; then
 fi
 
 if [[ $ZDS_TYPESENSE_VERSION == "" ]]; then
-    ZDS_TYPESENSE_VERSION="0.24.1" # needs to be also updated in .github/workflows/ci.yml
+    ZDS_TYPESENSE_VERSION="0.25.2" # needs to be also updated in .github/workflows/ci.yml
 fi
 
 if [[ $ZDS_TYPESENSE_API_KEY == "" ]]; then
