82fe692573f97700d32dcd147b616973d213216a
Philippe MILINK <philippe.milink@gmx.fr>
2024-07-28 17:41:47 +0200

    Augmente le timeout pour parler à Typesense

--- zds/settings/abstract_base/zds.py
+++ zds/settings/abstract_base/zds.py
@@ -23,7 +23,7 @@ SEARCH_CONNECTION = {
         }
     ],
     "api_key": "xyz",
-    "connection_timeout_seconds": 2,
+    "connection_timeout_seconds": 5,
 }
 
 # Anonymous [Dis]Likes. Authors of [dis]likes before those pk will never be shown
