30d805bdb450b908165d588ba536085e4a48d14c
Ph. SW. <philippe.milink@gmx.fr>
2024-09-01 12:30:28 +0200

    Corrige le nombre de sessions rapporté par Munin (#6636)
    Bug introduit par 2b2215e02db3233d2360060e1ab1243d5801f16c (PR #6021),
    où le backend pour gérer les sessions passe de
    django.contrib.sessions.backends.cached_db à
    zds.utils.custom_cached_db_backend, ce qui change la table utilisée pour
    stocker les sessions et le modèle à utiliser pour les manipuler. La vue
    qui compte le nombre de sessions pour Munin n'a pas été mise à jour
    pour utiliser le nouveau modèle.
    
    - Utilise les classes de sessions définies dans les paramètres, plutôt
      que forcément django.contrib.sessions
    - Ajoute un test pour vérifier que le nombre de sessions rapporté par
      Munin est correct
    - Renomme l'import DBStore en CachedDBStore pour éviter une confusion
      sur quel backend est utilisé pour stocker les sessions

--- zds/munin/tests.py
+++ zds/munin/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from zds.member.tests.factories import ProfileFactory
+
 
 class Munin(TestCase):
     def setUp(self):
@@ -25,3 +27,20 @@ class Munin(TestCase):
                 url = reverse(route)
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, 200)
+
+    def test_sessions(self):
+        """Makes sure the sessions are correctly counted in Munin data."""
+        response = self.client.get(reverse("munin:base:active-sessions"))
+        self.assertEqual(response.content.decode(), "sessions 0")
+
+        response = self.client.get(reverse("munin:base:total-sessions"))
+        self.assertEqual(response.content.decode(), "sessions 0")
+
+        profile = ProfileFactory()
+        self.client.force_login(profile.user)
+
+        response = self.client.get(reverse("munin:base:active-sessions"))
+        self.assertEqual(response.content.decode(), "sessions 1")
+
+        response = self.client.get(reverse("munin:base:total-sessions"))
+        self.assertEqual(response.content.decode(), "sessions 1")
