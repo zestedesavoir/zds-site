c0c51fbfa5a57cdf0b5c15cfb933066ba828e161
Philippe MILINK <philippe.milink@gmx.fr>
2024-10-19 23:37:32 +0200

    Précise mieux sur la page des alertes sur quel type d'élément porte l'alerte

--- zds/utils/models.py
+++ zds/utils/models.py
@@ -617,7 +617,14 @@ class CommentEdit(models.Model):
 
 
 class Alert(models.Model):
-    """Alerts on Profiles, PublishedContents and all kinds of Comments."""
+    """Alerts on Profiles, PublishedContents and all kinds of Comments.
+
+    The scope field indicates on which type of element the alert is made:
+    - PROFILE: the profile of a member
+    - FORUM: a post on a topic in a forum
+    - CONTENT: the content (article, opinion or tutorial) itself
+    - elements of TYPE_CHOICES (ARTICLE, OPINION, TUTORIAL): a comment on a content of this type
+    """
 
     SCOPE_CHOICES = [
         ("PROFILE", _("Profil")),
@@ -681,9 +688,17 @@ class Alert(models.Model):
 
     def get_type(self):
         if self.scope in TYPE_CHOICES_DICT:
-            return _("Commentaire")
+            assert self.comment is not None
+            return _(f"Commentaire sur un {self.SCOPE_CHOICES_DICT[self.scope].lower()}")
+        elif self.scope == "FORUM":
+            assert self.comment is not None
+            return _("Message de forum")
+        elif self.scope == "PROFILE":
+            assert self.profile is not None
+            return _("Profil")
         else:
-            return self.get_scope_display()
+            assert self.content is not None
+            return self.SCOPE_CHOICES_DICT[self.content.type]
 
     def is_automated(self):
         """Returns true if this alert was opened automatically."""
