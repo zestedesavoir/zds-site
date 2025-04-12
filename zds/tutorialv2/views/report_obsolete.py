13d83dae4830364a8fe754a3dcc42e814e37534c
Mathieu MOREL <Mathieu.Morel@bordeaux-inp.fr>
2025-04-12 17:27:12 +0200

    Ajout du tableau pour les signalements de contenus obsoletes

--- zds/tutorialv2/views/report_obsolete.py
+++ zds/tutorialv2/views/report_obsolete.py
@@ -2,27 +2,56 @@ from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMix
 
 from zds.tutorialv2.forms import DecideObsoleteForm
 from zds.tutorialv2.models.database import PotentialObsolete
-from zds.utils.paginator import ZdSPagingListView
+from django.views.generic import ListView
+from django.shortcuts import get_object_or_404
+from zds.utils.models import SubCategory
+from django.http import Http404
 
 
-class PotentialObsoleteContentListView(LoginRequiredMixin, PermissionRequiredMixin, ZdSPagingListView):
-    """
-    Displays a paginated list of reports of potential obsolete contents.
-    Only accessible to staff members.
-    """
+class PotentialObsoleteContentListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+    """List the obsolete reports, with possibilities of filters"""
 
     permission_required = "tutorialv2.change_publishablecontent"
-    model = PotentialObsolete
     template_name = "tutorialv2/list_page_elements/potentialobsolete_list.html"
     context_object_name = "reports"
     ordering = "-report_date"
-    paginate_by = 10
+    subcategory = None
 
     def get_queryset(self):
-        """Customize the queryset to order by report_date (most recent first)."""
-        return PotentialObsolete.objects.all().select_related("publishable_content").order_by(self.ordering)
+        queryset = PotentialObsolete.objects.all().select_related("publishable_content")
+
+        # filtering by type
+        try:
+            type_ = self.request.GET["type"]
+            if type_ == "unprocessed":
+                queryset = queryset.filter(status="nouveau")
+            if type_ == "processed":
+                queryset = queryset.filter(status="traite")
+            if type_ == "ignored":
+                queryset = queryset.filter(status="ignore")
+            if type_ == "article":
+                queryset = queryset.filter(publishable_content__type="ARTICLE")
+            if type_ == "tuto":
+                queryset = queryset.filter(publishable_content__type="TUTORIAL")
+            else:
+                raise KeyError()
+        except KeyError:
+            pass
+
+        # filtering by category
+        try:
+            category_pk = int(self.request.GET["subcategory"])
+            self.subcategory = get_object_or_404(SubCategory, pk=category_pk)
+            queryset = queryset.filter(publishable_content__subcategory__in=[self.subcategory])
+        except KeyError:
+            pass
+        except ValueError:
+            raise Http404("Format invalide pour le paramètre de la sous-catégorie.")
+
+        return queryset.order_by(self.ordering).all()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["forms_decide_obsolete"] = {report.pk: DecideObsoleteForm(obj=report) for report in context["reports"]}
+        context["category"] = self.subcategory
         return context
