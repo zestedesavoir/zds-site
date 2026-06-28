2813692b7abf2960628e1a4b94ce13c0c74d84ad
Philippe MILINK <philippe.milink@gmx.fr>
2026-06-28 21:13:17 +0200

    Utilise la version 30.2 de Typesense

--- scripts/define_variable.sh
+++ scripts/define_variable.sh
@@ -15,7 +15,7 @@ if [[ $ZDS_NVM_VERSION == "" ]]; then
 fi
 
 if [[ $ZDS_TYPESENSE_VERSION == "" ]]; then
-    ZDS_TYPESENSE_VERSION="29.0" # needs to be also updated in .github/workflows/ci.yml
+    ZDS_TYPESENSE_VERSION="3O.2" # needs to be also updated in .github/workflows/ci.yml
 fi
 
 if [[ $ZDS_TYPESENSE_API_KEY == "" ]]; then
